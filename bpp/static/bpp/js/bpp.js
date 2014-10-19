d0ce620976066333127c30c0611f00111d8ae222
dotz <dotz@pirx>
2014-10-19 02:26:59 +0200

    STATIC

---
+++ bpp/static/bpp/js/bpp.js
@@ -0,0 +1,8 @@
+if (window.bpp == undefined) window.bpp = {};
+
+var queryDict = {};
+
+location.search.substr(1).split("&").forEach(
+    function (item) {
+        queryDict[item.split("=")[0]] = item.split("=")[1]
+    });
