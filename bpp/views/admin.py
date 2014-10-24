a001b858b1d941bba7c67879fee23ca97ad518b1
Michal Pasternak <michal.dtz@gmail.com>
2014-10-25 01:09:04 +0200

    Fix #247

--- bpp/views/admin.py
+++ bpp/views/admin.py
@@ -5,16 +5,18 @@ from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.http.response import Http404
 from django.views.generic import RedirectView
+from bpp.models.patent import Patent, Patent_Autor
 from bpp.models.wydawnictwo_ciagle import Wydawnictwo_Ciagle, Wydawnictwo_Ciagle_Autor
+from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte, \
+    Wydawnictwo_Zwarte_Autor
 
 
-class WydawnictwoCiagleTozView(RedirectView):
-
+class TozView(RedirectView):
     @transaction.atomic
     def get_redirect_url(self, pk):
         try:
-            w = Wydawnictwo_Ciagle.objects.get(pk=pk)
-        except Wydawnictwo_Ciagle.DoesNotExist:
+            w = self.klass.objects.get(pk=pk)
+        except self.klass.DoesNotExist:
             raise Http404
 
         w_copy = copy.copy(w)
@@ -22,12 +24,26 @@ class WydawnictwoCiagleTozView(RedirectView):
         w_copy.tytul_oryginalny = u'[ ** KOPIA ** ]' + w_copy.tytul_oryginalny
         w_copy.save()
 
-        for wca in Wydawnictwo_Ciagle_Autor.objects.filter(rekord=w):
+        for wca in self.klass_autor.objects.filter(rekord=w):
             wca_copy = copy.copy(wca)
             wca_copy.id = None
             wca_copy.rekord = w_copy
             wca_copy.save()
 
-        return reverse("admin:bpp_wydawnictwo_ciagle_change", args=(w_copy.pk,))
+        return reverse("admin:bpp_%s_change" % self.klass_name, args=(w_copy.pk,))
 
 
+class WydawnictwoCiagleTozView(TozView):
+    klass = Wydawnictwo_Ciagle
+    klass_autor = Wydawnictwo_Ciagle_Autor
+    klass_name = 'wydawnictwo_ciagle'
+
+class WydawnictwoZwarteTozView(TozView):
+    klass = Wydawnictwo_Zwarte
+    klass_autor = Wydawnictwo_Zwarte_Autor
+    klass_name = 'wydawnictwo_zwarte'
+
+class PatentTozView(TozView):
+    klass = Patent
+    klass_autor = Patent_Autor
+    klass_name = 'patent'
