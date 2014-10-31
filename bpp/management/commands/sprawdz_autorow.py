85d7d734bf5e5a48adbc9030f5614c9716aa82d4
Michal Pasternak <michal.dtz@gmail.com>
2014-10-31 23:20:59 +0100

    Skrypt wykrywający nieprawidłowe kolejności

---
+++ bpp/management/commands/sprawdz_autorow.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+
+from django.core.management import BaseCommand
+
+from bpp.models.patent import Patent, Patent_Autor
+from bpp.models.wydawnictwo_ciagle import Wydawnictwo_Ciagle, \
+    Wydawnictwo_Ciagle_Autor
+from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte, \
+    Wydawnictwo_Zwarte_Autor
+
+
+def znajdz_i_napraw_podwojnych(model, autor_model):
+    rekord_pk = None
+    poprzednia_kolejnosc = None
+
+    for obj in autor_model.objects.all().order_by('rekord', 'kolejnosc'):
+        # print autor_model, obj.rekord_id, obj.pk, obj.kolejnosc
+
+        if obj.rekord_id != rekord_pk:
+            rekord_pk = obj.rekord_id
+            poprzednia_kolejnosc = None
+
+            if obj.kolejnosc != 1:
+                print "XX Dla rekordu %r %r autorzy zaczynaja sie od kolejnosci %i" % (
+                    autor_model, obj.pk, obj.kolejnosc)
+
+        if poprzednia_kolejnosc == obj.kolejnosc:
+            print "XX Wykryto zdublowana kolejnosc %i dla wpisu %s %s" % (
+                poprzednia_kolejnosc, autor_model, obj.pk)
+
+        if poprzednia_kolejnosc is not None:
+            delta = obj.kolejnosc - poprzednia_kolejnosc
+            if delta != 1:
+                print "XX roznica w kolejnosci wynosi %i dla rekordu %s %s" % (
+                    delta, autor_model, obj.pk
+                )
+        poprzednia_kolejnosc = obj.kolejnosc
+
+
+class Command(BaseCommand):
+    help = u'Naprawia indeks autorow - szuka wpisów o tej samej kolejności'
+
+    def handle(self, *args, **options):
+
+        for model, autor_model in [
+            (Wydawnictwo_Ciagle, Wydawnictwo_Ciagle_Autor),
+            (Wydawnictwo_Zwarte, Wydawnictwo_Zwarte_Autor),
+            (Patent, Patent_Autor)]:
+            znajdz_i_napraw_podwojnych(model, autor_model)
