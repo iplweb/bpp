856fa42d32d70b04ad691c3ea5f0b8f37a07f994
Michal Pasternak <michal.dtz@gmail.com>
2014-11-07 19:51:14 +0100

    Jeszcze lepsze indeksy kolejnosci autorow

---
+++ bpp/migrations/0005_auto_20141107_1950.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0004_auto_20141107_1101'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='patent_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'kolejnosc')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='wydawnictwo_ciagle_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'kolejnosc')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='wydawnictwo_zwarte_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'kolejnosc')]),
+        ),
+    ]
