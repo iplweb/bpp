2ed67cda6df8f7b8d83e53749054f28ddcc50270
Michal Pasternak <michal.dtz@gmail.com>
2014-11-07 11:01:28 +0100

    Migracja indeksów

---
+++ bpp/migrations/0004_auto_20141107_1101.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0003_auto_20141023_1419'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='patent_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'autor', 'kolejnosc')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='wydawnictwo_ciagle_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'autor', 'kolejnosc')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='wydawnictwo_zwarte_autor',
+            unique_together=set([('rekord', 'autor', 'typ_odpowiedzialnosci'), ('rekord', 'autor', 'kolejnosc')]),
+        ),
+    ]
