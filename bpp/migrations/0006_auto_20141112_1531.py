3b280effe23667972ec0a62d15b7ed8f9583e9d1
Michal Pasternak <michal.dtz@gmail.com>
2014-11-12 15:40:42 +0100

    Fix #316

---
+++ bpp/migrations/0006_auto_20141112_1531.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0005_auto_20141107_1950'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='praca_doktorska',
+            name='promotor',
+            field=models.ForeignKey(related_name=b'promotor_doktoratu', blank=True, to='bpp.Autor', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='patent',
+            name='afiliowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='patent',
+            name='recenzowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_doktorska',
+            name='afiliowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_doktorska',
+            name='recenzowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_habilitacyjna',
+            name='afiliowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_habilitacyjna',
+            name='recenzowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_ciagle',
+            name='afiliowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_ciagle',
+            name='recenzowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='afiliowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='recenzowana',
+            field=models.BooleanField(default=False, db_index=True),
+        ),
+    ]
--- bpp/models/praca_doktorska.py
+++ bpp/models/praca_doktorska.py
@@ -2,8 +2,10 @@
 
 from django.db import models
 from djorm_pgfulltext.models import SearchManager
-from bpp.models import Autor, ModelPunktowany, Jednostka
+
+from bpp.models import Autor, Jednostka
 from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte_Baza
+
 
 class Praca_Doktorska_Baza(Wydawnictwo_Zwarte_Baza):
 
@@ -19,6 +21,8 @@ class Praca_Doktorska_Baza(Wydawnictwo_Zwarte_Baza):
 
 
 class Praca_Doktorska(Praca_Doktorska_Baza):
+
+    promotor = models.ForeignKey(Autor, related_name="promotor_doktoratu", blank=True, null=True)
 
     objects = SearchManager(
         fields=['tytul_oryginalny', 'tytul'],
