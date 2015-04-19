976af9d057518c4e5022b5d48e40499a3431166e
dotz <dotz@THINKPAD>
2015-04-19 23:11:08 +0200

    Przechodze na inny system settings -> po jednym dla kazdego rodzaju konfiguracji + uzycie zmiennych srodowiskowych ZAMIAST generowania pliku local_settings.py

---
+++ django_bpp/settings/production.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+SENDFILE_BACKEND = 'sendfile.backends.nginx'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+
+MEDIA_ROOT = "/home/%s/django-bpp-media"
+SENDFILE_ROOT = MEDIA_ROOT
