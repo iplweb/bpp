350483b0cd8582bdebe7789cb6145e3fabd526b1
Michal Pasternak <dotz@THINKPAD>
2015-05-01 14:16:49 +0200

    bpp version

--- bpp/templatetags/bpp_version.py
+++ bpp/templatetags/bpp_version.py
@@ -4,15 +4,14 @@ import time
 
 from django import template
 
-from django_bpp.version import version
-
+from django_bpp.version import VERSION
 
 register = template.Library()
 
 
 @register.simple_tag
 def bpp_version():
-    return version
+    return VERSION
 
 @register.simple_tag
 def bpp_localtime():
