0a8bccb5eef970e579d03787abd0df519559e523
Michal Pasternak <dotz@THINKPAD>
2015-05-30 18:03:59 +0200

    Kasowanie autorow bez publikacji, #407

---
+++ bpp/management/commands/remove_empty_authors.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+from optparse import make_option
+import sys
+
+from django.core.management import BaseCommand
+from django.db import transaction
+
+from bpp.models.cache import Rekord
+from bpp.models import Autor, Autorzy
+
+class Command(BaseCommand):
+    help = 'Kasuje autorow bez prac'
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        for a in Autor.objects.all().only("id"):
+            res = Autorzy.objects.filter(autor_id=a.pk)[:1]
+            if res.count() == 0:
+                print "BRAK PRAC: ID=%s, autor=%s, USUWAM!" % (a.pk, str(a))
+                a.delete()
