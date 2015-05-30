0548c0b931ebcf2058303195d2fc800ff672223b
Michal Pasternak <dotz@THINKPAD>
2015-05-30 17:15:53 +0200

    Drobiazgi

--- django_bpp/settings/local.py
+++ django_bpp/settings/local.py
@@ -1,5 +1,16 @@
 # -*- encoding: utf-8 -*-
 
+# Na WINDOWS trzeba:
+# - zainstalowac nodejs
+# - zainstalowac npm
+# - zainstalowac bower,
+# - zainstalowac requirements
+#
+# Nastepnie uruchamiamy:
+# - venv\scripts\activate.bat
+# - python manage.py bower install
+# - python manage.py collectstatic
+
 import os
 
 os.environ['DJANGO_BPP_RAVEN_CONFIG_URL'] = 'http://4355f955f2ae4522ba06752c05eaff0a:5a62fbddd2ac4c0ab3d25b22c352df2a@sentry.iplweb.pl:9000/13'
@@ -12,6 +23,7 @@ os.environ['DJANGO_BPP_DB_USER'] = 'django-bpp'
 os.environ['DJANGO_BPP_DB_PASSWORD'] = 'test'
 os.environ['DJANGO_BPP_DB_HOST'] = 'localhost'
 os.environ['DJANGO_BPP_DB_PORT'] = '15432'
+os.environ['DJANGO_BPP_REDIS_PORT'] = '16379'
 
 from .base import *
 
@@ -32,3 +44,7 @@ MEDIA_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', '..', 'media')
 )
 SENDFILE_ROOT = MEDIA_ROOT
+
+COMPRESS_OFFLINE = False
+
+COMPRESS_ENABLED = False
