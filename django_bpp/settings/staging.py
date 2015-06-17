17eca6e9d888ff7a74ada177675369d51ac05c8d
Michal Pasternak <dotz@THINKPAD>
2015-06-17 21:59:21 +0200

    Notyfikacje dla linii polecen

--- django_bpp/settings/staging.py
+++ django_bpp/settings/staging.py
@@ -1,3 +1,8 @@
 from .production import *
 
 COMPRESS_OFFLINE = True
+
+# Te ustawienia tylko dla linii polecen
+NOTIFICATIONS_HOST = 'staging-bpp.local'
+NOTIFICATIONS_PORT = 80
+NOTIFICATIONS_PROTOCOL = 'http'
