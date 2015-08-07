de4b3307a2e47f3dd29f1ea7059510125b7ee90f
Michał Pasternak <michal.dtz@gmail.com>
2015-08-07 02:35:51 +0200

    Requirements dla MacOS X

---
+++ deployment/build-wheels.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Ten skrypt dziala spod virtualenv, aktywuj jakies"
+    exit 1
+fi
+
+export DISTDIR=${1-./DIST/}
+
+echo "Buduje wheels w $DISTDIR"
+
+mkdir -p $DISTDIR
+
+export PIP_WHEEL="pip wheel --wheel-dir=$DISTDIR --find-links=$DISTDIR --use-wheel -v"
+
+platform='unknown'
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Linux' ]]; then
+    $PIP_WHEEL -r requirements/ubuntu.requirements.txt
+
+elif [[ "$unamestr" == 'Darwin' ]]; then
+
+
+    for f in $DISTDIR/lxml*macosx*; do
+	[ -e "$f" ] && echo "lxml exists, not building" || $PIP_WHEEL --build-option="--static-deps" lxml 
+	break
+    done
+
+    $PIP_WHEEL -r requirements/osx.requirements.txt
+
+fi
+
+$PIP_WHEEL -r requirements/src.requirements.txt
+
+$PIP_WHEEL -r requirements/requirements.txt
+
