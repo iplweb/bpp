ecdeb5a29acfbd10caa6d465e7018a5817e5ca41
User vagrant <vagrant@django-devhost>
2015-08-08 15:09:52 +0000

    Linxu version

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 platform='unknown'
 unamestr=`uname`
@@ -7,14 +7,14 @@ hostnamestr=`hostname`
 
 if [[ "$unamestr" == 'Linux' ]]; then
 
-	apt-get -y install python postgresql python-gevent python-psycopg2 python-imaging python-crypto python-simplejson python-sqlalchemy postgresql-plpython-9.3 postgresql-contrib-9.3 redis-server zip unzip
+    sudo apt-get -y install python postgresql python-gevent python-psycopg2 python-imaging python-crypto python-simplejson python-sqlalchemy postgresql-plpython-9.3 postgresql-contrib-9.3 redis-server zip unzip
 	
-	if [[ "$hostnamestr" == "master" ]]; then
+    if [[ "$hostnamestr" == "master" ]]; then
 	
-		# Install dev build packages
-		sudo apt-get install -y  libpq-dev libjpeg-dev libpng-dev libxml2-dev libxslt1-dev libevent-dev firefox tightvncserver icewm libxslt1-dev python-dev
+	# Install dev build packages
+	sudo apt-get install -y  libpq-dev libjpeg-dev libpng-dev libxml2-dev libxslt1-dev libevent-dev firefox tightvncserver icewm libxslt1-dev python-dev
 	
-	fi
+    fi
 
 elif [[ "$unamestr" == 'Darwin' ]]; then
 
