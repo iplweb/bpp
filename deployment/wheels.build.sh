aa5f59214394a505f0d1fa2e4b149fa472c841e8
Michał Pasternak <michal.dtz@gmail.com>
2015-08-08 17:06:01 +0200

    Check

--- deployment/wheels.build.sh
+++ deployment/wheels.build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 if [ -z "$VIRTUAL_ENV" ]; then
     echo "Ten skrypt dziala spod virtualenv, aktywuj jakies"
@@ -16,10 +16,10 @@ export PIP_WHEEL="pip wheel --wheel-dir=$DISTDIR --find-links=$DISTDIR --use-whe
 platform='unknown'
 unamestr=`uname`
 
-if [ "$unamestr" == 'Linux' ]; then
+if [[ "$unamestr" == 'Linux' ]]; then
     $PIP_WHEEL -r requirements/$unamestr.requirements.txt
 
-elif [ "$unamestr" == 'Darwin' ]; then
+elif [[ "$unamestr" == 'Darwin' ]]; then
 
 
     for f in $DISTDIR/lxml*macosx*; do
