92283fa228dbabc444d98c3725d31d274a4e9dc2
Michał Pasternak <michal.dtz@gmail.com>
2015-08-08 17:51:30 +0200

    Check

---
+++ deployment/prepare-build-env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Ten skrypt dziala spod virtualenv, aktywuj jakies"
+    exit 1
+fi
+
+if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
+    echo "Ustaw zmienna DJANGO_SETTINGS_MODULE, bo sie nie uda..."
+    exit 1
+fi
+
+
+cd ../src/django_bpp
+
+npm install grunt-sass grunt-contrib-watch grunt-contrib-qunit
+
+python manage.py bower_install -F
+python manage.py collectstatic --noinput
+
+grunt build
+
+python manage.py migrate
+python manage.py compress --force
+
+
