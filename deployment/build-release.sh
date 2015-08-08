10b27afc3bb2ca7db76ac4a48596d37f053e3b03
Michał Pasternak <michal.dtz@gmail.com>
2015-08-08 21:46:54 +0200

    Inna nazwa

--- deployment/build-release.sh
+++ deployment/build-release.sh
@@ -13,16 +13,16 @@ export PWD=`pwd`
 export CURDIR=`basename $PWD`
 
 cd ..
-rm -rf django-bpp-release
-cp -R $CURDIR django-bpp-release
+rm -rf django_bpp-release
+cp -R $CURDIR django_bpp-release
 
-cd django-bpp-release
+cd django_bpp-release
 rm -rf src/django_bpp/node_modules files .git src/django_bpp/components Vagrantfile Makefile provisioning deployment/node_modules .idea .vagrant
 export VERSION=`python src/django_bpp/django_bpp/version.py`
 
 cd ..
-mv django-bpp-release django-bpp-release-$VERSION
+mv django_bpp-release django_bpp-release-$VERSION
 
-tar -czvf django-bpp-release-$VERSION.tgz django-bpp-release-$VERSION
+tar -czvf django_bpp-release-$VERSION.tgz django_bpp-release-$VERSION
 
-rm -rf django-bpp-release-$VERSION
+rm -rf django_bpp-release-$VERSION
