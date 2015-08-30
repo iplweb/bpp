d7878c5acf2eb002e88bad62480c9e0af701c547
Michał Pasternak <michal.dtz@gmail.com>
2015-08-30 17:25:54 +0200

    Checkpoint

---
+++ src/django_bpp/eksport_pbn/migrations/0003_auto_20150830_1633.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eksport_pbn', '0002_auto_20150826_0050'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='plikeksportupbn',
+            name='artykuly',
+            field=models.BooleanField(default=True, verbose_name=b'Artyku\xc5\x82y'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='plikeksportupbn',
+            name='ksiazki',
+            field=models.BooleanField(default=True, verbose_name=b'Ksi\xc4\x85\xc5\xbcki'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='plikeksportupbn',
+            name='rozdzialy',
+            field=models.BooleanField(default=True, verbose_name=b'Rozdzia\xc5\x82y'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='plikeksportupbn',
+            name='rok',
+            field=models.IntegerField(choices=[(2013, b'2013'), (2014, b'2014'), (2015, b'2015')]),
+            preserve_default=True,
+        ),
+    ]
