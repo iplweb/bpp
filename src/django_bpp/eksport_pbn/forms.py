d236c8d3f10263ce030e6f012259b5805044a628
Michał Pasternak <michal.dtz@gmail.com>
2015-08-30 17:25:47 +0200

    Checkpoint

--- src/django_bpp/eksport_pbn/forms.py
+++ src/django_bpp/eksport_pbn/forms.py
@@ -1,1 +1,28 @@
-__author__ = 'mpasternak'
+# -*- encoding: utf-8 -*-
+from crispy_forms.helper import FormHelper
+from crispy_forms_foundation.layout import Layout, Fieldset, Submit
+
+from django import forms
+from bpp.models.struktura import Wydzial
+from eksport_pbn.models import PlikEksportuPBN
+
+
+class EksportDoPBNForm(forms.ModelForm):
+    class Meta:
+        model = PlikEksportuPBN
+        fields = ['wydzial', 'rok', 'artykuly', 'ksiazki', 'rozdzialy']
+
+
+    def __init__(self, *args, **kw):
+        super(EksportDoPBNForm, self).__init__(*args, **kw)
+
+        helper = FormHelper(self)
+        helper.form_class = "custom"
+        helper.layout = Layout(
+            Fieldset(
+                u'Zamów eksport PBN',
+                'wydzial', 'rok', 'artykuly', 'ksiazki', 'rozdzialy',
+                Submit('submit', u'Zamów', css_id='id_submit')))
+        self.helper = helper
+
+        self.fields['wydzial'].queryset = Wydzial.objects.filter(widoczny=True)
--- src/django_bpp/eksport_pbn/urls.py
+++ src/django_bpp/eksport_pbn/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls import patterns, url, include
-from django.contrib.auth.decorators import login_required
+from django.conf.urls import patterns, url
 
-from django.conf import settings
-from eksport_pbn.views import WyborWydzialu, Generuj, SerwujPlik
-from integrator.views import FileUploadView, FileListView, AutorIntegrationFileDetail
+from eksport_pbn.views import Generuj, SerwujPlik, ZamowEksportDoPBN
 
 urlpatterns = patterns(
     '',
@@ -14,7 +11,9 @@ urlpatterns = patterns(
     url(r'^download/(?P<pk>[0-9]+)$',
         SerwujPlik.as_view(), name='pobierz'),
 
-    url(r'^$',
-        WyborWydzialu.as_view(), name='wybor_wydzialu'),
+    # url(r'^$',
+    #     WyborWydzialu.as_view(), name='wybor_wydzialu'),
+
+    url(r'zamow/$', ZamowEksportDoPBN.as_view(), name='zamow')
 
 )
