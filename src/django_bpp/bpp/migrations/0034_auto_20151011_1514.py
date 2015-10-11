ee31048ffe2934712da0bab6e1d0e5d4e4bad0d3
Michał Pasternak <michal.dtz@gmail.com>
2015-10-11 15:46:49 +0200

    Eksport DOI oraz popbrawki eksportu OpenAccess

---
+++ src/django_bpp/bpp/migrations/0034_auto_20151011_1514.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0033_auto_20151001_1425'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='wydawnictwo_ciagle',
+            name='openaccess_ilosc_miesiecy',
+            field=models.PositiveIntegerField(help_text=b'Ilo\xc5\x9b\xc4\x87 miesi\xc4\x99cy jakie up\xc5\x82yn\xc4\x99\xc5\x82y od momentu opublikowania do momentu udost\xc4\x99pnienia', null=True, verbose_name=b'OpenAccess: ilo\xc5\x9b\xc4\x87 miesi\xc4\x99cy', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='wydawnictwo_zwarte',
+            name='openaccess_ilosc_miesiecy',
+            field=models.PositiveIntegerField(help_text=b'Ilo\xc5\x9b\xc4\x87 miesi\xc4\x99cy jakie up\xc5\x82yn\xc4\x99\xc5\x82y od momentu opublikowania do momentu udost\xc4\x99pnienia', null=True, verbose_name=b'OpenAccess: ilo\xc5\x9b\xc4\x87 miesi\xc4\x99cy', blank=True),
+            preserve_default=True,
+        ),
+    ]
