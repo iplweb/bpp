7f7fce02409909761b087a0a88665bb7e3d98452
Michał Pasternak <michal.dtz@gmail.com>
2015-10-26 13:22:46 +0100

    Fix #420

---
+++ src/django_bpp/functional_tests/test_pytaj_o_zmiany_formularza.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+import pytest
+from selenium.common.exceptions import NoAlertPresentException
+
+
+def test_zmiany_formularza(preauth_browser, live_server):
+    preauth_browser.visit(live_server + '/password_change/')
+    elem = preauth_browser.find_by_id("id_old_password")[0]
+    elem.type("123348u3")
+
+    link = preauth_browser.find_link_by_href("/bpp/raporty/jednostek/")
+    link[0].click()
+
+    with pytest.raises(NoAlertPresentException):
+        preauth_browser.get_alert()
