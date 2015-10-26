62138ede49b34ed11464da4c4b675fb197a9257f
Michał Pasternak <michal.dtz@gmail.com>
2015-10-26 13:05:21 +0100

    Nowy skrypt do testow

---
+++ deployment/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Ten skrypt dziala spod virtualenv, aktywuj jakies"
+    exit 1
+fi
+
+if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
+    echo "Ustaw zmienna DJANGO_SETTINGS_MODULE, bo sie nie uda..."
+    exit 1
+fi
+
+unamestr=`uname`
+
+export DJANGO_SETTINGS_MODULE=django_bpp.settings.local
+
+if [[ "$unamestr" == 'Linux' ]]; then
+    export DISPLAY=localhost:1
+    export DJANGO_SETTINGS_MODULE=django_bpp.settings.test
+fi
+
+cd ../src/django_bpp
+
+grunt qunit
+py.test --create-db functional_tests integrator/tests eksport_pbn bpp/tests-pytest
+python manage.py test --noinput bpp
