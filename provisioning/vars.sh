470ce3faa484a299c0826179e0d14a26b427c81d
Michał Pasternak <michal.dtz@gmail.com>
2016-01-24 13:17:50 +0100

    Zmienne srodowiskowe

---
+++ provisioning/vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+echo "export DJANGO_SETTINGS_MODULE=django_bpp.settings.test" >> ~/.bashrc
+echo "export DJANGO_BPP_SECRET_KEY=0xdeadbeef" >> ~/.bashrc
+
+echo "export PGHOST=bpp-db" >> ~/.bashrc
+echo "export PGUSER=bpp" >> ~/.bashrc
+echo "export PGPASSWORD=password" >> ~/.bashrc
+echo "export PGDATABASE=bpp" >> ~/.bashrc
+
