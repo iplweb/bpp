4ef77112256711307c829bd9af99ae8b39a394ac
Michał Pasternak <michal.dtz@gmail.com>
2016-01-24 16:35:52 +0100

    Pobieranie dumpa bazy danych

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
 from fabric.api import *
 
-env['hosts'] = ['vagrant@bpp-master']
-env['password'] = 'vagrant'
+if not env['hosts']:
+    env['hosts'] = ['vagrant@bpp-master']
+    env['password'] = 'vagrant'
+
 env['shell'] = "/bin/bash -l -i -c" 
 
-def wheels():
-    run("django-bpp/provisioning/wheels.sh")
+env['db'] = 'bpp'
+env['dbuser'] = 'bpp'
 
 def prepare():
     run("django-bpp/buildscripts/prepare-build-env.sh")
@@ -16,6 +18,36 @@ def test():
 def build():
     run("django-bpp/buildscripts/build-release.sh")
 
+def venv():
+    run("rm -rf env")
+    run("/vagrant/provisioning/venv.sh")
+
+def wheels():
+    run("django-bpp/provisioning/wheels.sh")
+
 def vcs():
     with cd("django-bpp"):
         run("git pull")
+        run("git reset --hard")
+
+def download_db(restore=False, cleanup=False, recreate=True, download=True):
+    """Download remote database dump.
+    Restore dump on local host if 'restore' is True.
+    Drop and recreate the target database if 'recreate' is True, remove dump
+    file afterwards if 'remove' is also True.
+    """
+    if download is True:
+        with settings(hide('stdout', 'running')):
+            run('pg_dump --clean -F custom '
+                '{0} > {0}.backup'.format(env.db))
+        dump_file = get('{}.backup'.format(env.db), '%(host)s-%(path)s')[0]
+    if not restore:
+        return
+    if recreate:
+        with settings(warn_only=True):
+            local('dropdb -U {0} {1}'.format(env.dbuser, env.db))
+        local('createdb --echo --encoding=UTF8 '
+              '-U postgres --owner={0} {1}'.format(env.dbuser, env.db))
+    local('pg_restore -U postgres -d {1} {2}'.format(env.dbuser, env.db, dump_file))
+    if cleanup:
+        local('/bin/rm {}'.format(dump_file))
