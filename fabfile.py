8ed428d54021b88329f20d7fd14e8fa6ba5561eb
Michał Pasternak <michal.dtz@gmail.com>
2016-01-24 14:57:45 +0100

    Male poprawki procesu budowania

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,21 @@
+from fabric.api import *
+
+env['hosts'] = ['vagrant@bpp-master']
+env['password'] = 'vagrant'
+env['shell'] = "/bin/bash -l -i -c" 
+
+def wheels():
+    run("django-bpp/provisioning/wheels.sh")
+
+def prepare():
+    run("django-bpp/buildscripts/prepare-build-env.sh")
+
 def test():
-    from fabtools import require, deb
-    deb.update_index()
-    require.postgres.server()
+    run("django-bpp/buildscripts/run-tests.sh")
+
+def build():
+    run("django-bpp/buildscripts/build-release.sh")
+
+def vcs():
+    with cd("django-bpp"):
+        run("git pull")
