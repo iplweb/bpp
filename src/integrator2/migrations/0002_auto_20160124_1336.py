674cd4949ca906f4aa0313324407108a3d560165
Michał Pasternak <michal.dtz@gmail.com>
2016-01-24 13:37:13 +0100

    Django 1.8

---
+++ src/integrator2/migrations/0002_auto_20160124_1336.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import integrator2.models.lista_ministerialna
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('integrator2', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='listaministerialnaelement',
+            options={'ordering': ['nazwa']},
+        ),
+        migrations.AlterModelOptions(
+            name='listaministerialnaintegration',
+            options={'ordering': ['-uploaded_on'], 'verbose_name': 'integracja list ministerialnych'},
+        ),
+        migrations.AlterField(
+            model_name='listaministerialnaintegration',
+            name='last_updated_on',
+            field=models.DateTimeField(auto_now=True),
+        ),
+        migrations.AlterField(
+            model_name='listaministerialnaintegration',
+            name='year',
+            field=models.IntegerField(default=integrator2.models.lista_ministerialna.last_year, help_text='Rok dla kt\xf3rego zosta\u0142a wydana ta lista ministerialna', verbose_name='Rok'),
+        ),
+    ]
