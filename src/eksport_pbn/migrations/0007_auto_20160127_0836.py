12823f87cf1eb6d1b179e24507be3544b38d4b19
Michał Pasternak <michal.dtz@gmail.com>
2016-01-27 09:08:55 +0100

    Pola od_roku, do_roku i fajniejszy forumularz z datepickerem.

---
+++ src/eksport_pbn/migrations/0007_auto_20160127_0836.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eksport_pbn', '0006_auto_20151203_1418'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='plikeksportupbn',
+            name='rok',
+        ),
+        migrations.AddField(
+            model_name='plikeksportupbn',
+            name='do_roku',
+            field=models.IntegerField(default=2016, choices=[(2013, b'2013'), (2014, b'2014'), (2015, b'2015'), (2016, b'2016')]),
+        ),
+        migrations.AddField(
+            model_name='plikeksportupbn',
+            name='od_roku',
+            field=models.IntegerField(default=2016, choices=[(2013, b'2013'), (2014, b'2014'), (2015, b'2015'), (2016, b'2016')]),
+        ),
+        migrations.AlterField(
+            model_name='plikeksportupbn',
+            name='do_daty',
+            field=models.DateField(null=True, verbose_name=b'Do daty', blank=True),
+        ),
+    ]
