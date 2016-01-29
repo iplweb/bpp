cae3bd3daab51df6443c29081911fc0d91981b8a
Michał Pasternak <michal.dtz@gmail.com>
2016-01-29 12:02:13 +0100

    Testy dla walidacji formularza

--- src/eksport_pbn/tests.py
+++ src/eksport_pbn/tests.py
@@ -1,3 +1,7 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from datetime import datetime, timedelta
 
 from django.core.urlresolvers import reverse
@@ -118,9 +122,37 @@ def test_z_datami_2(db):
     assert str(d).replace("-", "_") in s
 
 
-def test_submit_report_form(admin_app):
-    w = mommy.make(Wydzial)
+def test_submit_report_form(admin_app, wydzial):
     page = admin_app.get(reverse("eksport_pbn:zamow"))
-    page.form['wydzial'] = w.pk
+    page.form['wydzial'] = wydzial.pk
     res = page.form.submit().maybe_follow()
     assert res.status_code == 200
+
+
+def test_submit_report_form_validation_data_od_do(admin_app, wydzial):
+    page = admin_app.get(reverse("eksport_pbn:zamow"))
+    page.form['wydzial'] = wydzial.pk
+    page.form['od_daty'] = '2010-01-01'
+    page.form['do_daty'] = '2009-01-01'
+    res = page.form.submit()
+    assert "Wartość w polu &#39;Od daty&#39; musi" in res.content.decode('utf-8')
+
+
+def test_submit_report_form_validation_rok_od_do(admin_app, wydzial):
+    page = admin_app.get(reverse("eksport_pbn:zamow"))
+    page.form['wydzial'] = wydzial.pk
+    page.form['od_roku'] = '2015'
+    page.form['do_roku'] = '2014'
+    res = page.form.submit()
+    assert "Wartość w polu &#39;Od roku&#39; musi" in res.content.decode('utf-8')
+
+
+
+def test_submit_report_form_validation_artykuly_ksiazki(admin_app, wydzial):
+    page = admin_app.get(reverse("eksport_pbn:zamow"))
+    page.form['wydzial'] = wydzial.pk
+    page.form['artykuly'] = False
+    page.form['ksiazki'] = False
+    page.form['rozdzialy'] = False
+    res = page.form.submit()
+    assert "Wybierz przynajmniej jedną" in res.content.decode('utf-8')
