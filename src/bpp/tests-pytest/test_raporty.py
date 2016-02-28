8d1f6c10599bf611d4eb6df9c0a565af8efbe528
Michał Pasternak <michal.dtz@gmail.com>
2016-02-28 21:16:37 +0100

    Popraw też raporty roczne dla autorów tabela 2.1 i 2.5, DODATKOWO dodając warunek dla autora dla tabel 2.3 i 2.4

--- src/bpp/tests-pytest/test_raporty.py
+++ src/bpp/tests-pytest/test_raporty.py
@@ -30,6 +30,28 @@ def test_bug_raport_roczny_autorow_pkt_2_2_2_6_issue_441(autor_jan_kowalski, jed
     assert tabela_2_6 == 1
 
 
+@pytest.mark.django_db
+def test_bug_raport_roczny_autorow_pkt_2_1_2_5_issue_441(autor_jan_kowalski, jednostka, obiekty_bpp):
+    # http://mantis.iplweb.pl/view.php?id=441
+
+    ksz = obiekty_bpp.charakter_formalny["KSZ"]
+    ang = obiekty_bpp.jezyk["ang."]
+
+    praca_2_1 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksz, jezyk=ang, rok=2015)
+    praca_2_1.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="aut.")
+
+    praca_2_5 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksz, jezyk=ang, rok=2015)
+    praca_2_5.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="red.")
+
+    base_query = get_base_query_autor(autor=autor_jan_kowalski, rok_min=2015, rok_max=2015)
+
+    tabela_2_1 = raport_autorow_tabela("2_1", base_query, autor_jan_kowalski).count()
+    assert tabela_2_1 == 1
+
+    tabela_2_5 = raport_autorow_tabela("2_5", base_query, autor_jan_kowalski).count()
+    assert tabela_2_5 == 1
+
+
 def test_raport_autorow(app, autor_jan_kowalski, jednostka, obiekty_bpp):
     ksp = obiekty_bpp.charakter_formalny["KSP"]
     pol = obiekty_bpp.jezyk["pol."]
