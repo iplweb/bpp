df77f04cfedd279a52e4e1831afc9b562d21a666
Michał Pasternak <michal.dtz@gmail.com>
2016-05-15 19:37:48 +0200

    Checkpoint commit

---
+++ src/bpp/migrations/0038_autor_pesel_md5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0037_auto_20160124_1336'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='autor',
+            name='pesel_md5',
+            field=models.CharField(help_text=b'Hash MD5 numeru PESEL', max_length=32, null=True, db_index=True, blank=True),
+        ),
+    ]
