86a2a2a638c5a5d15fe087e238ecc3d3a46a3388
Michał Pasternak <michal.dtz@gmail.com>
2016-07-09 20:56:08 +0200

    Migracja dla liczba_znakow_wydawniczych

---
+++ src/bpp/migrations/0038_auto_20160708_0732.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0037_auto_20160124_1336'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='wydawnictwo_ciagle',
+            name='liczba_znakow_wydawniczych',
+            field=models.IntegerField(db_index=True, null=True, verbose_name=b'Liczba znak\xc3\xb3w wydawniczych', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='liczba_znakow_wydawniczych',
+            field=models.IntegerField(db_index=True, null=True, verbose_name=b'Liczba znak\xc3\xb3w wydawniczych', blank=True),
+        ),
+    ]
