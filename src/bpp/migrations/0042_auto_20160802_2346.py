95444e85f84e3a2a625d6725b99abf82763c5398
Michał Pasternak <michal.dtz@gmail.com>
2016-08-02 23:46:48 +0200

    Migracje

---
+++ src/bpp/migrations/0042_auto_20160802_2346.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0041_auto_20160802_2211'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='wydawnictwo_ciagle',
+            name='ostatnio_zmieniony_dla_pbn',
+            field=models.DateTimeField(help_text=b'Moment ostatniej aktualizacji rekordu dla potrzeb PBN. To pole zmieni si\xc4\x99 automatycznie, gdy\n        nast\xc4\x85pi zmiana dowolnego z p\xc3\xb3l za wyj\xc4\x85tkiem blok\xc3\xb3w p\xc3\xb3l: \xe2\x80\x9epunktacja\xe2\x80\x9d, \xe2\x80\x9epunktacja komisji centralnej\xe2\x80\x9d,\n        \xe2\x80\x9eadnotacje\xe2\x80\x9d oraz pole \xe2\x80\x9estatus korekty\xe2\x80\x9d.', verbose_name=b'Ostatnio zmieniony (dla PBN)', auto_now_add=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='ostatnio_zmieniony_dla_pbn',
+            field=models.DateTimeField(help_text=b'Moment ostatniej aktualizacji rekordu dla potrzeb PBN. To pole zmieni si\xc4\x99 automatycznie, gdy\n        nast\xc4\x85pi zmiana dowolnego z p\xc3\xb3l za wyj\xc4\x85tkiem blok\xc3\xb3w p\xc3\xb3l: \xe2\x80\x9epunktacja\xe2\x80\x9d, \xe2\x80\x9epunktacja komisji centralnej\xe2\x80\x9d,\n        \xe2\x80\x9eadnotacje\xe2\x80\x9d oraz pole \xe2\x80\x9estatus korekty\xe2\x80\x9d.', verbose_name=b'Ostatnio zmieniony (dla PBN)', auto_now_add=True),
+        ),
+    ]
---
+++ src/eksport_pbn/migrations/0008_auto_20160802_2346.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eksport_pbn', '0007_auto_20160127_0836'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='plikeksportupbn',
+            name='rodzaj_daty',
+            field=models.SmallIntegerField(default=3, help_text=b'Jakie pole z dat\xc4\x85 b\xc4\x99dzie u\xc5\xbcywane do wybierania rekord\xc3\xb3w?', verbose_name=b'Rodzaj pola daty', choices=[(1, b'data utworzenia'), (2, b'data aktualizacji'), (3, b'data aktualizacji dla PBN')]),
+        ),
+    ]
