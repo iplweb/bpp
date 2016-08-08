9c1112cd1aa669a5f292494949349caa7720028f
Michał Pasternak <michal.dtz@gmail.com>
2016-08-09 00:32:07 +0200

    Połącz migracje

---
+++ src/bpp/migrations/0045_auto_20160809_0004.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0044_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='jednostka',
+            name='obca_jednostka',
+            field=models.BooleanField(default=False, help_text=b'Zaznacz dla jednostki skupiaj\xc4\x85cej autor\xc3\xb3w nieindeksowanych,\n        np. dla "Obca jednostka" lub "Doktoranci".\n\n                <br/><br/>\n        Je\xc5\xbceli wi\xc4\x99cej, ni\xc5\xbc jedna jednostka w bazie danych ma to pole oznaczone jako \'PRAWDA\', autorzy\n        przypisywani b\xc4\x99d\xc4\x85 do jednostki obcej o ni\xc5\xbcszym numerze unikalnego identyfikatora (ID).\n        '),
+        ),
+    ]
