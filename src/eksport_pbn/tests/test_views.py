55e71d776d5b70706401aa9ed23682d15341c85e
Michał Pasternak <michal.dtz@gmail.com>
2016-12-10 21:29:05 +0100

    Rozbij testy na klika plików.

---
+++ src/eksport_pbn/tests/test_views.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+
+from django.core.urlresolvers import reverse
+
+
+def test_wybor(app, wydzial):
+    page = app.get(reverse('eksport_pbn:zamow'))
+    assert '2013' in page.content
