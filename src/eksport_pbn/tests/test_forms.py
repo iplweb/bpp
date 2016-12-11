76071595eb9c5a0df1460572ce7eccd376d5aec8
Michał Pasternak <michal.dtz@gmail.com>
2016-12-11 08:29:29 +0100

    Nic lepszego nie wymyślę (inne cudzysłowy na MacOS, inne na Linuxie)

--- src/eksport_pbn/tests/test_forms.py
+++ src/eksport_pbn/tests/test_forms.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 
 from django.core.urlresolvers import reverse
 
+def normalize_quotes(content):
+    return content.decode("utf-8").replace("&#39;", "'")
 
 def test_submit_report_form(admin_app, wydzial):
     page = admin_app.get(reverse("eksport_pbn:zamow"))
@@ -18,9 +20,7 @@ def test_submit_report_form_validation_data_od_do(admin_app, wydzial):
     page.form['od_daty'] = '2010-01-01'
     page.form['do_daty'] = '2009-01-01'
     res = page.form.submit()
-    macOs = "Wartość w polu &#39;Od daty&#39;" in res.content.decode("utf-8")
-    linux = "Wartość w polu 'Od daty'" in res.content.decode("utf-8")
-    assert macOs or linux
+    assert "Wartość w polu 'Od daty'" in normalize_quotes(res.content)
 
 
 def test_submit_report_form_validation_rok_od_do(admin_app, wydzial):
@@ -29,7 +29,7 @@ def test_submit_report_form_validation_rok_od_do(admin_app, wydzial):
     page.form['od_roku'] = '2015'
     page.form['do_roku'] = '2014'
     res = page.form.submit()
-    assert u'Warto\u015b\u0107 w polu &#39;Od roku&#39; musi' in res.content.decode('utf-8')
+    assert "Wartość w polu 'Od roku'" in normalize_quotes(res.content)
 
 
 def test_submit_report_form_validation_artykuly_ksiazki(admin_app, wydzial):
