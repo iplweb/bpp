30c291f5a699d0dbb768c10f78b9c64204e4ccd7
Michał Pasternak <michal.dtz@gmail.com>
2017-01-17 20:05:36 +0100

    Merge

---
+++ src/egeria/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('egeria', '0006_auto_20161210_2246'),
+        ('egeria', '0010_auto_20161119_2208'),
+    ]
+
+    operations = [
+    ]
