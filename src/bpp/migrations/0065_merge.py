b9322f4fb6a9876763ce55efcbf36e9f5c70c9fe
Michał Pasternak <michal.dtz@gmail.com>
2017-01-17 20:05:36 +0100

    Merge

---
+++ src/bpp/migrations/0065_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0064_autor_jednostka_aktualna'),
+        ('bpp', '0051_auto_20161201_1137'),
+    ]
+
+    operations = [
+    ]
