d8612e9f514f30fa1645beb927cee76d59dcfba4
Michał Pasternak <michal.dtz@gmail.com>
2017-02-06 00:08:36 +0100

    Dodaj pole 'całkowita liczba autorów' dla wydawnictwa zwartego

---
+++ src/bpp/migrations/0066_wydawnictwo_zwarte_calkowita_liczba_autorow.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0065_merge'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='wydawnictwo_zwarte',
+            name='calkowita_liczba_autorow',
+            field=models.PositiveIntegerField(help_text=b'Je\xc5\xbceli dodajesz monografi\xc4\x99, wpisz tutaj ca\xc5\x82kowit\xc4\x85 liczb\xc4\x99\n        autor\xc3\xb3w monografii. Ta informacja zostanie u\xc5\xbcyta w eksporcie danych do PBN.', null=True, blank=True),
+        ),
+    ]
