2eb674c0d9f4910c1621d070f1fb554b8cd2d7c2
Michał Pasternak <michal.dtz@gmail.com>
2017-02-07 00:28:54 +0100

    Usprawnienia filtrowania

--- src/bpp/migrations/0035_nomorefixtures_data_migration.py
+++ src/bpp/migrations/0035_nomorefixtures_data_migration.py
@@ -16,13 +16,21 @@ def load_fixtures(apps, schema_editor):
         "plec.json",
         "rodzaj_zrodla.json",
         "status_korekty.json",
-        "typ_kbn.json",
+        # 2017/02/06 przesuwam wczytywanie tego pliku do migracji 0067 - tabela Typ_KBN została zmieniona
+        # (ma więcej kolumn), wczytywanie danych przez loaddata _zawiedzie_ ponieważ loaddata używa świeżych
+        # obiektów, a nie obiektów na tym etapie migracji. Zatem, w migracji 0067 sprawdzamy, czy tabela
+        # Typ_KBN jest pusta, jeżeli jest - to tam zostanie zaczytany typ_kbn.json
+        #
+        # A tak między nami, to na tym etapie, ta migracja uruchamiana jest wyłącznie podczas testowania bazy
+        # danych czy tworzenia jej od zera. Na produkcyjnej bazie danych nie zostanie uruchomiona już nigdy.
+        #
+        # "typ_kbn.json",
         "typ_odpowiedzialnosci.json",
         "tytul.json",
         # "um_lublin_uczelnia.json",
         # "um_lublin_wydzial.json",
         "zrodlo_informacji.json"]:
-        call_command('loaddata', fixture, app_label='bpp')
+        call_command('loaddata', fixture, app='bpp', verbosity=0)
 
 
 class Migration(migrations.Migration):
