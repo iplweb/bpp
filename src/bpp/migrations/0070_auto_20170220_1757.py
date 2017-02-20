b0f79224639e4b836849c3954f61d2192a9b7ce0
Michał Pasternak <michal.dtz@gmail.com>
2017-02-20 23:46:11 +0100

    Lepszy regex do stron + testy, indeksowane pole „informacje”

---
+++ src/bpp/migrations/0070_auto_20170220_1757.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0069_eksport_pbn_charaktery'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='patent',
+            name='informacje',
+            field=models.TextField(db_index=True, null=True, verbose_name=b'Informacje', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_doktorska',
+            name='informacje',
+            field=models.TextField(db_index=True, null=True, verbose_name=b'Informacje', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='praca_habilitacyjna',
+            name='informacje',
+            field=models.TextField(db_index=True, null=True, verbose_name=b'Informacje', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_ciagle',
+            name='informacje',
+            field=models.TextField(db_index=True, null=True, verbose_name=b'Informacje', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='informacje',
+            field=models.TextField(db_index=True, null=True, verbose_name=b'Informacje', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='wydawnictwo_zwarte',
+            name='wydawnictwo_nadrzedne',
+            field=models.ForeignKey(related_name='wydawnictwa_powiazane_set', blank=True, to='bpp.Wydawnictwo_Zwarte', help_text=b'Je\xc5\xbceli dodajesz rozdzia\xc5\x82,\n        tu wybierz prac\xc4\x99, w ramach kt\xc3\xb3rej dany rozdzia\xc5\x82 wyst\xc4\x99puje.', null=True),
+        ),
+    ]
--- src/bpp/tests-pytest/test_models/test_abstract.py
+++ src/bpp/tests-pytest/test_models/test_abstract.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 from model_mommy import mommy
 
+from bpp.models.abstract import BRAK_PAGINACJI
 from bpp.models.struktura import Jednostka, Wydzial
 from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte_Autor
-
+import pytest
 
 def test_eksport_pbn_author_afiliacja_w_kontekscie_wydzialu(uczelnia, autor_jan_kowalski, wydawnictwo_zwarte):
     w1 = mommy.make(Wydzial, uczelnia=uczelnia)
@@ -77,3 +78,31 @@ def test_eksport_pbn_editor_afiliacja_w_kontekscie_wydzialu(uczelnia, autor_jan_
     toplevel = []
     wydawnictwo_zwarte.eksport_pbn_other_editors(toplevel, w2, Wydawnictwo_Zwarte_Autor)
     assert toplevel[0].text == "0"
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [("s. 35", "35-35"),
+     ("ss. 90", "1-90"),
+     ("ss. 190", "1-190"),
+     ("ss. 290", "1-290"),
+     ("s. 10-20", "10-20"),
+     ("tia", None),
+     ("s. e27-e53", "e27-e53"),
+     ("[b.pag.]", "brak"),
+     ("[b. pag.]", "brak"),
+     ("[b. pag]", "brak"),
+     ("s. 132-153.", "132-153"),
+     ("s. 143.", "143-143"),
+     ("aosidfjoaisd fjoiasdjf s. 132-153.", "132-153"),
+     ("      s. 143.", "143-143"),
+     ("      s.          143.", "143-143"),
+     ("s. P-29.", "P-29"),
+     ("s. PP-4.", "PP-4"),
+     # ("s. P-27-P-28.", "P-27-P-28"), # to się nie uda
+     ("s. xiii-xiv.", "xiii-xiv")
+     ])
+def test_eksport_pbn_zakres_stron(input, expected, wydawnictwo_ciagle):
+    wydawnictwo_ciagle.szczegoly = input
+    ret = wydawnictwo_ciagle.eksport_pbn_zakres_stron()
+    assert ret == expected
