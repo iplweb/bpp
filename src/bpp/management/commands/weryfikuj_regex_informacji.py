bd494f606e94ae28591a914a55faebcc112637fc
Michał Pasternak <michal.dtz@gmail.com>
2017-02-20 23:47:53 +0100

    Pole „issue” i „volume” w eksporcie do PBN, czyli „tom” i „numer”. Regex dla ekstrakcji informacji + testy.

---
+++ src/bpp/management/commands/weryfikuj_regex_informacji.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+from django.core.management import BaseCommand
+
+from bpp.models.wydawnictwo_ciagle import Wydawnictwo_Ciagle, parsed_informacje_regex
+
+
+class Command(BaseCommand):
+    help = "Weryfikuje regex abstract.parsed_informacje_regex dla pola 'Informacje' wydawnictw ciągłych"
+
+    def handle(self, *args, **options):
+        q = Wydawnictwo_Ciagle.objects.exclude(informacje="").exclude(informacje=None).filter(rok__gte=2010)
+        total = q.count()
+        ret = []
+        cnt = 0
+        for elem in q.only("pk", "tytul_oryginalny", "informacje"):
+            parsed = parsed_informacje_regex.match(elem.informacje)
+            if parsed is None or parsed.groupdict()['rok'] is None:
+                print elem.pk, "|", elem.informacje, "|", elem.tytul_oryginalny
--- src/bpp/tests-pytest/test_models/test_wydawnictwo_ciagle.py
+++ src/bpp/tests-pytest/test_models/test_wydawnictwo_ciagle.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 
-import pytest
 import time
 
+import pytest
 from lxml import etree
 
 from bpp.admin.helpers import MODEL_PUNKTOWANY_KOMISJA_CENTRALNA, MODEL_PUNKTOWANY
@@ -11,10 +11,11 @@ from bpp.models.system import Status_Korekty
 
 
 @pytest.mark.django_db
-def test_models_wydawnictwo_ciagle_dirty_fields_ostatnio_zmieniony_dla_pbn(wydawnictwo_ciagle, wydawnictwo_zwarte, autor_jan_nowak, jednostka, statusy_korekt):
+def test_models_wydawnictwo_ciagle_dirty_fields_ostatnio_zmieniony_dla_pbn(wydawnictwo_ciagle, wydawnictwo_zwarte,
+                                                                           autor_jan_nowak, jednostka, statusy_korekt):
     Licencja_OpenAccess.objects.create(nazwa="lic 1 ", skrot="l1")
     Licencja_OpenAccess.objects.create(nazwa="lic 2 ", skrot="l2")
-    
+
     # Licencje muszą być w bazie, jakiekolwiek
     assert Licencja_OpenAccess.objects.all().first() != Licencja_OpenAccess.objects.all().last()
 
@@ -38,7 +39,7 @@ def test_models_wydawnictwo_ciagle_dirty_fields_ostatnio_zmieniony_dla_pbn(wydaw
 
         time.sleep(0.5)
 
-        for fld in MODEL_PUNKTOWANY_KOMISJA_CENTRALNA + MODEL_PUNKTOWANY + ('adnotacje', ):
+        for fld in MODEL_PUNKTOWANY_KOMISJA_CENTRALNA + MODEL_PUNKTOWANY + ('adnotacje',):
             setattr(wyd, fld, 123)
             wyd.save()
             assert ost_zm_pbn == wyd.ostatnio_zmieniony_dla_pbn
@@ -50,7 +51,7 @@ def test_models_wydawnictwo_ciagle_dirty_fields_ostatnio_zmieniony_dla_pbn(wydaw
         try:
             assert ost_zm_pbn != wyd.ostatnio_zmieniony_dla_pbn
         except TypeError:
-            pass # TypeError: can't compare offset-naive and offset-aware datetimes
+            pass  # TypeError: can't compare offset-naive and offset-aware datetimes
 
         time.sleep(0.5)
 
@@ -102,3 +103,48 @@ def test_export_pubmed_id(wydawnictwo_ciagle):
     import pytest
     with pytest.raises(IndexError):
         toplevel[0]
+
+
+@pytest.mark.parametrize("informacje,expected",
+                         [("bez sensu", None),
+                          ("2016 vol. 5 nr 10", "5"),
+                          ("2019 Bd 4 nr. 3 test", "4")])
+def test_eksport_pbn_get_volume(informacje, expected, wydawnictwo_ciagle):
+    wydawnictwo_ciagle.informacje = informacje
+    assert wydawnictwo_ciagle.eksport_pbn_get_volume() == expected
+
+
+def test_eksport_pbn_volume(wydawnictwo_ciagle):
+    toplevel = etree.fromstring("<body></body>")
+    wydawnictwo_ciagle.informacje = "bez sensu"
+    wydawnictwo_ciagle.eksport_pbn_volume(toplevel)
+    assert toplevel[0].text == "brak"
+
+    toplevel = etree.fromstring("<body></body>")
+    wydawnictwo_ciagle.informacje = "2016 vol 4"
+    wydawnictwo_ciagle.eksport_pbn_volume(toplevel)
+    assert toplevel[0].text == "4"
+
+
+@pytest.mark.parametrize("informacje,expected",
+                         [("bez sensu", None),
+                          ("2016 vol. 5 nr 10", "10"),
+                          ("2019 Bd 4 nr. 3 test", "3"),
+                          ("2019 Bd 4 nr 3 test", "3"),
+                          ("2019 Bd 4 z. 3 test", "3"),
+                          ("2019 Bd 4 h. 3 test", "3")])
+def test_eksport_pbn_get_issue(informacje, expected, wydawnictwo_ciagle):
+    wydawnictwo_ciagle.informacje = informacje
+    assert wydawnictwo_ciagle.eksport_pbn_get_issue() == expected
+
+
+def test_eksport_pbn_issue(wydawnictwo_ciagle):
+    toplevel = etree.fromstring("<body></body>")
+    wydawnictwo_ciagle.informacje = "2016 vol 4"
+    wydawnictwo_ciagle.eksport_pbn_issue(toplevel)
+    assert toplevel[0].text == "brak"
+
+    toplevel = etree.fromstring("<body></body>")
+    wydawnictwo_ciagle.informacje = "2016 vol 4 nr 5B"
+    wydawnictwo_ciagle.eksport_pbn_issue(toplevel)
+    assert toplevel[0].text == "5B"
