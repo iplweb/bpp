32316199fbefcc242a2513587a7b6b76b9a664e5
Michał Pasternak <michal.dtz@gmail.com>
2017-02-23 20:21:57 +0100

    Pchnij wspomagacze selenium do oddzielnego pliku

---
+++ src/django_bpp/selenium_util.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+import time
+
+from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def wait_for(condition_function):
+    start_time = time.time()
+    while time.time() < start_time + 10:
+        if condition_function():
+            return True
+        else:
+            time.sleep(0.1)
+    raise Exception(
+        'Timeout waiting for {}'.format(condition_function.__name__)
+    )
+
+
+class wait_for_page_load(object):
+    def __init__(self, browser):
+        self.browser = browser
+
+    def __enter__(self):
+        self.old_page = self.browser.find_by_tag('html')[0]._element
+
+    def __exit__(self, *_):
+        WebDriverWait(self.browser, 10).until(
+            staleness_of(self.old_page)
+        )
