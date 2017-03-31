c7cd396bad3a7e13368e45befc6346d0e0df6fbb
Michał Pasternak <michal.dtz@gmail.com>
2017-03-31 20:28:10 +0200

    Poprawione raporty autorów i jednostek, zgodnie z wytycznymi.

---
+++ src/bpp/migrations/0071_fix_liczba_znakow_wyd.sql
@@ -0,0 +1,75 @@
+BEGIN;
+
+DROP VIEW bpp_wydawnictwo_ciagle_view CASCADE;
+CREATE VIEW bpp_wydawnictwo_ciagle_view AS SELECT
+  django_content_type.id::text || '_' || bpp_wydawnictwo_ciagle.id::text AS fake_id,
+
+  django_content_type.id AS content_type_id,
+  bpp_wydawnictwo_ciagle.id AS object_id,
+
+  tytul_oryginalny,
+  tytul,
+  search_index,
+
+  rok,
+
+  jezyk_id,
+  typ_kbn_id,
+
+  charakter_formalny_id,
+
+  zrodlo_id,
+
+  NULL::text AS wydawnictwo,
+
+  slowa_kluczowe,
+  informacje,
+  szczegoly,
+  uwagi,
+
+  impact_factor,
+  punkty_kbn,
+  index_copernicus,
+  punktacja_wewnetrzna,
+
+  kc_impact_factor,
+  kc_punkty_kbn,
+  kc_index_copernicus,
+
+  adnotacje,
+
+  utworzono,
+  ostatnio_zmieniony,
+
+  tytul_oryginalny_sort,
+  opis_bibliograficzny_cache,
+  opis_bibliograficzny_autorzy_cache,
+  opis_bibliograficzny_zapisani_autorzy_cache,
+
+  afiliowana,
+  recenzowana,
+
+  liczba_znakow_wydawniczych,
+
+  www
+
+FROM
+  bpp_wydawnictwo_ciagle, django_content_type
+WHERE
+  django_content_type.app_label = 'bpp' AND
+  django_content_type.model = 'wydawnictwo_ciagle';
+
+
+CREATE VIEW bpp_rekord AS
+  SELECT * FROM bpp_wydawnictwo_ciagle_view
+    UNION
+      SELECT * FROM bpp_wydawnictwo_zwarte_view
+        UNION
+          SELECT * FROM bpp_patent_view
+            UNION
+              SELECT * FROM bpp_praca_doktorska_view
+                UNION
+                  SELECT * FROM bpp_praca_habilitacyjna_view;
+
+
+COMMIT;
--- src/bpp/tests-pytest/test_raporty.py
+++ src/bpp/tests-pytest/test_raporty.py
@@ -8,50 +8,6 @@ from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte
 from bpp.views.raporty.raport_aut_jed_common import get_base_query_autor, raport_autorow_tabela
 
 
-@pytest.mark.django_db
-def test_bug_raport_roczny_autorow_pkt_2_2_2_6_issue_441(autor_jan_kowalski, jednostka, obiekty_bpp):
-    # http://mantis.iplweb.pl/view.php?id=441
-
-    ksp = obiekty_bpp.charakter_formalny["KSP"]
-    pol = obiekty_bpp.jezyk["pol."]
-
-    praca_2_2 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksp, rok=2015)
-    praca_2_2.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="aut.")
-
-    praca_2_6 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksp, jezyk=pol, rok=2015)
-    praca_2_6.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="red.")
-
-    base_query = get_base_query_autor(autor=autor_jan_kowalski, rok_min=2015, rok_max=2015)
-
-    tabela_2_2 = raport_autorow_tabela("2_2", base_query, autor_jan_kowalski).count()
-    assert tabela_2_2 == 1
-
-    tabela_2_6 = raport_autorow_tabela("2_6", base_query, autor_jan_kowalski).count()
-    assert tabela_2_6 == 1
-
-
-@pytest.mark.django_db
-def test_bug_raport_roczny_autorow_pkt_2_1_2_5_issue_441(autor_jan_kowalski, jednostka, obiekty_bpp):
-    # http://mantis.iplweb.pl/view.php?id=441
-
-    ksz = obiekty_bpp.charakter_formalny["KSZ"]
-    ang = obiekty_bpp.jezyk["ang."]
-
-    praca_2_1 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksz, jezyk=ang, rok=2015)
-    praca_2_1.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="aut.")
-
-    praca_2_5 = mommy.make(Wydawnictwo_Zwarte, punkty_kbn=5, charakter_formalny=ksz, jezyk=ang, rok=2015)
-    praca_2_5.dodaj_autora(autor_jan_kowalski, jednostka, typ_odpowiedzialnosci_skrot="red.")
-
-    base_query = get_base_query_autor(autor=autor_jan_kowalski, rok_min=2015, rok_max=2015)
-
-    tabela_2_1 = raport_autorow_tabela("2_1", base_query, autor_jan_kowalski).count()
-    assert tabela_2_1 == 1
-
-    tabela_2_5 = raport_autorow_tabela("2_5", base_query, autor_jan_kowalski).count()
-    assert tabela_2_5 == 1
-
-
 def test_raport_autorow(app, autor_jan_kowalski, jednostka, obiekty_bpp):
     ksp = obiekty_bpp.charakter_formalny["KSP"]
     pol = obiekty_bpp.jezyk["pol."]
@@ -75,3 +31,9 @@ def test_raport_jednostek(app, autor_jan_kowalski, jednostka, obiekty_bpp):
 
     page = app.get(reverse("bpp:raport-jednostek", args=(jednostka.pk, 2015)))
     assert page.status_code == 200
+
+
+@pytest.mark.django_db
+def test_raport_aut_jed_1_4(obiekty_bpp, autor_jan_kowalski, jednostka):
+    base_query = get_base_query_autor(autor=autor_jan_kowalski, rok_min=0, rok_max=9999)
+    tabela_1_4 = raport_autorow_tabela("1_4", base_query, autor_jan_kowalski)
