ed9b22780c308f6e33f06e964cfcadf8eb10e034
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-04-27 08:42:16 +0200

    Używaj indeksu GIN zamiast GIST dla pełnotekstowego wyszukiwania

---
+++ src/bpp/migrations/0072_gist_to_gin.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bpp', '0071_fix_liczba_znakow_wyd'),
+    ]
+
+    operations = [
+        migrations.RunSQL("drop index bpp_rekord_mat_search_index_idx"),
+        migrations.RunSQL("CREATE INDEX bpp_rekord_mat_search_index_idx ON bpp_rekord_mat USING GIN(search_index)")
+    ]
