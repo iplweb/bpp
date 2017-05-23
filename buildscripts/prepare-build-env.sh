aeaa373878285e8bc3797e588fe1b80cd61e9eb2
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-05-24 00:37:32 +0200

    'yes' było zbędne

--- buildscripts/prepare-build-env.sh
+++ buildscripts/prepare-build-env.sh
@@ -11,5 +11,5 @@ cd $SCRIPTPATH/..
 yarn install
 
 rm -rf src/components/bower_components src/django_bpp/staticroot
-yes n | python src/manage.py bower_install -F
+python src/manage.py bower_install -F
 echo "2" |python src/manage.py bower install "jquery#2.1.4"
