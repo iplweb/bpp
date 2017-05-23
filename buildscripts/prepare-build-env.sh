30071dd3dde0a75558d0f756c2923449c2b07f63
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-05-23 08:12:22 +0200

    1) Pakiety globalnie instalowane powinny już być dostępne 2) przenieś budowanie css, html, js do innego pliku 3) opcja NO_REBUILD zbędna

--- buildscripts/prepare-build-env.sh
+++ buildscripts/prepare-build-env.sh
@@ -1,25 +1,6 @@
 #!/bin/bash -e
 
-#
 # Ten skrypt przygotowywuje do budowania django-bpp
-#
-
-NO_REBUILD=0
-
-while test $# -gt 0
-do
-    case "$1" in
-        --no-rebuild) NO_REBUILD=1
-            ;;
-	--help) echo "--no-rebuild"
-	    exit 1
-	    ;;
-        --*) echo "bad option $1"
-	    exit 1
-            ;;
-    esac
-    shift
-done
 
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
@@ -29,15 +10,8 @@ cd $SCRIPTPATH/..
 
 export PATH=$PATH:./node_modules/.bin/
 
-sudo npm install -g bower grunt-cli
-npm install grunt grunt-sass grunt-contrib-watch grunt-contrib-qunit 
+npm-cache install grunt grunt-sass grunt-contrib-watch grunt-contrib-qunit 
 
 rm -rf src/components/bower_components src/django_bpp/staticroot
 yes n | python src/manage.py bower_install -F
 echo "2" |python src/manage.py bower install "jquery#2.1.4"
-
-grunt build
-
-python src/manage.py collectstatic --noinput
-
-python src/manage.py compress --force
