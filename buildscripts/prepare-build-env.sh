8c094939b2b822e0d34dcf07ec7c7a2807970a17
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-05-23 02:13:39 +0200

    Nie trzeba tworzyć bazy danych przez compress

--- buildscripts/prepare-build-env.sh
+++ buildscripts/prepare-build-env.sh
@@ -40,17 +40,4 @@ grunt build
 
 python src/manage.py collectstatic --noinput
 
-
-if [ "$NO_REBUILD" == "0" ]; then
-    # Nie przebudowuj bazy danych 
-    dropdb --if-exists bpp
-    createdb bpp 
-    python src/manage.py migrate --noinput
-    dropdb --if-exists test_bpp
-    createdb test_bpp --template=bpp
-
-    export GIT_BRANCH_NAME=`git status |grep "On branch"|sed "s/On branch //"`
-    stellar replace $GIT_BRANCH_NAME || stellar snapshot $GIT_BRANCH_NAME
-fi
-
 python src/manage.py compress --force
