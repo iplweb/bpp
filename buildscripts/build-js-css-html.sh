f89f71b3f03d923248b3de1140697f7225ba8058
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-05-25 00:47:12 +0200

    Remove static root to make sure it gets rebuilt

--- buildscripts/build-js-css-html.sh
+++ buildscripts/build-js-css-html.sh
@@ -8,5 +8,6 @@ popd > /dev/null
 cd $SCRIPTPATH/..
 
 grunt build > /dev/null
+rm -rf $SCRIPTPATH/src/django_bpp/staticroot
 python src/manage.py collectstatic --noinput -v0
 python src/manage.py compress --force -v0
