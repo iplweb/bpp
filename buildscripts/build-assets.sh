3eb72aef26d90feca852dae02c78e17b86395f24
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-02 14:59:47 +0200

    Rebuild everything npm

--- buildscripts/build-assets.sh
+++ buildscripts/build-assets.sh
@@ -12,8 +12,10 @@ yarn install
 python src/manage.py bower_install -RF
 echo "2" |python src/manage.py bower install "jquery#2.1.4" -- --allow-root
 
-npm rebuild sass grunt-sass
+npm rebuild 
+
 grunt build 
+
 rm -rf $SCRIPTPATH/src/django_bpp/staticroot
 python src/manage.py collectstatic --noinput 
 python src/manage.py compress --force 
