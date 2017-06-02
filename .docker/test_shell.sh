5109578a3da71cfa4dc89f4a34e7be429d0af96c
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-02 21:50:50 +0200

    Enable xvfb for phantomjs

--- .docker/test_shell.sh
+++ .docker/test_shell.sh
@@ -4,4 +4,8 @@ export HOSTNAME=`hostname`
 export THIS_CONTAINER_IP=`cat /etc/hosts |grep $HOSTNAME|cut  -f 1`
 export DJANGO_LIVE_TEST_SERVER_ADDRESS=$THIS_CONTAINER_IP:9015
 
+export DISPLAY=:99
+
+Xvfb $DISPLAY &
+
 bash $*
