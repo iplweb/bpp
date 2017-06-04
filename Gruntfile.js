92a9ade2834e02844764553ffb3561e8a78890b1
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-04 15:22:51 +0200

    reformat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,39 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // pkg: grunt.file.readJSON('package.json'),
 
-
-module.exports = function(grunt) {
-  grunt.initConfig({
-	  // pkg: grunt.file.readJSON('package.json'),
-
-    sass: {
-      options: {
-        includePaths: ['src/django_bpp/staticroot/foundation-sites/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+        sass: {
+            options: {
+                includePaths: ['src/django_bpp/staticroot/foundation-sites/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'src/bpp/static/scss/app.css': 'src/bpp/static/scss/app.scss'
+                }
+            }
         },
-        files: {
-          'src/bpp/static/scss/app.css': 'src/bpp/static/scss/app.scss'
+
+        watch: {
+            grunt: {files: ['Gruntfile.js']},
+
+            sass: {
+                files: 'src/bpp/static/scss/*.scss',
+                tasks: ['sass']
+            }
+        },
+
+        qunit: {
+            all: ['src/notifications/static/notifications/js/tests/index.html']
         }
-      }
-    },
+    });
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
-      sass: {
-        files: 'src/bpp/static/scss/*.scss',
-        tasks: ['sass']
-      }
-    },
-
-    qunit: {
-      all: ['src/notifications/static/notifications/js/tests/index.html']
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+    grunt.registerTask('build', ['sass',]);
+    grunt.registerTask('default', ['build', 'watch']);
 }
