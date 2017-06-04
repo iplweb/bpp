316cb83bcf293103e23fdfea0dd3e671476869ad
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-04 21:18:40 +0200

    Remove proper path, print total size after building

--- buildscripts/build-assets.sh
+++ buildscripts/build-assets.sh
@@ -13,7 +13,7 @@ python src/manage.py bower_install -RF -v0
 
 npm rebuild > /dev/null
 
-rm -rf $SCRIPTPATH/src/django_bpp/staticroot
+rm -rf src/django_bpp/staticroot
 python src/manage.py collectstatic --noinput -v0
 
 grunt build 
@@ -21,3 +21,5 @@ grunt build
 python src/manage.py collectstatic --noinput -v0
 
 python src/manage.py compress --force  -v0
+echo -n "Static root size: "
+du -ch src/django_bpp/staticroot | grep total
