4ac68ada8a612a2ac8a5bf1b1e3e1c08a9971114
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-19 20:12:10 +0200

    1.10 fixes

--- conftest.py
+++ conftest.py
@@ -14,7 +14,8 @@ from bpp.models.patent import Patent
 from bpp.models.praca_doktorska import Praca_Doktorska
 from bpp.models.praca_habilitacyjna import Praca_Habilitacyjna
 from bpp.models.struktura import Uczelnia, Wydzial, Jednostka
-from bpp.models.system import Jezyk, Charakter_Formalny, Typ_KBN, Status_Korekty, Typ_Odpowiedzialnosci
+from bpp.models.system import Jezyk, Charakter_Formalny, Typ_KBN, \
+    Status_Korekty, Typ_Odpowiedzialnosci
 from bpp.models.wydawnictwo_ciagle import Wydawnictwo_Ciagle
 from bpp.models.wydawnictwo_zwarte import Wydawnictwo_Zwarte
 from bpp.models.zrodlo import Zrodlo
@@ -25,6 +26,7 @@ NORMAL_DJANGO_USER_PASSWORD = 'test_password'
 
 from django.conf import settings
 from bpp.tests.util import setup_mommy
+
 setup_mommy()
 
 
@@ -38,7 +40,8 @@ def rok():
 
 
 @pytest.fixture
-def normal_django_user(request, db, django_user_model):  # , django_username_field):
+def normal_django_user(request, db,
+                       django_user_model):  # , django_username_field):
     """
     A normal Django user
     """
@@ -56,7 +59,8 @@ def normal_django_user(request, db, django_user_model):  # , django_username_fie
     return obj
 
 
-def _preauth_session_id_helper(username, password, client, browser, live_server, django_user_model,
+def _preauth_session_id_helper(username, password, client, browser,
+                               live_server, django_user_model,
                                django_username_field):
     # Jeżeli poprzednia sesja sobie wisi np. na stronie wymagającej potwierdzenia
     # jej opuszczenia, to będzie problem, stąd:
@@ -73,33 +77,40 @@ def _preauth_session_id_helper(username, password, client, browser, live_server,
     browser.visit(live_server + reverse("login_form"))
     browser.fill('username', username)
     browser.fill('password', password)
-    with wait_for_page_load(browser):  # time.sleep(2)
+    with wait_for_page_load(browser):
         browser.find_by_css("input[type=submit]").click()
-    browser.authorized_user = django_user_model.objects.get(**{django_username_field: username})
+    browser.authorized_user = django_user_model.objects.get(
+        **{django_username_field: username})
     return browser
 
 
 @pytest.fixture
-def preauth_browser(normal_django_user, client, browser, live_server, django_user_model, django_username_field):
+def preauth_browser(normal_django_user, client, browser, live_server,
+                    django_user_model, django_username_field):
     return _preauth_session_id_helper(
         NORMAL_DJANGO_USER_LOGIN, NORMAL_DJANGO_USER_PASSWORD, client,
         browser, live_server, django_user_model, django_username_field)
 
 
 @pytest.fixture
-def preauth_admin_browser(admin_user, client, browser, live_server, django_user_model, django_username_field):
-    return _preauth_session_id_helper('admin', 'password', client, browser, live_server, django_user_model,
+def preauth_admin_browser(admin_user, client, browser, live_server,
+                          django_user_model, django_username_field):
+    return _preauth_session_id_helper('admin', 'password', client, browser,
+                                      live_server, django_user_model,
                                       django_username_field)
 
 
 @pytest.fixture
 def uczelnia(db):
-    return Uczelnia.objects.get_or_create(skrot='TE', nazwa='Testowa uczelnia')[0]
+    return \
+    Uczelnia.objects.get_or_create(skrot='TE', nazwa='Testowa uczelnia')[0]
 
 
 @pytest.mark.django_db
 def _wydzial_maker(nazwa, skrot, uczelnia, **kwargs):
-    return Wydzial.objects.get_or_create(uczelnia=uczelnia, skrot=skrot, nazwa=nazwa, **kwargs)[0]
+    return \
+    Wydzial.objects.get_or_create(uczelnia=uczelnia, skrot=skrot, nazwa=nazwa,
+                                  **kwargs)[0]
 
 
 @pytest.mark.django_db
@@ -111,12 +122,15 @@ def wydzial_maker(db):
 @pytest.mark.django_db
 @pytest.fixture(scope="function")
 def wydzial(uczelnia, db):
-    return _wydzial_maker(uczelnia=uczelnia, skrot='W1', nazwa=u'Wydział Testowy I')
+    return _wydzial_maker(uczelnia=uczelnia, skrot='W1',
+                          nazwa=u'Wydział Testowy I')
 
 
 def _autor_maker(imiona, nazwisko, tytul="dr. ", **kwargs):
     tytul = Tytul.objects.get_or_create(skrot=tytul, nazwa=tytul)[0]
-    return Autor.objects.get_or_create(tytul=tytul, imiona=imiona, nazwisko=nazwisko, **kwargs)[0]
+    return \
+    Autor.objects.get_or_create(tytul=tytul, imiona=imiona, nazwisko=nazwisko,
+                                **kwargs)[0]
 
 
 @pytest.fixture
@@ -137,36 +151,43 @@ def autor(db):
 
 @pytest.fixture(scope="function")
 def autor_jan_kowalski(db):
-    return _autor_maker(imiona="Jan", nazwisko="Kowalski", tytul="prof. dr hab. n. med.")
+    return _autor_maker(imiona="Jan", nazwisko="Kowalski",
+                        tytul="prof. dr hab. n. med.")
 
 
 def _jednostka_maker(nazwa, skrot, wydzial, **kwargs):
     return \
-        Jednostka.objects.get_or_create(nazwa=nazwa, skrot=skrot, wydzial=wydzial, uczelnia=wydzial.uczelnia, **kwargs)[
+        Jednostka.objects.get_or_create(nazwa=nazwa, skrot=skrot,
+                                        wydzial=wydzial,
+                                        uczelnia=wydzial.uczelnia, **kwargs)[
             0]
 
 
 @pytest.mark.django_db
 @pytest.fixture(scope="function")
 def jednostka(wydzial, db):
-    return _jednostka_maker("Jednostka Uczelni", skrot="Jedn. Ucz.", wydzial=wydzial)
+    return _jednostka_maker("Jednostka Uczelni", skrot="Jedn. Ucz.",
+                            wydzial=wydzial)
 
 
 @pytest.mark.django_db
 @pytest.fixture(scope="function")
 def druga_jednostka(wydzial, db):
-    return _jednostka_maker("Druga Jednostka Uczelni", skrot="Dr. Jedn. Ucz.", wydzial=wydzial)
+    return _jednostka_maker("Druga Jednostka Uczelni", skrot="Dr. Jedn. Ucz.",
+                            wydzial=wydzial)
 
 
 @pytest.mark.django_db
 @pytest.fixture(scope="function")
 def obca_jednostka(wydzial):
-    return _jednostka_maker("Obca Jednostka", skrot="OJ", wydzial=wydzial, skupia_pracownikow=False,
-                            zarzadzaj_automatycznie=False, widoczna=False, wchodzi_do_raportow=False)
+    return _jednostka_maker("Obca Jednostka", skrot="OJ", wydzial=wydzial,
+                            skupia_pracownikow=False,
+                            zarzadzaj_automatycznie=False, widoczna=False,
+                            wchodzi_do_raportow=False)
 
 
 @pytest.fixture
-def jednostka_maker(db):
+def jednostka_maker():
     return _jednostka_maker
 
 
@@ -175,7 +196,7 @@ def _zrodlo_maker(nazwa, skrot, **kwargs):
 
 
 @pytest.fixture
-def zrodlo_maker(db):
+def zrodlo_maker():
     return _zrodlo_maker
 
 
@@ -188,7 +209,7 @@ def set_default(varname, value, dct):
     if varname not in dct:
         dct[varname] = value
 
-
+@pytest.mark.django_db
 def _wydawnictwo_maker(klass, **kwargs):
     if 'rok' not in kwargs:
         kwargs['rok'] = rok()
@@ -213,7 +234,9 @@ def _wydawnictwo_maker(klass, **kwargs):
 
 def _wydawnictwo_ciagle_maker(**kwargs):
     if 'zrodlo' not in kwargs:
-        set_default('zrodlo', _zrodlo_maker(nazwa=u'Źrodło Ciągłego Wydawnictwa', skrot=u'Źród. Ciąg. Wyd.'), kwargs)
+        set_default('zrodlo',
+                    _zrodlo_maker(nazwa=u'Źrodło Ciągłego Wydawnictwa',
+                                  skrot=u'Źród. Ciąg. Wyd.'), kwargs)
 
     set_default('informacje', 'zrodlo-informacje', kwargs)
     set_default('issn', '123-IS-SN-34', kwargs)
@@ -221,15 +244,13 @@ def _wydawnictwo_ciagle_maker(**kwargs):
     return _wydawnictwo_maker(Wydawnictwo_Ciagle, **kwargs)
 
 
-@pytest.mark.django_db
-@pytest.fixture
 def wydawnictwo_ciagle_maker(db):
     return _wydawnictwo_ciagle_maker
 
-
 @pytest.fixture(scope="function")
-def wydawnictwo_ciagle(db):
-    return _wydawnictwo_ciagle_maker()
+def wydawnictwo_ciagle():
+    ret = _wydawnictwo_ciagle_maker()
+    return ret
 
 
 def _zwarte_base_maker(klass, **kwargs):
@@ -262,13 +283,15 @@ def zwarte_maker(db):
 
 
 def _habilitacja_maker(**kwargs):
-    Charakter_Formalny.objects.get_or_create(nazwa='habilitacja', skrot='H')
+    Charakter_Formalny.objects.get_or_create(nazwa='Praca habilitacyjna',
+                                             skrot='H')
     return _zwarte_base_maker(Praca_Habilitacyjna, **kwargs)
 
 
 @pytest.fixture(scope="function")
 def habilitacja(jednostka, db):
-    return _habilitacja_maker(tytul_oryginalny=u'Praca habilitacyjna', jednostka=jednostka)
+    return _habilitacja_maker(tytul_oryginalny=u'Praca habilitacyjna',
+                              jednostka=jednostka)
 
 
 @pytest.fixture
@@ -277,13 +300,15 @@ def habilitacja_maker(db):
 
 
 def _doktorat_maker(**kwargs):
-    Charakter_Formalny.objects.get_or_create(nazwa='Praca doktorska', skrot='D')
+    Charakter_Formalny.objects.get_or_create(nazwa='Praca doktorska',
+                                             skrot='D')
     return _zwarte_base_maker(Praca_Doktorska, **kwargs)
 
 
 @pytest.fixture(scope="function")
-def doktorat(jednostka, db):
-    return _doktorat_maker(tytul_oryginalny=u'Praca doktorska', jednostka=jednostka)
+def doktorat(jednostka):
+    return _doktorat_maker(tytul_oryginalny=u'Praca doktorska',
+                           jednostka=jednostka)
 
 
 @pytest.fixture
@@ -333,42 +358,20 @@ def _webtest_login(webtest_app, username, password, login_form='login_form'):
     form['username'] = username  # normal_django_user.username
     form['password'] = password  # NORMAL_DJANGO_USER_PASSWORD
     res = form.submit().follow()
-    assert res.context['user'].username == username  # normal_django_user.username
+    assert res.context[
+               'user'].username == username  # normal_django_user.username
     return webtest_app
 
 
 @pytest.fixture(scope='function')
 def app(webtest_app, normal_django_user):
-    return _webtest_login(webtest_app, NORMAL_DJANGO_USER_LOGIN, NORMAL_DJANGO_USER_PASSWORD)
+    return _webtest_login(webtest_app, NORMAL_DJANGO_USER_LOGIN,
+                          NORMAL_DJANGO_USER_PASSWORD)
 
 
 @pytest.fixture(scope='function')
 def admin_app(webtest_app, admin_user):
     return _webtest_login(webtest_app, 'admin', 'password')
-
-
-@pytest.fixture
-def jezyki(db):
-    Jezyk.objects.get_or_create(nazwa='angielski', skrot='ang.')
-    Jezyk.objects.get_or_create(nazwa='polski', skrot='pol.')
-    for elem in fixture("jezyk.json"):
-        Jezyk.objects.get_or_create(**elem['fields'])
-
-    return dict([(x.skrot, x) for x in Jezyk.objects.all()])
-
-
-@pytest.fixture
-def charaktery_formalne(db):
-    for elem in fixture("charakter_formalny.json"):
-        Charakter_Formalny.objects.get_or_create(**elem['fields'])
-    return dict([(x.skrot, x) for x in Charakter_Formalny.objects.all()])
-
-
-@pytest.fixture
-def typy_kbn(db):
-    for elem in fixture("typ_kbn.json"):
-        Typ_KBN.objects.get_or_create(**elem['fields'])
-    return dict([(x.skrot, x) for x in Typ_KBN.objects.all()])
 
 
 def fixture(name):
@@ -380,59 +383,64 @@ def fixture(name):
                     "src", "bpp", "fixtures", name)
             ), "r"))
 
+@pytest.fixture(scope='function')
+def standard_data():
+    s = time.time()
 
-@pytest.fixture
-def statusy_korekt(db):
+    pl, created = Jezyk.objects.get_or_create(
+        pk=1, skrot='pol.', nazwa='polski')
+    pl.skrot_dla_pbn = 'PL'
+    pl.save()
+    assert pl.pk == 1
+
+    ang, created = Jezyk.objects.get_or_create(
+        pk=2, skrot='ang.', nazwa='angielski')
+    ang.skrot_dla_pbn = 'EN'
+    ang.save()
+    assert ang.pk == 2
+
+    for elem in fixture("jezyk.json"):
+        Jezyk.objects.get_or_create(pk=elem['pk'], **elem['fields'])
+
+    for elem in fixture("charakter_formalny.json"):
+        Charakter_Formalny.objects.get_or_create(pk=elem['pk'], **elem['fields'])
+
+    for elem in fixture("typ_kbn.json"):
+        Typ_KBN.objects.get_or_create(pk=elem['pk'], **elem['fields'])
+
     for elem in fixture("status_korekty.json"):
-        Status_Korekty.objects.get_or_create(**elem['fields'])
+        Status_Korekty.objects.get_or_create(pk=elem['pk'], **elem['fields'])
 
-
-@pytest.fixture
-def funkcje_autorow(db):
     for elem in fixture("funkcja_autora.json"):
-        Funkcja_Autora.objects.get_or_create(**elem['fields'])
-    return Funkcja_Autora.objects.all()
+        Funkcja_Autora.objects.get_or_create(pk=elem['pk'], **elem['fields'])
 
-
-@pytest.fixture
-def tytuly(db):
     for elem in fixture("tytul.json"):
-        Tytul.objects.get_or_create(**elem['fields'])
-    return Tytul.objects.all()
+        Tytul.objects.get_or_create(pk=elem['pk'], **elem['fields'])
 
-
-@pytest.fixture
-def typy_odpowiedzialnosci(db):
     for elem in fixture("typ_odpowiedzialnosci.json"):
-        Typ_Odpowiedzialnosci.objects.get_or_create(**elem['fields'])
+        Typ_Odpowiedzialnosci.objects.get_or_create(pk=elem['pk'], **elem['fields'])
 
-
-@pytest.fixture
-def obiekty_bpp(typy_kbn, charaktery_formalne, jezyki, statusy_korekt, typy_odpowiedzialnosci):
-    class ObiektyBpp:
-        jezyk = jezyki
-        charakter_formalny = charaktery_formalne
-        typ_kbn = typy_kbn
-        typy_odpowiedzialnosci = Typ_Odpowiedzialnosci.objects.all()
-        status_korekty = Status_Korekty.objects.all()
-
-    return ObiektyBpp
+    e = time.time()
+    print "standard data took %.2f" % (e-s)
 
 
 @pytest.fixture(scope="function")
-def wydawnictwo_ciagle_z_autorem(wydawnictwo_ciagle, autor_jan_kowalski, jednostka):
+def wydawnictwo_ciagle_z_autorem(wydawnictwo_ciagle, autor_jan_kowalski,
+                                 jednostka):
     wydawnictwo_ciagle.dodaj_autora(autor_jan_kowalski, jednostka)
     return wydawnictwo_ciagle
 
 
 @pytest.fixture(scope="function")
-def wydawnictwo_zwarte_z_autorem(wydawnictwo_zwarte, autor_jan_kowalski, jednostka):
+def wydawnictwo_zwarte_z_autorem(wydawnictwo_zwarte, autor_jan_kowalski,
+                                 jednostka):
     wydawnictwo_zwarte.dodaj_autora(autor_jan_kowalski, jednostka)
     return wydawnictwo_zwarte
 
 
 @pytest.fixture(scope="function")
-def wydawnictwo_ciagle_z_dwoma_autorami(wydawnictwo_ciagle_z_autorem, autor_jan_nowak, jednostka):
+def wydawnictwo_ciagle_z_dwoma_autorami(wydawnictwo_ciagle_z_autorem,
+                                        autor_jan_nowak, jednostka):
     wydawnictwo_ciagle_z_autorem.dodaj_autora(autor_jan_nowak, jednostka)
     return wydawnictwo_ciagle_z_autorem
 
@@ -440,7 +448,8 @@ def wydawnictwo_ciagle_z_dwoma_autorami(wydawnictwo_ciagle_z_autorem, autor_jan_
 @pytest.fixture(scope="session")
 @pytest.mark.django_db
 def chf_ksp():
-    chf_ksp, created = Charakter_Formalny.objects.get_or_create(skrot='KSP', nazwa="Książka w języku polskim")
+    chf_ksp, created = Charakter_Formalny.objects.get_or_create(skrot='KSP',
+                                                                nazwa="Książka w języku polskim")
     if created:
         chf_ksp.ksiazka_pbn = True
         chf_ksp.save()
@@ -450,7 +459,8 @@ def chf_ksp():
 @pytest.fixture(scope="session")
 @pytest.mark.django_db
 def chf_roz():
-    chf_roz, created = Charakter_Formalny.objects.get_or_create(skrot='ROZ', nazwa="Rozdział książki")
+    chf_roz, created = Charakter_Formalny.objects.get_or_create(skrot='ROZ',
+                                                                nazwa="Rozdział książki")
     if created:
         chf_roz.rozdzial_pbn = True
         chf_roz.save()
@@ -470,6 +480,7 @@ def pytest_configure():
     Rekord._meta.managed = True
     Autorzy._meta.managed = True
 
+
 @pytest.fixture(scope="session")
 def splinter_driver_kwargs(splinter_webdriver):
     if splinter_webdriver == "remote":
@@ -483,10 +494,10 @@ def splinter_driver_kwargs(splinter_webdriver):
         chrome_op.add_argument("--enable-automation")
         chrome_op.add_argument("--start-maximized")
         chrome_op.add_experimental_option('prefs', {
-                'credentials_enable_service': False,
-                'profile': {
-                    'password_manager_enabled': False
-                    }
-                })
+            'credentials_enable_service': False,
+            'profile': {
+                'password_manager_enabled': False
+            }
+        })
         return {'browser': "chrome",
                 "desired_capabilities": chrome_op.to_capabilities()}
