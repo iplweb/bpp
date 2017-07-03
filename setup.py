c743ff31ea1296807d9ea843b81f2b1ae0024b3f
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-03 20:52:35 +0200

    Bump version: 0.10.102 → 0.10.103

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = list(parse_reqs("requirements_dev.txt")) + requirements
 
 setup(
     name='django-bpp',
-    version='0.10.102',
+    version='0.10.103',
     description="System informatyczny do zarządzania bibliografią publikacji pracowników naukowych",
     long_description=readme + '\n\n' + history,
     author="Michał Pasternak",
--- src/django_bpp/version.py
+++ src/django_bpp/version.py
@@ -1,4 +1,4 @@
-VERSION = "0.10.102"
+VERSION = "0.10.103"
 
 if __name__ == "__main__":
     import sys
