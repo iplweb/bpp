6eb29fb9fb5eac60064d4b4c166d6e88c0e6a560
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-03 17:35:41 +0200

    pip3

--- buildscripts/build-wheel.sh
+++ buildscripts/build-wheel.sh
@@ -9,8 +9,8 @@ export DISTDIR=$SCRIPTPATH/../dist
 echo "Buduje wheels w $DISTDIR"
 
 mkdir -p $DISTDIR
-pip2 wheel -q --wheel-dir=$DISTDIR --find-links=$DISTDIR -r requirements.txt 
+pip3 wheel -q --wheel-dir=$DISTDIR --find-links=$DISTDIR -r requirements.txt 
 
 export DISTDIR_DEV=${DISTDIR}_dev
 mkdir -p $DISTDIR_DEV
-pip2 wheel -q --wheel-dir=$DISTDIR_DEV --find-links=$DISTDIR --find-links=$DISTDIR_DEV -r requirements_dev.txt 
+pip3 wheel -q --wheel-dir=$DISTDIR_DEV --find-links=$DISTDIR --find-links=$DISTDIR_DEV -r requirements_dev.txt 
