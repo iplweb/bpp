b4cd1da6325b50e51137ecd55ffdbdb3349078ea
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-13 01:32:30 +0200

    Rozbij admina, wstępnie, na kilka plików

--- src/bpp/admin/konferencja.py
+++ src/bpp/admin/konferencja.py
@@ -1,2 +1,34 @@
 # -*- encoding: utf-8 -*-
 
+from django.contrib import admin
+
+from bpp.admin.helpers import ADNOTACJE_FIELDSET
+from bpp.models.konferencja import Konferencja
+
+from .common import CommitedModelAdmin
+
+class KonferencjaAdmin(CommitedModelAdmin):
+    list_display = ['nazwa', 'rozpoczecie', 'zakonczenie', 'miasto',
+                    'panstwo', 'baza_scopus', 'baza_wos']
+    list_filter = ['miasto', 'panstwo', 'rozpoczecie', 'zakonczenie',
+                   'baza_scopus', 'baza_wos', 'baza_inna']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'nazwa',
+                'skrocona_nazwa',
+                'rozpoczecie',
+                'zakonczenie',
+                'miasto',
+                'panstwo',
+                'baza_scopus',
+                'baza_wos',
+                'baza_inna')
+        }),
+        ADNOTACJE_FIELDSET
+    )
+
+    readonly_fields = ['ostatnio_zmieniony']
+    pass
+
+admin.site.register(Konferencja, KonferencjaAdmin)
--- src/bpp/models/konferencja.py
+++ src/bpp/models/konferencja.py
@@ -6,9 +6,16 @@ from bpp.models.abstract import ModelZAdnotacjami, ModelZNazwa
 
 
 class Konferencja(ModelZNazwa, ModelZAdnotacjami):
-    skrocona_nazwa = models.TextField(
+    nazwa = models.TextField(
+        max_length=512,
+        db_index=True)
+
+    skrocona_nazwa = models.CharField(
         "Skrócona nazwa",
-        max_length=250)
+        max_length=250,
+        null=True,
+        blank=True
+    )
 
     rozpoczecie = models.DateField(
         "Rozpoczęcie"
@@ -18,30 +25,41 @@ class Konferencja(ModelZNazwa, ModelZAdnotacjami):
         "Zakończenie"
     )
 
-    miasto = models.TextField(
+    miasto = models.CharField(
         "Miasto",
         max_length=100
     )
 
-    panstwo = models.TextField(
+    panstwo = models.CharField(
         "Państwo",
         max_length=100
     )
 
-    bazy_scopus = models.BooleanField(
+    baza_scopus = models.BooleanField(
         "Indeksowana w SCOPUS?",
         default=False
     )
 
-    bazy_wos = models.BooleanField(
+    baza_wos = models.BooleanField(
         "Indeksowana w WOS?",
         default=False
     )
 
-    baza_inna = models.TextField(
+    baza_inna = models.CharField(
         "Indeksowana w...",
+        max_length=200,
         help_text="Wpisz nazwę innej bazy w której indeksowana jest ta "
                   "konferencja",
         blank=True,
         null=True,
     )
+
+    class Meta:
+        unique_together = ('nazwa', 'rozpoczecie')
+        ordering = ('-rozpoczecie', 'nazwa')
+        verbose_name = 'konferencja'
+        verbose_name_plural = 'konferencje'
+
+    def __str__(self):
+        return self.nazwa
+
