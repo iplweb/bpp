56f702ae48f5f4d053f836f2d73654e423596519
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-14 09:50:09 +0200

    Uproszczony skrypt

--- src/bpp/management/commands/rebuild_cache.py
+++ src/bpp/management/commands/rebuild_cache.py
@@ -10,26 +10,6 @@ from bpp.models.cache import Rekord
 class Command(BaseCommand):
     help = 'Odbudowuje cache'
 
-    option_list = BaseCommand.option_list + (
-        make_option("--initial-offset", action="store", type="int", default=0),
-        make_option("--skip", action="store", type="int", default=0),
-        make_option("--pk", action="store", type="str", default=None)
-    )
-
     def handle(self, *args, **options):
-        if options['pk']:
-            Rekord.objects.get(pk=options['pk']).original.zaktualizuj_cache()
-            sys.exit(0)
-
-        skip = options['skip']
-        for r in Rekord.objects.all()[options['initial_offset']:].only(
-                "content_type_id", "object_id"):
-
-            if skip > 0:
-                skip -= 1
-                continue
-
+        for r in Rekord.objects.all().only("content_type_id", "object_id"):
             r.original.zaktualizuj_cache()
-
-            skip = options['skip']
-
