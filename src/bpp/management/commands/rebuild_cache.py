8c48fb326fd000cdc13ab8545c9748c9f3a917b6
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-14 15:06:46 +0200

    wielowątkowość

--- src/bpp/management/commands/rebuild_cache.py
+++ src/bpp/management/commands/rebuild_cache.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-from optparse import make_option
-import sys
 
 from django.core.management import BaseCommand
 
@@ -10,6 +8,21 @@ from bpp.models.cache import Rekord
 class Command(BaseCommand):
     help = 'Odbudowuje cache'
 
+    def add_arguments(self, parser):
+        parser.add_argument("--initial-offset", action="store", type=int,
+                            default=0)
+        parser.add_argument("--skip", action="store", type=int, default=0)
+
     def handle(self, *args, **options):
-        for r in Rekord.objects.all().only("content_type_id", "object_id"):
-            r.original.zaktualizuj_cache()
+        qset = Rekord.objects.all()[options['initial_offset']:]
+
+        action = True
+        for r in qset.only("content_type_id", "object_id"):
+            if action:
+                r.original.zaktualizuj_cache()
+                action = False
+                skip = options['skip']
+
+            skip -= 1
+            if skip == 0:
+                action = True
