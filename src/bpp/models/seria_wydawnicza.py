fb2c639381447cdfaa6a693992346d0ccf7f0e5f
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-20 00:20:35 +0200

    Zmień nazwę

---
+++ src/bpp/models/seria_wydawnicza.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+from bpp.models.abstract import ModelZNazwa, ModelZISSN
+
+
+class Seria_Wydawnicza(ModelZNazwa,
+                       ModelZISSN):
+
+    class Meta:
+        verbose_name_plural = "serie wydawnicze"
+        verbose_name = "seria wydawnicza"
+    pass
