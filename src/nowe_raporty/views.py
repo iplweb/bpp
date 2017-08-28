7928b57953573b4e88a2058090eed79d2d727890
Michał Pasternak <michal.dtz@gmail.com>
2017-08-29 01:53:06 +0200

    Generowanie odpowiedzi django

--- src/nowe_raporty/views.py
+++ src/nowe_raporty/views.py
@@ -1,13 +1,20 @@
-from django.conf import settings
-from django.http.response import HttpResponseRedirect
-from django.template.context import RequestContext
-from django.views.generic import FormView, TemplateView
-from django.views.generic.detail import DetailView
+# -*- encoding: utf-8 -*-
+
+import os
 
 from bpp.models.autor import Autor
 from bpp.models.cache import Rekord
 from bpp.models.struktura import Wydzial, Jednostka
-from flexible_reports.adapters import django_tables2
+from django.conf import settings
+from django.http.response import HttpResponse, FileResponse
+from django.http.response import HttpResponseRedirect
+from django.template.context import RequestContext
+from django.views.generic import FormView, TemplateView
+from django.views.generic.detail import DetailView
+from django_tables2.export.export import TableExport
+
+from flexible_reports.adapters.django_tables2 import as_docx, \
+    as_tablib_dataset, as_tablib_databook
 from .forms import AutorRaportForm
 from .forms import JednostkaRaportForm, WydzialRaportForm
 
@@ -54,8 +61,8 @@ class GenerujRaportBase(DetailView):
         if self.kwargs['od_roku'] == self.kwargs['do_roku']:
             return self.kwargs['od_roku']
         else:
-            return "%s-%s" %(self.kwargs['od_roku'],
-                             self.kwargs['do_roku'])
+            return "%s-%s" % (self.kwargs['od_roku'],
+                              self.kwargs['do_roku'])
 
     @property
     def title(self):
@@ -88,14 +95,49 @@ class GenerujRaportBase(DetailView):
         kwargs['form_title'] = self.form_title
         return super(GenerujRaportBase, self).get_context_data(**kwargs)
 
+    def as_docx_response(self, report, parent_context, filename=None):
+        data = as_docx(report, parent_context)
+
+        response = FileResponse(
+            data,
+            content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        )
+
+        if filename is None:
+            filename = report.title + ".docx"
+
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+            filename
+        )
+
+        response['Content-Length'] = os.stat(data.name).st_size
+        return response
+
+    def as_xlsx_response(self, report, parent_context, filename=None):
+        response = HttpResponse(
+            content_type=TableExport.FORMATS[TableExport.XLSX])
+        if filename is None:
+            filename = report.title + ".xlsx"
+
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+            filename
+        )
+
+        xlsx = as_tablib_databook(report, parent_context)
+        data = xlsx.export(TableExport.XLSX)
+        response['Content-Length'] = len(data)
+        response.write(data)
+        return response
+
     def render_to_response(self, context, **response_kwargs):
         _export = self.request.GET.get('_export')
 
         if _export in ('docx', 'xlsx'):
             context['request'] = self.request
             parent_context = RequestContext(self.request, context)
-            fun = getattr(django_tables2, "as_%s_response" % _export)
-            return fun(context['report'], parent_context, filename=self.title + "." + _export)
+            fun = getattr(self, "as_%s_response" % _export)
+            return fun(context['report'], parent_context,
+                       filename=self.title + "." + _export)
 
         return super(GenerujRaportBase, self).render_to_response(
             context, **response_kwargs)
