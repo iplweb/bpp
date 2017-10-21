2b6fd719cba3bf7e1ef5d81e14c76cdfb2aad363
Michał Pasternak <michal.dtz@gmail.com>
2017-10-22 00:45:45 +0200

    Use nginx by docker-compose

--- src/django_bpp/settings/test.py
+++ src/django_bpp/settings/test.py
@@ -12,6 +12,16 @@ SELENIUM_DRIVER = "Firefox"
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
+MEDIA_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..', 'media')
+)
+SENDFILE_ROOT = MEDIA_ROOT
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
+
+NOTIFICATIONS_HOST = 'nginx_http_push'
+NOTIFICATIONS_PORT = 80
