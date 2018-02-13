26a892c316aaf3a1efea1498aab25241c89a9483
Michał Pasternak <michal.dtz@gmail.com>
2018-02-13 21:00:18 +0100

    Mark flaky tests as flaky

--- src/bpp/tests_pytest/test_selenium/test_admin.py
+++ src/bpp/tests_pytest/test_selenium/test_admin.py
@@ -146,7 +146,7 @@ def test_admin_patent_toz(preauth_admin_browser, live_server):
     preauth_admin_browser.is_element_present_by_id('navigation-menu', 5000)
     assert wcc() == 2
 
-@flaky(max_runs=15)
+@flaky(max_runs=25)
 def test_admin_patent_tamze(preauth_admin_browser, live_server):
     c = any_patent(informacje="TO INFORMACJE")
     with wait_for_page_load(preauth_admin_browser):
@@ -408,6 +408,7 @@ def test_autorform_kasowanie_autora(autorform_browser, autorform_jednostka):
     autorform_browser.execute_script("window.onbeforeunload = function(e) {};")
 
 
+@flaky(max_runs=15)
 def test_bug_on_user_add(preauth_admin_browser, live_server):
     preauth_admin_browser.visit(live_server + reverse('admin:bpp_bppuser_add'))
     preauth_admin_browser.fill("username", "as")
