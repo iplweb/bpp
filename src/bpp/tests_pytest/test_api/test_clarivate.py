ea9d0f19a888c009547f328cdee0526037fd0fa5
Michał Pasternak <michal.dtz@gmail.com>
2018-04-20 16:48:28 +0200

    fix test

--- src/bpp/tests_pytest/test_api/test_clarivate.py
+++ src/bpp/tests_pytest/test_api/test_clarivate.py
@@ -1,8 +1,15 @@
+import pytest
+from django.core.exceptions import ImproperlyConfigured
 from django.urls import reverse
 from mock import Mock
 
 
 def test_Uczelnia_wosclient(uczelnia):
+    with pytest.raises(ImproperlyConfigured):
+        uczelnia.wosclient()
+
+    uczelnia.clarivate_username = "foo"
+    uczelnia.clarivate_password = "bar"
     res = uczelnia.wosclient()
     assert res is not None
 
@@ -26,7 +33,7 @@ def test_GetWoSAMRInformation_post_good(wd_app, uczelnia, mocker):
     assert res.json['timesCited'] == '-1'
 
 
-def test_GetWoSAMRInformation_post_error(wd_app, mocker):
+def test_GetWoSAMRInformation_post_error(wd_app, uczelnia, mocker):
     m = Mock()
     m.query_single = Mock(side_effect=Exception("lel"))
     fn = mocker.patch("bpp.models.struktura.Uczelnia.wosclient", return_value=m)
