07c8b64f722576bfbe2312d6dda0dcd175780ba6
Michał Pasternak <michal.dtz@gmail.com>
2018-05-14 11:32:36 +0200

    Konfiguracja wprowadzania powiązań
    * możliwość globalnej konfiguracji sposobu wprowadzania powiązań autorów z rekordami

--- src/django_bpp/settings/base.py
+++ src/django_bpp/settings/base.py
@@ -551,3 +551,8 @@ CRISPY_CLASS_CONVERTERS = {
 }
 
 SILENCED_SYSTEM_CHECKS = ["urls.W003"]
+
+# Sposób generowania inline dla powiązań rekordu autora z rekordem
+# publikacji
+
+INLINE_DLA_AUTOROW = os.getenv("DJANGO_BPP_INLINE_DLA_AUTOROW", "stacked")
