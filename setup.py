a5b2bc72cadc7d12225bfa7412ff489c0e2c1845
Michał Pasternak <michal.dtz@gmail.com>
2018-05-21 08:57:30 +0200

    Bump version: 1.0.19-dev → 1.0.19

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ test_requirements = list(parse_reqs("requirements_dev.txt")) + requirements
 
 setup(
     name='bpp',
-    version='1.0.19-dev',
+    version='1.0.19',
     description="System informatyczny do zarządzania bibliografią publikacji pracowników naukowych",
     long_description=readme + '\n\n' + history,
     author="Michał Pasternak",
