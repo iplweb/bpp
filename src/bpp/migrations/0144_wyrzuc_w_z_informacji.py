abeeddc8c76a9a393184989f6a40d19e2262fe17
Michał Pasternak <michal.dtz@gmail.com>
2018-06-29 18:29:06 +0200

    Przepisz migracje aby była w SQL nie w Djagno ROM
        # Ta migracja NIE może iść przez Django, musi być na poziomie SQL,
        # ponieważ faktyczna zmiana pola "Informacje" doprowadzi do zaktualizowania
        # daty w polu "Ostatnio zmieniony dla PBN" - jeżeli zrobimy to
        # przez Django. Jeżeli jednak zrobimy to przez SQL, to powinno
        # być w porządku...

--- src/bpp/migrations/0144_wyrzuc_w_z_informacji.py
+++ src/bpp/migrations/0144_wyrzuc_w_z_informacji.py
@@ -3,23 +3,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-from django.db.models import Q
-
-
-def wyrzuc_w_z_informacji(apps, schema_editor):
-    Wydawnictwo_Zwarte = apps.get_model("bpp", "Wydawnictwo_Zwarte")
-    for elem in Wydawnictwo_Zwarte.objects.filter(
-            Q(informacje__istartswith="w:") or
-            Q(informacje__istartswith="w :")):
-        l = elem.informacje.lower()
-        if l[:2] == "w:":
-            n = 2
-        if l[:3] == "w :":
-            n = 3
-
-        ni = elem.informacje[n:].strip()
-        elem.informacje = ni
-        elem.save()
 
 
 class Migration(migrations.Migration):
@@ -27,9 +10,70 @@ class Migration(migrations.Migration):
         ('bpp', '0143_uczelnia_wydruk_logo_szerokosc'),
     ]
 
+    # Ta migracja NIE może iść przez Django, musi być na poziomie SQL,
+    # ponieważ faktyczna zmiana pola "Informacje" doprowadzi do zaktualizowania
+    # daty w polu "Ostatnio zmieniony dla PBN" - jeżeli zrobimy to
+    # przez Django. Jeżeli jednak zrobimy to przez SQL, to powinno
+    # być w porządku...
+
     operations = [
-        migrations.RunPython(
-            wyrzuc_w_z_informacji,
-            migrations.RunPython.noop
-        )
+        migrations.RunSQL(
+            """
+            UPDATE bpp_wydawnictwo_zwarte SET 
+            informacje = TRIM(SUBSTR(informacje, 3))
+            WHERE informacje ILIKE 'W:%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_patent SET 
+            informacje = TRIM(SUBSTR(informacje, 3))
+            WHERE informacje ILIKE 'W:%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_praca_doktorska SET 
+            informacje = TRIM(SUBSTR(informacje, 3))
+            WHERE informacje ILIKE 'W:%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_praca_habilitacyjna SET 
+            informacje = TRIM(SUBSTR(informacje, 3))
+            WHERE informacje ILIKE 'W:%';
+            """,
+            migrations.RunSQL.noop),
+
+
+
+        migrations.RunSQL(
+            """
+            UPDATE bpp_wydawnictwo_zwarte SET 
+            informacje = TRIM(SUBSTR(informacje, 4))
+            WHERE informacje ILIKE 'W :%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_patent SET 
+            informacje = TRIM(SUBSTR(informacje, 4))
+            WHERE informacje ILIKE 'W :%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_praca_doktorska SET 
+            informacje = TRIM(SUBSTR(informacje, 4))
+            WHERE informacje ILIKE 'W :%';
+            """,
+            migrations.RunSQL.noop),
+        migrations.RunSQL(
+            """
+            UPDATE bpp_praca_habilitacyjna SET 
+            informacje = TRIM(SUBSTR(informacje, 4))
+            WHERE informacje ILIKE 'W :%';
+            """,
+            migrations.RunSQL.noop),
     ]
