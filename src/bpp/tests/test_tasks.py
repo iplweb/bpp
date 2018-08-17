73cbabf6f5b7d86df6716c17c285624427381b95
Michał Pasternak <michal.dtz@gmail.com>
2018-08-18 00:09:36 +0200

    tests_pytests i tests -> tests

--- src/bpp/tests/test_tasks.py
+++ src/bpp/tests/test_tasks.py
@@ -1,25 +1,39 @@
 # -*- encoding: utf-8 -*-
-from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
-from bpp.models import Rekord, Charakter_Formalny
-from bpp.models.cache import with_cache
-from bpp.tasks import zaktualizuj_opis
-from bpp.tests import any_ciagle
+
+from datetime import datetime, timedelta
+
+from mock import Mock
+from model_mommy import mommy
+
+from bpp.models import Wydawnictwo_Ciagle
+from celeryui.models import Report
+import pytest
+
+from bpp.tasks import remove_old_report_files, _zaktualizuj_liczbe_cytowan
+from django.utils import timezone
+
+@pytest.mark.django_db
+def test_remove_old_report_files():
+    mommy.make(Report)
+    r = mommy.make(Report)
+    r.started_on = timezone.now() - timedelta(days=20)
+    r.save()
+
+    assert Report.objects.all().count() == 2
+
+    remove_old_report_files()
+
+    assert Report.objects.all().count() == 1
 
 
-class TestTasks(TestCase):
-    def setUp(self):
-        Charakter_Formalny.objects.get_or_create(skrot='ZSZ', nazwa="Streszczenie zjazdowe konferencji międzynarodowej")
+@pytest.mark.django_db
+def test_zaktualizuj_liczbe_cytowan(uczelnia, wydawnictwo_ciagle, mocker):
 
-    def test_zaktualizuj_opis(self):
+    m = Mock()
+    m.query_multiple= Mock(return_value=[{wydawnictwo_ciagle.pk: {'timesCited': '31337'}}])
+    fn = mocker.patch("bpp.models.struktura.Uczelnia.wosclient", return_value=m)
 
-        c = any_ciagle(
-            charakter_formalny=Charakter_Formalny.objects.get(skrot='ZSZ'),
-            szczegoly='wtf-lol')
+    _zaktualizuj_liczbe_cytowan([Wydawnictwo_Ciagle,])
 
-        zaktualizuj_opis("bpp", "wydawnictwo_ciagle", c.pk)
-
-        self.assertEqual(
-            c.opis_bibliograficzny(),
-            Rekord.objects.all()[0].opis_bibliograficzny_cache
-        )
+    wydawnictwo_ciagle.refresh_from_db()
+    assert wydawnictwo_ciagle.liczba_cytowan == 31337
