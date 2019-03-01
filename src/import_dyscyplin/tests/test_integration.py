8a47b15913cf6d770151ad2639589a24c79fc02e
Michał Pasternak <michal.dtz@gmail.com>
2019-03-01 21:18:53 +0100

    Use nginx, stupid

--- src/import_dyscyplin/tests/test_integration.py
+++ src/import_dyscyplin/tests/test_integration.py
@@ -9,9 +9,9 @@ from django_bpp.selenium_util import wait_for_page_load
 
 
 @pytest.mark.django_db(transaction=True)
-def test_integracyjny(preauth_admin_browser, live_server):
+def test_integracyjny(preauth_admin_browser, nginx_live_server):
     mommy.make(Uczelnia)
-    preauth_admin_browser.visit(live_server + reverse("import_dyscyplin:index"))
+    preauth_admin_browser.visit(nginx_live_server + reverse("import_dyscyplin:index"))
 
     with wait_for_page_load(preauth_admin_browser):
         preauth_admin_browser.find_by_id("add-new-file").click()
