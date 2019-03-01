3ccee5d31898faac9c11e621536bd7dd49d2faec
Michał Pasternak <michal.dtz@gmail.com>
2019-03-01 20:27:36 +0100

    Fix tests, maybe

--- src/import_dyscyplin/tests/test_integration.py
+++ src/import_dyscyplin/tests/test_integration.py
@@ -1,11 +1,11 @@
+import os
+
 import pytest
-import time
 from django.urls import reverse
 from model_mommy import mommy
 
 from bpp.models import Uczelnia
 from django_bpp.selenium_util import wait_for_page_load
-from pathlib import Path
 
 
 @pytest.mark.django_db(transaction=True)
@@ -13,17 +13,13 @@ def test_integracyjny(preauth_admin_browser, live_server):
     mommy.make(Uczelnia)
     preauth_admin_browser.visit(live_server + reverse("import_dyscyplin:index"))
 
-    preauth_admin_browser.find_by_id("download-example-file").click()
-    time.sleep(2)
-
     with wait_for_page_load(preauth_admin_browser):
         preauth_admin_browser.find_by_id("add-new-file").click()
 
-    home = Path.home()
-    import os
-    os.system("find %s" % home)
-
-    preauth_admin_browser.find_by_id("id_plik").type(str(home / "Downloads/default.xlsx"))
+    preauth_admin_browser.find_by_id("id_plik").type(
+        os.path.join(
+            os.path.dirname(__file__),
+            "../static/import_dyscyplin/xlsx/default.xlsx"))
 
     with wait_for_page_load(preauth_admin_browser):
         preauth_admin_browser.find_by_id("id_submit").click()
