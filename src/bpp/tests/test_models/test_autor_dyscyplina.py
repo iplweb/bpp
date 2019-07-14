124be757a49d8a9081909c5cf1cc003a62d375e5
Michał Pasternak <michal.dtz@gmail.com>
2019-07-14 08:50:57 +0200

    Dodatkowe testy triggerów Autor_Dyscyplina

--- src/bpp/tests/test_models/test_autor_dyscyplina.py
+++ src/bpp/tests/test_models/test_autor_dyscyplina.py
@@ -5,7 +5,10 @@ from django.db import InternalError
 from bpp.models import Autor_Dyscyplina
 
 
-def test_autor_dyscyplina_save_ta_sama(autor_jan_kowalski, dyscyplina1, dyscyplina2, rok):
+def test_autor_dyscyplina_save_ta_sama_clean(autor_jan_kowalski, dyscyplina1, dyscyplina2, rok):
+    """Sprawdź funkcjonowanie triggera bazodanowego przy wrzuceniu tej samej
+        dyscypliny do Autor_Dyscyplina (czyli nie 'save' tylko testujemy trigger
+        po stronie bazy danych). """
     ad = Autor_Dyscyplina.objects.create(
         rok=rok,
         autor=autor_jan_kowalski,
@@ -17,6 +20,21 @@ def test_autor_dyscyplina_save_ta_sama(autor_jan_kowalski, dyscyplina1, dyscypli
 
     with pytest.raises(ValidationError):
         ad.clean()
+
+
+def test_autor_dyscyplina_save_ta_sama_trigger(autor_jan_kowalski, dyscyplina1, dyscyplina2, rok):
+    """Sprawdź funkcjonowanie triggera bazodanowego przy wrzuceniu tej samej
+        dyscypliny do Autor_Dyscyplina (czyli nie 'save' tylko testujemy trigger
+        po stronie bazy danych). """
+
+    with pytest.raises(InternalError):
+        Autor_Dyscyplina.objects.create(
+            rok=rok,
+            autor=autor_jan_kowalski,
+            dyscyplina_naukowa=dyscyplina1,
+            subdyscyplina_naukowa=dyscyplina1
+        )
+
 
 
 def test_autor_dyscyplina_procent_ponad(autor_jan_kowalski, dyscyplina1, dyscyplina2, rok):
@@ -32,8 +50,30 @@ def test_autor_dyscyplina_procent_ponad(autor_jan_kowalski, dyscyplina1, dyscypl
         ad.clean()
 
 
-def test_autor_dyscyplina_change_trigger_single(autor_jan_kowalski, jednostka, dyscyplina1, dyscyplina2, dyscyplina3,
-                                                rok, wydawnictwo_ciagle, wydawnictwo_zwarte, typy_odpowiedzialnosci):
+def test_autor_dyscyplina_dopisanie_regula_nr_1(
+        autor_jan_kowalski, dyscyplina1, dyscyplina2, wydawnictwo_ciagle, rok, jednostka, typy_odpowiedzialnosci
+):
+    wydawnictwo_ciagle.rok = rok
+    wydawnictwo_ciagle.save()
+
+    wca = wydawnictwo_ciagle.dodaj_autora(autor_jan_kowalski, jednostka, dyscyplina_naukowa=None)
+
+    Autor_Dyscyplina.objects.create(
+        autor=autor_jan_kowalski, rok=rok,
+        dyscyplina_naukowa=dyscyplina1,
+        subdyscyplina_naukowa=dyscyplina2
+    )
+
+    assert wca.dyscyplina_naukowa is None
+    wca.refresh_from_db()
+    assert wca.dyscyplina_naukowa is None
+
+
+def test_autor_dyscyplina_zmiana_dyscypliny_regula_2(
+        autor_jan_kowalski, jednostka, dyscyplina1, dyscyplina2,
+        dyscyplina3,
+        rok, wydawnictwo_ciagle, wydawnictwo_zwarte,
+        typy_odpowiedzialnosci):
     """Sprawdź, czy zmiana przypisania Autor_Dyscyplina na dany rok pociągnie zmianę w wydawnictwach 
     ciągłych, zwartych i patentach. Skasowanie przypisania z kolei ustawi przypisanie na NULL. """
 
@@ -79,6 +119,34 @@ def test_autor_dyscyplina_change_trigger_single(autor_jan_kowalski, jednostka, d
     assert wza.dyscyplina_naukowa == dyscyplina1
 
 
+def test_autor_dyscyplina_zmiana_subdyscypliny_na_pusta_regula_3(
+        autor_jan_kowalski, jednostka, dyscyplina1, dyscyplina2,
+        rok, wydawnictwo_ciagle, typy_odpowiedzialnosci):
+    """Sprawdź, czy zmiana przypisania Autor_Dyscyplina na dany rok pociągnie zmianę w wydawnictwach
+    ciągłych. Zmieniamy subdyscypline na pusta. Wydawnictwa powiazane maja miec ustawione
+    NULL"""
+
+    ad = Autor_Dyscyplina.objects.create(
+        rok=rok,
+        autor=autor_jan_kowalski,
+        dyscyplina_naukowa=dyscyplina1,
+        subdyscyplina_naukowa=dyscyplina2
+    )
+
+    wca = wydawnictwo_ciagle.dodaj_autora(
+        autor=autor_jan_kowalski,
+        jednostka=jednostka,
+        zapisany_jako="J. Kowalski",
+        dyscyplina_naukowa=dyscyplina2
+    )
+
+    ad.subdyscyplina_naukowa = None
+    ad.save()
+
+    wca.refresh_from_db()
+    assert wca.dyscyplina_naukowa == None
+
+
 def test_autor_dyscyplina_change_trigger_subdys_from_none_bug(
         autor_jan_kowalski, dyscyplina1, dyscyplina2, rok):
     """Sprawdź, czy zmiana przypisania Autor_Dyscyplina na dany rok pociągnie zmianę w wydawnictwach
@@ -93,6 +161,43 @@ def test_autor_dyscyplina_change_trigger_subdys_from_none_bug(
 
     ad.subdyscyplina_naukowa = dyscyplina2
     ad.save()  # Tu był bug przy zmianie z None na coś.
+
+
+def test_autor_dyscyplina_zmiana_dyscypliny_regula_4(
+        autor_jan_kowalski, jednostka, dyscyplina1, dyscyplina2,
+        rok, wydawnictwo_ciagle, wydawnictwo_zwarte,
+        typy_odpowiedzialnosci):
+    """Sprawdź, czy zmiana przypisania Autor_Dyscyplina na dany rok pociągnie zmianę w wydawnictwach
+    ciągłych, zwartych.
+
+    Skasowanie przypisania na dany rok ustawi dyscypliny na NULL. """
+
+    ad = Autor_Dyscyplina.objects.create(
+        rok=rok,
+        autor=autor_jan_kowalski,
+        dyscyplina_naukowa=dyscyplina1,
+        subdyscyplina_naukowa=dyscyplina2
+    )
+
+    wza = wydawnictwo_zwarte.dodaj_autora(
+        autor=autor_jan_kowalski,
+        jednostka=jednostka,
+        dyscyplina_naukowa=dyscyplina1
+    )
+
+    wca = wydawnictwo_ciagle.dodaj_autora(
+        autor=autor_jan_kowalski,
+        jednostka=jednostka,
+        dyscyplina_naukowa=dyscyplina2
+    )
+
+    ad.delete()
+
+    wca.refresh_from_db()
+    assert wca.dyscyplina_naukowa == None
+
+    wza.refresh_from_db()
+    assert wza.dyscyplina_naukowa == None
 
 
 def test_autor_dyscyplina_change_trigger_double(autor_jan_kowalski, jednostka, dyscyplina1, dyscyplina2, rok,
@@ -127,22 +232,9 @@ def test_autor_dyscyplina_change_trigger_double(autor_jan_kowalski, jednostka, d
 
     wca.refresh_from_db()
     assert wca.dyscyplina_naukowa == dyscyplina2
+
     wza.refresh_from_db()
     assert wza.dyscyplina_naukowa == dyscyplina1
-
-
-def test_autor_dyscyplina_taka_sama_trigger(autor_jan_kowalski, dyscyplina1, rok):
-    """Sprawdź funkcjonowanie triggera bazodanowego przy wrzuceniu tej samej
-    dyscypliny do Autor_Dyscyplina (czyli nie 'save' tylko testujemy trigger
-    po stronie bazy danych). """
-
-    with pytest.raises(InternalError):
-        Autor_Dyscyplina.objects.create(
-            rok=rok,
-            autor=autor_jan_kowalski,
-            dyscyplina_naukowa=dyscyplina1,
-            subdyscyplina_naukowa=dyscyplina1
-        )
 
 
 def test_autor_dyscyplina_zmiana_roku(autor_jan_kowalski, dyscyplina1, rok):
