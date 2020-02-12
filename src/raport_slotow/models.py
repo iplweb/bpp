5afb31823b38461900ca506547a6f175fbaf7d76
Michał Pasternak <michal.dtz@gmail.com>
2020-02-12 23:27:13 +0100

    Raport slotów "zerowy"

--- src/raport_slotow/models.py
+++ src/raport_slotow/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
 
-# Create your models here.
+from bpp.fields import YearField
+
+
+class RaportZerowyEntry(models.Model):
+    autor = models.ForeignKey("bpp.Autor", DO_NOTHING, primary_key=True)
+    rok = YearField()
+    dyscyplina_naukowa = models.ForeignKey("bpp.Dyscyplina_Naukowa", DO_NOTHING)
+
+    class Meta:
+        managed = False
+        ordering = (
+            "autor",
+            "rok",
+        )
--- src/raport_slotow/views.py
+++ src/raport_slotow/views.py
@@ -2,28 +2,43 @@ import urllib
 from datetime import datetime
 from urllib.parse import urlencode
 
-import django_filters
-from django.db.models import Window, Sum, F, Min, Max
-from django.forms import TextInput, NumberInput
-from django.http import HttpResponseRedirect, Http404
+from django.db.models import F, Max, Min, Sum, Window
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.utils import timezone
 from django.views.generic import FormView, TemplateView
 from django_filters.views import FilterView
-from django_tables2 import RequestConfig, MultiTableMixin, SingleTableMixin
-from future.backports.urllib.parse import urlencode
+from django_tables2 import MultiTableMixin, RequestConfig, SingleTableMixin
 
-from bpp.models import Autor, Cache_Punktacja_Autora_Query, Cache_Punktacja_Autora_Sum, \
-    Cache_Punktacja_Autora_Sum_Gruop, Dyscyplina_Naukowa, Cache_Punktacja_Autora_Sum_Ponizej, \
-    Cache_Punktacja_Autora_Sum_Group_Ponizej, Jednostka
+from bpp.models import (
+    Autor,
+    Cache_Punktacja_Autora_Query,
+    Cache_Punktacja_Autora_Sum,
+    Cache_Punktacja_Autora_Sum_Group_Ponizej,
+    Cache_Punktacja_Autora_Sum_Gruop,
+    Cache_Punktacja_Autora_Sum_Ponizej,
+)
+from bpp.models.cache import Autorzy
+from bpp.models.dyscyplina_naukowa import Autor_Dyscyplina
 from bpp.views.mixins import UczelniaSettingRequiredMixin
 from django_bpp.version import VERSION
-from raport_slotow.filters import RaportSlotowUczelniaFilter
+from raport_slotow.filters import RaportSlotowUczelniaFilter, RaportZerowyFilter
 from raport_slotow.forms import AutorRaportSlotowForm, ParametryRaportSlotowUczelniaForm
-from raport_slotow.tables import RaportSlotowAutorTable, RaportSlotowUczelniaTable
-from raport_slotow.util import create_temporary_table_as, MyExportMixin, MyTableExport, clone_temporary_table, \
-    insert_into
+from raport_slotow.models import RaportZerowyEntry
+from raport_slotow.tables import (
+    RaportSlotowAutorTable,
+    RaportSlotowUczelniaTable,
+    RaportSlotowZerowyTable,
+)
+from raport_slotow.util import (
+    MyExportMixin,
+    MyTableExport,
+    clone_temporary_table,
+    create_temporary_table_as,
+    insert_into,
+)
+
+from django.utils import timezone
 
 
 class WyborOsoby(UczelniaSettingRequiredMixin, FormView):
@@ -33,25 +48,32 @@ class WyborOsoby(UczelniaSettingRequiredMixin, FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Wybór autora'
+        context["title"] = "Wybór autora"
         return context
 
     def form_valid(self, form):
         """If the form is valid, redirect to the supplied URL."""
         return HttpResponseRedirect(
-            reverse("raport_slotow:raport",
-                    kwargs={"autor": form.cleaned_data['obiekt'].slug,
-                            "od_roku": form.cleaned_data['od_roku'],
-                            "do_roku": form.cleaned_data['do_roku'],
-                            }) + "?_export=" + form.cleaned_data['_export']
+            reverse(
+                "raport_slotow:raport",
+                kwargs={
+                    "autor": form.cleaned_data["obiekt"].slug,
+                    "od_roku": form.cleaned_data["od_roku"],
+                    "do_roku": form.cleaned_data["do_roku"],
+                },
+            )
+            + "?_export="
+            + form.cleaned_data["_export"]
         )
 
 
-class RaportSlotow(UczelniaSettingRequiredMixin, MyExportMixin, MultiTableMixin, TemplateView):
+class RaportSlotow(
+    UczelniaSettingRequiredMixin, MyExportMixin, MultiTableMixin, TemplateView
+):
     template_name = "raport_slotow/raport_slotow_autor.html"
     table_class = RaportSlotowAutorTable
     uczelnia_attr = "pokazuj_raport_slotow_autor"
-    export_formats = ['html', 'xlsx']
+    export_formats = ["html", "xlsx"]
 
     def create_export(self, export_format):
         tables = self.get_tables()
@@ -62,12 +84,12 @@ class RaportSlotow(UczelniaSettingRequiredMixin, MyExportMixin, MultiTableMixin,
             export_description=[
                 ("Nazwa raportu:", "raport slotów - autor"),
                 ("Autor:", str(self.autor)),
-                (f"Dyscyplina:", str(tables[n].dyscyplina_naukowa or 'żadna')),
+                (f"Dyscyplina:", str(tables[n].dyscyplina_naukowa or "żadna")),
                 (f"Od roku:", self.od_roku),
                 (f"Do roku:", self.do_roku),
                 ("Wygenerowano:", timezone.now()),
-                ("Wersja oprogramowania BPP", VERSION)
-            ]
+                ("Wersja oprogramowania BPP", VERSION),
+            ],
         )
         return exporter.response(filename=self.get_export_filename(export_format, n))
 
@@ -83,21 +105,29 @@ class RaportSlotow(UczelniaSettingRequiredMixin, MyExportMixin, MultiTableMixin,
             autor=self.autor,
             rekord__rok__gte=self.od_roku,
             rekord__rok__lte=self.do_roku,
-            pkdaut__gt=0)
+            pkdaut__gt=0,
+        )
 
         ret = []
-        for elem in cpaq.distinct('dyscyplina'):
+        for elem in cpaq.distinct("dyscyplina"):
             table_class = self.get_table_class()
             table = table_class(
-                data=cpaq.filter(dyscyplina_id=elem.dyscyplina_id).select_related("rekord", "dyscyplina"))
-            RequestConfig(self.request, paginate=self.get_table_pagination(table)).configure(table)
+                data=cpaq.filter(dyscyplina_id=elem.dyscyplina_id).select_related(
+                    "rekord", "dyscyplina"
+                )
+            )
+            RequestConfig(
+                self.request, paginate=self.get_table_pagination(table)
+            ).configure(table)
             table.dyscyplina_naukowa = elem.dyscyplina
             ret.append(table)
 
         if not ret:
             table_class = self.get_table_class()
             table = table_class(data=cpaq.select_related("rekord", "dyscyplina"))
-            RequestConfig(self.request, paginate=self.get_table_pagination(table)).configure(table)
+            RequestConfig(
+                self.request, paginate=self.get_table_pagination(table)
+            ).configure(table)
             table.dyscyplina_naukowa = None
             ret.append(table)
 
@@ -108,15 +138,13 @@ class RaportSlotow(UczelniaSettingRequiredMixin, MyExportMixin, MultiTableMixin,
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(RaportSlotow, self).get_context_data(**kwargs)
-        context['autor'] = self.autor
-        context['od_roku'] = self.od_roku
-        context['do_roku'] = self.do_roku
+        context["autor"] = self.autor
+        context["od_roku"] = self.od_roku
+        context["do_roku"] = self.do_roku
         return context
 
     def get_export_filename(self, export_format, n):
-        return f'raport_slotow_{self.autor.slug}_{self.od_roku}-{self.do_roku}-{n}.{export_format}'
-
-    pass
+        return f"raport_slotow_{self.autor.slug}_{self.od_roku}-{self.do_roku}-{n}.{export_format}"
 
 
 class ParametryRaportSlotowUczelnia(UczelniaSettingRequiredMixin, FormView):
@@ -126,38 +154,52 @@ class ParametryRaportSlotowUczelnia(UczelniaSettingRequiredMixin, FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Wybór roku'
+        context["title"] = "Wybór roku"
         return context
 
     def form_valid(self, form):
         return HttpResponseRedirect(
-            reverse("raport_slotow:raport-uczelnia") + '?' + urlencode(form.cleaned_data)
+            reverse("raport_slotow:raport-uczelnia")
+            + "?"
+            + urlencode(form.cleaned_data)
         )
 
 
-
-class RaportSlotowUczelnia(UczelniaSettingRequiredMixin, MyExportMixin, SingleTableMixin, FilterView):
+class RaportSlotowUczelnia(
+    UczelniaSettingRequiredMixin, MyExportMixin, SingleTableMixin, FilterView
+):
     template_name = "raport_slotow/raport_slotow_uczelnia.html"
     table_class = RaportSlotowUczelniaTable
     uczelnia_attr = "pokazuj_raport_slotow_uczelnia"
-    export_formats = ['html', 'xlsx']
+    export_formats = ["html", "xlsx"]
     filterset_class = RaportSlotowUczelniaFilter
 
     def get_table(self, **kwargs):
         table_class = self.get_table_class()
-        table = table_class(data=self.get_table_data(), od_roku=self.data['od_roku'], do_roku=self.data['do_roku'],
-                            **kwargs)
-        RequestConfig(self.request, paginate=self.get_table_pagination(table)).configure(table)
+        table = table_class(
+            data=self.get_table_data(),
+            od_roku=self.data["od_roku"],
+            do_roku=self.data["do_roku"],
+            **kwargs,
+        )
+        RequestConfig(
+            self.request, paginate=self.get_table_pagination(table)
+        ).configure(table)
         return table
 
     def get_export_description(self):
-        return [("Nazwa raportu:", "raport slotów - uczelnia"),
-                (f"Od roku:", self.data['od_roku']),
-                (f"Do roku:", self.data['do_roku']),
-                ("Minimalny slot:", self.data['minimalny_slot']),
-                ("Uwzględnij autorów poniżej minimalnego slotu:", "tak" if self.data['pokazuj_ponizej'] else "nie"),
-                ("Wygenerowano:", timezone.now()),
-                ("Wersja oprogramowania BPP", VERSION)]
+        return [
+            ("Nazwa raportu:", "raport slotów - uczelnia"),
+            (f"Od roku:", self.data["od_roku"]),
+            (f"Do roku:", self.data["do_roku"]),
+            ("Minimalny slot:", self.data["minimalny_slot"]),
+            (
+                "Uwzględnij autorów poniżej minimalnego slotu:",
+                "tak" if self.data["pokazuj_ponizej"] else "nie",
+            ),
+            ("Wygenerowano:", timezone.now()),
+            ("Wersja oprogramowania BPP", VERSION),
+        ]
 
     def get_form(self, dct):
         return ParametryRaportSlotowUczelniaForm(dct)
@@ -177,8 +219,10 @@ class RaportSlotowUczelnia(UczelniaSettingRequiredMixin, MyExportMixin, SingleTa
     def get_context_data(self, *args, **kwargs):
         context = super(RaportSlotowUczelnia, self).get_context_data(**kwargs)
         context.update(self.data)
-        context['data'] = self.data
-        context['export_link'] = urllib.parse.urlencode(dict(self.request.GET, **{"_export": "xlsx"}), doseq=True)
+        context["data"] = self.data
+        context["export_link"] = urllib.parse.urlencode(
+            dict(self.request.GET, **{"_export": "xlsx"}), doseq=True
+        )
         return context
 
     def get_export_filename(self, export_format):
@@ -186,63 +230,137 @@ class RaportSlotowUczelnia(UczelniaSettingRequiredMixin, MyExportMixin, SingleTa
         return f"raport_dyscyplin_{self.data['od_roku']}-{self.data['do_roku']}_{stamp}.{export_format}"
 
     def get_queryset(self):
-        self.min_slot = self.data['minimalny_slot']
+        self.min_slot = self.data["minimalny_slot"]
 
-        partition_by = [F('autor_id'), F('jednostka_id'), F('dyscyplina_id')]
-        order_by = "autor", "jednostka", "dyscyplina", (F('pkdaut') / F('slot')).desc()
-        group_by = 'autor_id', 'jednostka_id', 'dyscyplina_id',
+        partition_by = [F("autor_id"), F("jednostka_id"), F("dyscyplina_id")]
+        order_by = "autor", "jednostka", "dyscyplina", (F("pkdaut") / F("slot")).desc()
+        group_by = (
+            "autor_id",
+            "jednostka_id",
+            "dyscyplina_id",
+        )
         select_related = "autor", "jednostka", "dyscyplina"
 
-        qset1 = Cache_Punktacja_Autora_Query.objects.filter(
-            rekord__rok__gte=self.data['od_roku'],
-            rekord__rok__lte=self.data['do_roku'],
-            pkdaut__gt=0
-        ).annotate(
-            pkdautslot=F('pkdaut') / F('slot'),
-            pkdautsum=Window(
-                expression=Sum('pkdaut'),
-                partition_by=partition_by,
-                order_by=[(F('pkdaut') / F('slot')).desc(), "rekord__tytul_oryginalny", ],
-            ),
-            pkdautslotsum=Window(
-                expression=Sum('slot'),
-                partition_by=partition_by,
-                order_by=[(F('pkdaut') / F('slot')).desc(), "rekord__tytul_oryginalny", ]
+        qset1 = (
+            Cache_Punktacja_Autora_Query.objects.filter(
+                rekord__rok__gte=self.data["od_roku"],
+                rekord__rok__lte=self.data["do_roku"],
+                pkdaut__gt=0,
             )
-        ).order_by(*order_by)
-
+            .annotate(
+                pkdautslot=F("pkdaut") / F("slot"),
+                pkdautsum=Window(
+                    expression=Sum("pkdaut"),
+                    partition_by=partition_by,
+                    order_by=[
+                        (F("pkdaut") / F("slot")).desc(),
+                        "rekord__tytul_oryginalny",
+                    ],
+                ),
+                pkdautslotsum=Window(
+                    expression=Sum("slot"),
+                    partition_by=partition_by,
+                    order_by=[
+                        (F("pkdaut") / F("slot")).desc(),
+                        "rekord__tytul_oryginalny",
+                    ],
+                ),
+            )
+            .order_by(*order_by)
+        )
 
         create_temporary_table_as("bpp_temporary_cpaq", qset1)
 
-        pokazuj_ponizej = self.data['pokazuj_ponizej']
+        pokazuj_ponizej = self.data["pokazuj_ponizej"]
 
         if pokazuj_ponizej:
             # Stworz klona tabelki wynikowej dla autorów poniżej progu
             clone_temporary_table("bpp_temporary_cpaq", "bpp_temporary_cpaq_2")
 
         # Usuń wszystkie wyniki mniejsze od poszukiwanego minimalnego slotu
-        Cache_Punktacja_Autora_Sum.objects.filter(pkdautslotsum__lt=self.min_slot).delete()
+        Cache_Punktacja_Autora_Sum.objects.filter(
+            pkdautslotsum__lt=self.min_slot
+        ).delete()
 
         # Wrzuć do tabeli 'wyjściowej' najmniejsze wartości z tabeli sumowania
-        create_temporary_table_as("bpp_temporary_cpasg", Cache_Punktacja_Autora_Sum.objects.values(
-            *group_by
-        ).annotate(pkdautslotsum=Min('pkdautslotsum'), pkdautsum=Min('pkdautsum')).order_by())
+        create_temporary_table_as(
+            "bpp_temporary_cpasg",
+            Cache_Punktacja_Autora_Sum.objects.values(*group_by)
+            .annotate(pkdautslotsum=Min("pkdautslotsum"), pkdautsum=Min("pkdautsum"))
+            .order_by(),
+        )
 
         if pokazuj_ponizej:
             # Usuń wszystkie wyniki powjżej
-            Cache_Punktacja_Autora_Sum_Ponizej.objects.filter(pkdautslotsum__gte=self.min_slot).delete()
+            Cache_Punktacja_Autora_Sum_Ponizej.objects.filter(
+                pkdautslotsum__gte=self.min_slot
+            ).delete()
             # Wrzuć do tabelki grupowania najwyższe wyniki
-            insert_into("bpp_temporary_cpasg", Cache_Punktacja_Autora_Sum_Ponizej.objects.values(
-                *group_by
-            ).annotate(pkdautslotsum=Max('pkdautslotsum'), pkdautsum=Max('pkdautsum')).order_by())
+            insert_into(
+                "bpp_temporary_cpasg",
+                Cache_Punktacja_Autora_Sum_Ponizej.objects.values(*group_by)
+                .annotate(
+                    pkdautslotsum=Max("pkdautslotsum"), pkdautsum=Max("pkdautsum")
+                )
+                .order_by(),
+            )
 
             clone_temporary_table("bpp_temporary_cpasg", "bpp_temporary_cpasg_2")
             create_temporary_table_as(
                 "bpp_temporary_cpasg",
-                Cache_Punktacja_Autora_Sum_Group_Ponizej.objects.values(
-                    *group_by
-                ).annotate(pkdautslotsum=Max('pkdautslotsum'), pkdautsum=Max('pkdautsum')).order_by())
+                Cache_Punktacja_Autora_Sum_Group_Ponizej.objects.values(*group_by)
+                .annotate(
+                    pkdautslotsum=Max("pkdautslotsum"), pkdautsum=Max("pkdautsum")
+                )
+                .order_by(),
+            )
 
-        return Cache_Punktacja_Autora_Sum_Gruop.objects.all().annotate(
-            avg=F('pkdautsum') / F('pkdautslotsum')
-        ).select_related(*select_related)
+        return (
+            Cache_Punktacja_Autora_Sum_Gruop.objects.all()
+            .annotate(avg=F("pkdautsum") / F("pkdautslotsum"))
+            .select_related(*select_related)
+        )
+
+
+class RaportSlotowZerowy(
+    UczelniaSettingRequiredMixin, MyExportMixin, SingleTableMixin, FilterView
+):
+    """Pokazuje listę wszystkich autorów, którzy mają zadeklarowane dziedziny naukowe
+    ale nie posiadają slotów za konkretny rok.
+    """
+
+    template_name = "raport_slotow/raport_slotow_zerowy.html"
+    uczelnia_attr = "pokazuj_raport_slotow_autor"
+    filterset_class = RaportZerowyFilter
+    table_class = RaportSlotowZerowyTable
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(RaportSlotowZerowy, self).get_context_data(**kwargs)
+        context["export_link"] = urllib.parse.urlencode(
+            dict(self.request.GET, **{"_export": "xlsx"}), doseq=True
+        )
+        return context
+
+    def get_queryset(self):
+        # wartośći zadeklarowane w bazie danych
+        defined = (
+            Autor_Dyscyplina.objects.values("autor_id", "rok", "dyscyplina_naukowa_id")
+            .exclude(dyscyplina_naukowa_id=None)
+            .union(
+                Autor_Dyscyplina.objects.values(
+                    "autor_id", "rok", "subdyscyplina_naukowa_id"
+                ).exclude(subdyscyplina_naukowa_id=None)
+            )
+        )
+
+        # zestawy autor/rok/dyscyplina z całej bazy danych
+        existent = (
+            Autorzy.objects.all()
+            .values("autor_id", "rekord__rok", "dyscyplina_naukowa_id")
+            .exclude(dyscyplina_naukowa_id=None)
+            .distinct()
+        )
+
+        res = defined.difference(existent)
+        create_temporary_table_as("raport_slotow_raportzerowyentry", res)
+        return RaportZerowyEntry.objects.all()
