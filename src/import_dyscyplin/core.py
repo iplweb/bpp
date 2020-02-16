1b8ea0d37c3f5f3df11f36d7078917123c5b8288
Michał Pasternak <michal.dtz@gmail.com>
2020-02-16 22:58:08 +0100

    Napraw błąd gdzie są 2 jednostki o tej samej nazwie

--- src/import_dyscyplin/core.py
+++ src/import_dyscyplin/core.py
@@ -1,14 +1,18 @@
-from _decimal import InvalidOperation, Decimal
+from _decimal import Decimal, InvalidOperation
 from hashlib import md5
 
 import xlrd
+from django.core.exceptions import MultipleObjectsReturned
 from django.db.models import Q
 from xlrd import XLRDError
 
-from bpp.models import Wydzial, Jednostka, Autor, Autor_Jednostka
-from import_dyscyplin.exceptions import BadNoOfSheetsException, HeaderNotFoundException, ImproperFileException
+from bpp.models import Autor, Autor_Jednostka, Jednostka, Wydzial
+from import_dyscyplin.exceptions import (
+    BadNoOfSheetsException,
+    HeaderNotFoundException,
+    ImproperFileException,
+)
 from import_dyscyplin.models import Import_Dyscyplin_Row
-
 
 # naglowek = [
 #     "lp", "tytuł/stopień", "nazwisko", "imię", "pesel",
@@ -28,13 +32,17 @@ def matchuj_wydzial(nazwa):
 def matchuj_jednostke(nazwa):
     try:
         return Jednostka.objects.get(
-            Q(nazwa__iexact=nazwa.strip()) |
-            Q(skrot__iexact=nazwa.strip()))
+            Q(nazwa__iexact=nazwa.strip()) | Q(skrot__iexact=nazwa.strip())
+        )
+    except MultipleObjectsReturned:
+        return None
     except Jednostka.DoesNotExist:
         pass
 
 
-def matchuj_autora(imiona, nazwisko, jednostka=None, pesel_md5=None, orcid=None, tytul_str=None):
+def matchuj_autora(
+    imiona, nazwisko, jednostka=None, pesel_md5=None, orcid=None, tytul_str=None
+):
     if pesel_md5:
         try:
             return (Autor.objects.get(pesel_md5__iexact=pesel_md5.strip()), "")
@@ -47,10 +55,13 @@ def matchuj_autora(imiona, nazwisko, jednostka=None, pesel_md5=None, orcid=None,
         except Autor.DoesNotExist:
             pass
 
-    queries = [Q(
-        Q(nazwisko__iexact=nazwisko.strip()) | Q(poprzednie_nazwiska__icontains=nazwisko.strip()),
-        imiona__iexact=imiona.strip()
-    )]
+    queries = [
+        Q(
+            Q(nazwisko__iexact=nazwisko.strip())
+            | Q(poprzednie_nazwiska__icontains=nazwisko.strip()),
+            imiona__iexact=imiona.strip(),
+        )
+    ]
     if tytul_str:
         queries.append(queries[0] & Q(tytul__skrot=tytul_str))
 
@@ -78,17 +89,23 @@ def matchuj_autora(imiona, nazwisko, jednostka=None, pesel_md5=None, orcid=None,
 
     if jednostka:
 
-        queries = [Q(
-            Q(autor__nazwisko__iexact=nazwisko.strip()) | Q(autor__poprzednie_nazwiska__icontains=nazwisko.strip()),
-            autor__imiona__iexact=imiona.strip()
-        )]
+        queries = [
+            Q(
+                Q(autor__nazwisko__iexact=nazwisko.strip())
+                | Q(autor__poprzednie_nazwiska__icontains=nazwisko.strip()),
+                autor__imiona__iexact=imiona.strip(),
+            )
+        ]
         if tytul_str:
             queries.append(queries[0] & Q(autor__tytul__skrot=tytul_str))
 
         for qry in queries:
             try:
                 return (jednostka.autor_jednostka_set.get(qry).autor, "")
-            except (Autor_Jednostka.MultipleObjectsReturned, Autor_Jednostka.DoesNotExist):
+            except (
+                Autor_Jednostka.MultipleObjectsReturned,
+                Autor_Jednostka.DoesNotExist,
+            ):
                 pass
 
     return (None, "nie udało się dopasować")
@@ -111,8 +128,11 @@ def pesel_md5(value_from_xls):
     return md5(original_pesel).hexdigest()
 
 
-def znajdz_naglowek(sciezka, try_names=['imię', 'imie', 'imiona', 'nazwisko', 'nazwiska', 'orcid', 'pesel'],
-                    min_points=3):
+def znajdz_naglowek(
+    sciezka,
+    try_names=["imię", "imie", "imiona", "nazwisko", "nazwiska", "orcid", "pesel"],
+    min_points=3,
+):
     """
     :return: ([str, str...], no_row)
     """
@@ -125,7 +145,6 @@ def znajdz_naglowek(sciezka, try_names=['imię', 'imie', 'imiona', 'nazwisko', '
     if len(f.sheets()) != 1:
         raise BadNoOfSheetsException()
 
-    naglowek_row = None
     s = f.sheet_by_index(0)
 
     for n in range(s.nrows):
@@ -156,7 +175,6 @@ def przeanalizuj_plik_xls(sciezka, parent):
     if len(f.sheets()) != 1:
         raise BadNoOfSheetsException()
 
-    naglowek_row = None
     s = f.sheet_by_index(0)
 
     if parent.kolumna_set.all().count() == 0:
@@ -168,47 +186,49 @@ def przeanalizuj_plik_xls(sciezka, parent):
     jednostka_cache = {}
 
     for n in range(parent.wiersz_naglowka + 1, s.nrows):
-        original = dict(zip(
-            naglowek, [elem.value for elem in s.row(n)]
-        ))
+        original = dict(zip(naglowek, [elem.value for elem in s.row(n)]))
 
-        if not original['nazwisko'].strip():
+        if not original["nazwisko"].strip():
             continue
 
         try:
-            original['pesel_md5'] = pesel_md5(original['pesel'])
-            del original['pesel']
+            original["pesel_md5"] = pesel_md5(original["pesel"])
+            del original["pesel"]
         except KeyError:
-            original['pesel_md5'] = None
+            original["pesel_md5"] = None
 
-        original['nazwa_jednostki'] = original['nazwa jednostki']  # templatka wymaga
+        original["nazwa_jednostki"] = original["nazwa jednostki"]  # templatka wymaga
 
-        wydzial = wydzial_cache.get(original['wydział'])
+        wydzial = wydzial_cache.get(original["wydział"])
         if not wydzial:
-            wydzial = wydzial_cache[original['wydział']] = matchuj_wydzial(original['wydział'])
+            wydzial = wydzial_cache[original["wydział"]] = matchuj_wydzial(
+                original["wydział"]
+            )
 
-        jednostka = jednostka_cache.get(original['nazwa jednostki'])
+        jednostka = jednostka_cache.get(original["nazwa jednostki"])
         if not jednostka:
-            jednostka = jednostka_cache[original['nazwa jednostki']] = matchuj_jednostke(original['nazwa jednostki'])
+            jednostka = jednostka_cache[
+                original["nazwa jednostki"]
+            ] = matchuj_jednostke(original["nazwa jednostki"])
 
         autor, info = matchuj_autora(
-            original['imię'],
-            original['nazwisko'],
+            original["imię"],
+            original["nazwisko"],
             jednostka=jednostka,
-            pesel_md5=original.get('pesel_md5', None),
-            orcid=original.get('orcid', None),
-            tytul_str=original['tytuł']
+            pesel_md5=original.get("pesel_md5", None),
+            orcid=original.get("orcid", None),
+            tytul_str=original["tytuł"],
         )
 
         bledny = False
 
-        for elem in ['procent dyscypliny', 'procent subdyscypliny']:
+        for elem in ["procent dyscypliny", "procent subdyscypliny"]:
             v = original.get(elem)
 
             if v is None:
                 continue
 
-            if str(v) == '':
+            if str(v) == "":
                 original[elem] = None
                 continue
 
@@ -223,22 +243,20 @@ def przeanalizuj_plik_xls(sciezka, parent):
             row_no=n,
             parent=parent,
             original=original,
-            nazwisko=original['nazwisko'],
-            imiona=original['imię'],
-            nazwa_jednostki=original.get('nazwa_jednostki'),
+            nazwisko=original["nazwisko"],
+            imiona=original["imię"],
+            nazwa_jednostki=original.get("nazwa_jednostki"),
             jednostka=jednostka,
-            nazwa_wydzialu=original.get('wydział'),
+            nazwa_wydzialu=original.get("wydział"),
             wydzial=wydzial,
             autor=autor,
             info=info,
-
-            dyscyplina=original.get('dyscyplina'),
-            kod_dyscypliny=original.get('kod dyscypliny'),
-            procent_dyscypliny=original.get('procent dyscypliny'),
-
-            subdyscyplina=original.get('subdyscyplina'),
-            kod_subdyscypliny=original.get('kod subdyscypliny'),
-            procent_subdyscypliny=original.get('procent subdyscypliny')
+            dyscyplina=original.get("dyscyplina"),
+            kod_dyscypliny=original.get("kod dyscypliny"),
+            procent_dyscypliny=original.get("procent dyscypliny"),
+            subdyscyplina=original.get("subdyscyplina"),
+            kod_subdyscypliny=original.get("kod subdyscypliny"),
+            procent_subdyscypliny=original.get("procent subdyscypliny"),
         )
 
         if bledny:
