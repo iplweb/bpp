12665f646b37341d3aa38a567cf6789444dbbcce
Michał Pasternak <michal.dtz@gmail.com>
2020-03-18 00:12:03 +0100

    Fix build

--- src/raport_slotow/tests/test_raport_slotow_ewaluacja.py
+++ src/raport_slotow/tests/test_raport_slotow_ewaluacja.py
@@ -11,24 +11,21 @@ def test_raport_slotow_ewaluacja_parametry_view(admin_client):
 
 def test_raport_slotow_ewaluacja_parametry_view_post(admin_client):
     res = admin_client.post(
-        reverse("raport_slotow:index-ewaluacja"),
-        {"od_roku": 2020, "do_roku": 2030, "_export": "html"},
+        reverse("raport_slotow:index-ewaluacja"), {"rok": 2020, "_export": "html"},
     )
     assert res.status_code == 302
 
 
 def test_raport_slotow_ewaluacja_raport(admin_client, praca_z_dyscyplina):
     res = admin_client.get(
-        reverse("raport_slotow:raport-ewaluacja")
-        + "?_export=html&od_roku=2000&do_roku=3000"
+        reverse("raport_slotow:raport-ewaluacja") + "?_export=html&rok=2020"
     )
     assert res.status_code == 200
 
 
 def test_raport_slotow_ewaluacja_raport_xlsx(admin_client, praca_z_dyscyplina):
     res = admin_client.get(
-        reverse("raport_slotow:raport-ewaluacja")
-        + "?_export=xlsx&od_roku=2000&do_roku=3000"
+        reverse("raport_slotow:raport-ewaluacja") + "?_export=xlsx&rok=2020"
     )
     assert res.status_code == 200
 
