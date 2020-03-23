555395dd7bc326a2ad5b12770c2776cbdff26ba6
Michał Pasternak <michal.dtz@gmail.com>
2020-03-23 20:42:01 +0100

    Nie pozwalaj na skasowanie aktualnego konta + ostatniego konta administratora;
    reformat

--- src/bpp/admin/__init__.py
+++ src/bpp/admin/__init__.py
@@ -6,19 +6,35 @@ from multiseek.models import SearchForm
 
 from bpp.models import Rodzaj_Prawa_Patentowego, Zewnetrzna_Baza_Danych
 # Proste tabele
-from bpp.models.openaccess import Tryb_OpenAccess_Wydawnictwo_Ciagle, Tryb_OpenAccess_Wydawnictwo_Zwarte, \
-    Czas_Udostepnienia_OpenAccess, Licencja_OpenAccess, Wersja_Tekstu_OpenAccess
+from bpp.models.openaccess import (
+    Tryb_OpenAccess_Wydawnictwo_Ciagle,
+    Tryb_OpenAccess_Wydawnictwo_Zwarte,
+    Czas_Udostepnienia_OpenAccess,
+    Licencja_OpenAccess,
+    Wersja_Tekstu_OpenAccess,
+)
 from . import zrodlo
 from .autor import AutorAdmin  # noqa
 from .charakter_formalny import Charakter_FormalnyAdmin  # noqa
-from .core import BaseBppAdmin, CommitedModelAdmin, \
-    KolumnyZeSkrotamiMixin, generuj_inline_dla_autorow
-from .core import RestrictDeletionToAdministracjaGroupAdmin, \
-    RestrictDeletionToAdministracjaGroupMixin
+from .core import (
+    BaseBppAdmin,
+    CommitedModelAdmin,
+    KolumnyZeSkrotamiMixin,
+    generuj_inline_dla_autorow,
+)
+from .core import (
+    RestrictDeletionToAdministracjaGroupAdmin,
+    RestrictDeletionToAdministracjaGroupMixin,
+)
 from .dyscyplina_naukowa import Dyscyplina_NaukowaAdmin  # noqa
-from .filters import LiczbaZnakowFilter, CalkowitaLiczbaAutorowFilter, \
-    JednostkaFilter, PBNIDObecnyFilter, \
-    PeselMD5ObecnyFilter, OrcidObecnyFilter
+from .filters import (
+    LiczbaZnakowFilter,
+    CalkowitaLiczbaAutorowFilter,
+    JednostkaFilter,
+    PBNIDObecnyFilter,
+    PeselMD5ObecnyFilter,
+    OrcidObecnyFilter,
+)
 from .helpers import *
 from .jednostka import JednostkaAdmin  # NOQA
 from .konferencja import KonferencjaAdmin  # noqa
@@ -31,19 +47,27 @@ from .wydawca import WydawcaAdmin  # noqa
 from .wydawnictwo_ciagle import Wydawnictwo_CiagleAdmin
 from .wydawnictwo_zwarte import Wydawnictwo_ZwarteAdmin_Baza, Wydawnictwo_ZwarteAdmin
 from .wydzial import WydzialAdmin
-from ..models import Jezyk, Typ_KBN, Tytul, Funkcja_Autora, Rodzaj_Zrodla, \
-    Typ_Odpowiedzialnosci, Status_Korekty, \
-    Zrodlo_Informacji, BppUser  # Publikacja_Habilitacyjna
+from ..models import (
+    Jezyk,
+    Typ_KBN,
+    Tytul,
+    Funkcja_Autora,
+    Rodzaj_Zrodla,
+    Typ_Odpowiedzialnosci,
+    Status_Korekty,
+    Zrodlo_Informacji,
+    BppUser,
+)  # Publikacja_Habilitacyjna
 from ..models.nagroda import OrganPrzyznajacyNagrody
 from ..models.system import Charakter_PBN
 
 
 class JezykAdmin(RestrictDeletionToAdministracjaGroupAdmin):
-    list_display = ['nazwa', 'skrot', 'skrot_dla_pbn']
+    list_display = ["nazwa", "skrot", "skrot_dla_pbn"]
 
 
 class Funkcja_AutoraAdmin(RestrictDeletionToAdministracjaGroupAdmin):
-    list_display = ['nazwa', 'skrot']
+    list_display = ["nazwa", "skrot"]
 
 
 admin.site.register(Jezyk, JezykAdmin)
@@ -53,83 +77,105 @@ admin.site.register(Status_Korekty, RestrictDeletionToAdministracjaGroupAdmin)
 admin.site.register(Zrodlo_Informacji, RestrictDeletionToAdministracjaGroupAdmin)
 admin.site.register(Rodzaj_Prawa_Patentowego, RestrictDeletionToAdministracjaGroupAdmin)
 
-admin.site.register(OrganPrzyznajacyNagrody,
-                    RestrictDeletionToAdministracjaGroupAdmin)
+admin.site.register(OrganPrzyznajacyNagrody, RestrictDeletionToAdministracjaGroupAdmin)
 
 
 @admin.register(Zewnetrzna_Baza_Danych)
-class Zewnetrzna_Baza_DanychAdmin(RestrictDeletionToAdministracjaGroupAdmin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Zewnetrzna_Baza_DanychAdmin(
+    RestrictDeletionToAdministracjaGroupAdmin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
-class Charakter_PBNAdmin(RestrictDeletionToAdministracjaGroupMixin,
-                         CommitedModelAdmin):
-    list_display = ['identyfikator', 'wlasciwy_dla', 'opis',
-                    'charaktery_formalne',
-                    'typy_kbn']
-    readonly_fields = ['identyfikator', 'wlasciwy_dla', 'opis', 'help_text']
+class Charakter_PBNAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
+    list_display = [
+        "identyfikator",
+        "wlasciwy_dla",
+        "opis",
+        "charaktery_formalne",
+        "typy_kbn",
+    ]
+    readonly_fields = ["identyfikator", "wlasciwy_dla", "opis", "help_text"]
 
     def charaktery_formalne(self, rec):
-        return ", ".join(["%s (%s)" % (x.nazwa, x.skrot) for x in
-                          rec.charakter_formalny_set.all()])
+        return ", ".join(
+            ["%s (%s)" % (x.nazwa, x.skrot) for x in rec.charakter_formalny_set.all()]
+        )
 
     def typy_kbn(self, rec):
-        return ", ".join(["%s (%s)" % (x.nazwa, x.skrot) for x in
-                          rec.typ_kbn_set.all()])
+        return ", ".join(
+            ["%s (%s)" % (x.nazwa, x.skrot) for x in rec.typ_kbn_set.all()]
+        )
 
 
 admin.site.register(Charakter_PBN, Charakter_PBNAdmin)
 
 
 class NazwaISkrotAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['skrot', 'nazwa']
-    search_fields = ['skrot', 'nazwa']
+    list_display = ["skrot", "nazwa"]
+    search_fields = ["skrot", "nazwa"]
 
 
 admin.site.register(Tytul, NazwaISkrotAdmin)
 
 
 class Typ_KBNAdmin(RestrictDeletionToAdministracjaGroupAdmin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot', 'artykul_pbn', 'charakter_pbn']
+    list_display = ["nazwa", "skrot", "artykul_pbn", "charakter_pbn"]
 
 
 admin.site.register(Typ_KBN, Typ_KBNAdmin)
 
 
-class Typ_OdpowiedzialnosciAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot', 'typ_ogolny']
+class Typ_OdpowiedzialnosciAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot", "typ_ogolny"]
 
 
-class Tryb_OpenAccess_Wydawnictwo_CiagleAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Tryb_OpenAccess_Wydawnictwo_CiagleAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
-admin.site.register(Tryb_OpenAccess_Wydawnictwo_Ciagle, Tryb_OpenAccess_Wydawnictwo_CiagleAdmin)
+admin.site.register(
+    Tryb_OpenAccess_Wydawnictwo_Ciagle, Tryb_OpenAccess_Wydawnictwo_CiagleAdmin
+)
 
 
-class Tryb_OpenAccess_Wydawnictwo_ZwarteAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Tryb_OpenAccess_Wydawnictwo_ZwarteAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
-admin.site.register(Tryb_OpenAccess_Wydawnictwo_Zwarte, Tryb_OpenAccess_Wydawnictwo_ZwarteAdmin)
+admin.site.register(
+    Tryb_OpenAccess_Wydawnictwo_Zwarte, Tryb_OpenAccess_Wydawnictwo_ZwarteAdmin
+)
 
 
-class Czas_Udostepnienia_OpenAccessAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Czas_Udostepnienia_OpenAccessAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
 admin.site.register(Czas_Udostepnienia_OpenAccess, Czas_Udostepnienia_OpenAccessAdmin)
 
 
-class Licencja_OpenAccessAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Licencja_OpenAccessAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
 admin.site.register(Licencja_OpenAccess, Licencja_OpenAccessAdmin)
 
 
-class Wersja_Tekstu_OpenAccessAdmin(RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin):
-    list_display = ['nazwa', 'skrot']
+class Wersja_Tekstu_OpenAccessAdmin(
+    RestrictDeletionToAdministracjaGroupMixin, CommitedModelAdmin
+):
+    list_display = ["nazwa", "skrot"]
 
 
 admin.site.register(Wersja_Tekstu_OpenAccess, Wersja_Tekstu_OpenAccessAdmin)
@@ -156,18 +202,38 @@ class BppUserCreationForm(UserCreationForm):
         except BppUser.DoesNotExist:
             return username
         raise forms.ValidationError(
-            self.error_messages['duplicate_username'],
-            code='duplicate_username',
+            self.error_messages["duplicate_username"], code="duplicate_username",
         )
 
 
 class BppUserAdmin(UserAdmin):
     list_display = (
-        'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser', 'lista_grup')
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "lista_grup",
+    )
 
     add_form = BppUserCreationForm
 
     # change_form_template = 'loginas/change_form.html'
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None:
+            # Nie pozwól na usunięcie obecnego konta
+            if obj == request.user:
+                return False
+
+            # Sprawdź przy kasowaniu konta 'admin' czy są jeszcze jakieś konta z
+            # uprawnieniem admina, jeżeli nie, to nie pozwól na to:
+            if not BppUser.objects.exclude(pk=obj.pk).exists():
+                return False
+
+        return super(BppUserAdmin, self).has_delete_permission(request, obj)
 
     def lista_grup(self, row):
         return ", ".join([x.name for x in row.groups.all()])
@@ -177,9 +243,9 @@ admin.site.register(BppUser, BppUserAdmin)
 
 
 class SearchFormAdmin(admin.ModelAdmin):
-    list_display = ['name', 'owner', 'public']
-    fields = ['name', 'owner', 'public', 'data']
-    readonly_fields = ['data']
+    list_display = ["name", "owner", "public"]
+    fields = ["name", "owner", "public", "data"]
+    readonly_fields = ["data"]
 
 
 SearchForm._meta.verbose_name = "formularz wyszukiwania"
