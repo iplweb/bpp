6c526a5974c24a7271f8f7024f9e617d2c0fe618
Michał Pasternak <michal.dtz@gmail.com>
2020-03-28 23:17:47 +0100

    Fix tests

--- src/bpp/tests/test_selenium/test_admin/test_clarivate.py
+++ src/bpp/tests/test_selenium/test_admin/test_clarivate.py
@@ -7,7 +7,7 @@ from mock import Mock
 from selenium.webdriver.support.expected_conditions import alert_is_present
 
 from bpp.tests.util import scroll_into_view
-from django_bpp.selenium_util import wait_for
+from django_bpp.selenium_util import wait_for, wait_for_page_load
 from .helpers import *
 
 ID = "id_tytul_oryginalny"
@@ -15,10 +15,16 @@ ID = "id_tytul_oryginalny"
 pytestmark = [pytest.mark.slow, pytest.mark.selenium]
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def browser_z_wydawnictwem(preauth_admin_browser, live_server, wydawnictwo_ciagle):
     browser = preauth_admin_browser
-    browser.visit(live_server + reverse("admin:bpp_wydawnictwo_ciagle_change", args=(wydawnictwo_ciagle.pk,)))
+    with wait_for_page_load(browser):
+        browser.visit(
+            live_server
+            + reverse(
+                "admin:bpp_wydawnictwo_ciagle_change", args=(wydawnictwo_ciagle.pk,)
+            )
+        )
     return browser
 
 
@@ -29,24 +35,27 @@ def test_admin_get_wos_information_clarivate_brak_danych(browser_z_wydawnictwem)
     assertPopupContains(browser, "DOI")
 
 
-def test_admin_get_wos_information_clarivate_pmid(uczelnia, mocker, browser_z_wydawnictwem):
+def test_admin_get_wos_information_clarivate_pmid(
+    uczelnia, mocker, browser_z_wydawnictwem
+):
     from mock import Mock
+
     m = Mock()
-    m.query_single = Mock(return_value={'timesCited': '31337'})
+    m.query_single = Mock(return_value={"timesCited": "31337"})
     fn = mocker.patch("bpp.models.struktura.Uczelnia.wosclient", return_value=m)
 
     browser = browser_z_wydawnictwem
     browser.find_by_id("id_pubmed_id").type("31337")
     scroll_into_view(browser_z_wydawnictwem, "id_liczba_cytowan_get")
     browser.find_by_id("id_liczba_cytowan_get").click()
-    wait_for(
-        lambda: browser.find_by_id("id_liczba_cytowan_get").value == "Pobrano!"
-    )
+    wait_for(lambda: browser.find_by_id("id_liczba_cytowan_get").value == "Pobrano!")
     assert browser.find_by_id("id_liczba_cytowan_get").value == "Pobrano!"
     assert browser.find_by_id("id_liczba_cytowan").value == "31337"
 
 
-def test_admin_get_wos_information_clarivate_err(uczelnia, browser_z_wydawnictwem, mocker):
+def test_admin_get_wos_information_clarivate_err(
+    uczelnia, browser_z_wydawnictwem, mocker
+):
     m = Mock()
     m.query_single = Mock(side_effect=Exception("lel"))
     mocker.patch("bpp.models.struktura.Uczelnia.wosclient", return_value=m)
@@ -55,14 +64,13 @@ def test_admin_get_wos_information_clarivate_err(uczelnia, browser_z_wydawnictwe
     browser.find_by_id("id_pubmed_id").type("31337")
     scroll_into_view(browser_z_wydawnictwem, "id_liczba_cytowan_get")
     browser.find_by_id("id_liczba_cytowan_get").click()
-    wait_for(
-        lambda: alert_is_present()(browser.driver)
-    )
+    wait_for(lambda: alert_is_present()(browser.driver))
     assertPopupContains(browser, "lel")
 
 
 def test_admin_get_wos_information_clarivate_misconfigured(
-        uczelnia, browser_z_wydawnictwem, mocker):
+    uczelnia, browser_z_wydawnictwem, mocker
+):
     uczelnia.clarivate_password = uczelnia.clarivate_username = ""
     uczelnia.save()
 
@@ -70,16 +78,12 @@ def test_admin_get_wos_information_clarivate_misconfigured(
     browser.find_by_id("id_pubmed_id").type("31337")
     scroll_into_view(browser_z_wydawnictwem, "id_liczba_cytowan_get")
     browser.find_by_id("id_liczba_cytowan_get").click()
-    wait_for(
-        lambda: alert_is_present()(browser.driver)
-    )
+    wait_for(lambda: alert_is_present()(browser.driver))
     assertPopupContains(browser, "Brak użytkownika API")
 
     uczelnia.clarivate_username = "fa"
     uczelnia.save()
 
     browser.find_by_id("id_liczba_cytowan_get").click()
-    wait_for(
-        lambda: alert_is_present()(browser.driver)
-    )
+    wait_for(lambda: alert_is_present()(browser.driver))
     assertPopupContains(browser, "Brak hasła do API")
