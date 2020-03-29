1178b1aeff2f44e0aefafde50c563508f498d95f
Michał Pasternak <michal.dtz@gmail.com>
2020-03-30 00:02:22 +0200

    BPP API V1

--- src/api_v1/serializers/zrodlo.py
+++ src/api_v1/serializers/zrodlo.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 # Serializers define the API representation.
+from api_v1.serializers.util import AbsoluteUrlSerializerMixin, ChoicesSerializerField
 from bpp.models import Zrodlo, Rodzaj_Zrodla
 
 
@@ -13,10 +14,21 @@ class Rodzaj_ZrodlaSerializer(serializers.ModelSerializer):
         ]
 
 
-class ZrodloSerializer(serializers.HyperlinkedModelSerializer):
+class ZrodloSerializer(
+    AbsoluteUrlSerializerMixin, serializers.HyperlinkedModelSerializer
+):
     rodzaj = serializers.HyperlinkedRelatedField(
         view_name="api_v1:rodzaj_zrodla-detail", read_only=True
     )
+
+    jezyk = serializers.HyperlinkedRelatedField(
+        view_name="api_v1:jezyk-detail", read_only=True
+    )
+
+    zasieg = serializers.StringRelatedField()
+
+    openaccess_licencja = serializers.StringRelatedField()
+    openaccess_tryb_dostepu = serializers.StringRelatedField()
 
     class Meta:
         model = Zrodlo
@@ -28,4 +40,14 @@ class ZrodloSerializer(serializers.HyperlinkedModelSerializer):
             "nazwa_alternatywna",
             "skrot_nazwy_alternatywnej",
             "zasieg",
+            "www",
+            "doi",
+            "poprzednia_nazwa",
+            "jezyk",
+            "wydawca",
+            #
+            "openaccess_tryb_dostepu",
+            "openaccess_licencja",
+            #
+            "absolute_url",
         ]
