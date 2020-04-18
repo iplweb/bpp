812fba2a2c8f0a9a6b47332817b8add93660318b
Michał Pasternak <michal.dtz@gmail.com>
2020-04-18 06:08:10 +0200

    Mark as flaky

--- src/import_dyscyplin/tests/test_integration.py
+++ src/import_dyscyplin/tests/test_integration.py
@@ -2,24 +2,29 @@ import os
 
 import pytest
 from django.urls import reverse
+from flaky import flaky
 from model_mommy import mommy
 
 from bpp.models import Uczelnia
 from django_bpp.selenium_util import wait_for_page_load
 
 
+@flaky(max_runs=5)
 @pytest.mark.django_db(transaction=True)
 def test_integracyjny(preauth_admin_browser, nginx_live_server):
     mommy.make(Uczelnia)
-    preauth_admin_browser.visit(nginx_live_server.url + reverse("import_dyscyplin:index"))
+    preauth_admin_browser.visit(
+        nginx_live_server.url + reverse("import_dyscyplin:index")
+    )
 
     with wait_for_page_load(preauth_admin_browser):
         preauth_admin_browser.find_by_id("add-new-file").click()
 
     preauth_admin_browser.find_by_id("id_plik").type(
         os.path.join(
-            os.path.dirname(__file__),
-            "../static/import_dyscyplin/xlsx/default.xlsx"))
+            os.path.dirname(__file__), "../static/import_dyscyplin/xlsx/default.xlsx"
+        )
+    )
 
     with wait_for_page_load(preauth_admin_browser):
         preauth_admin_browser.find_by_id("id_submit").click()
