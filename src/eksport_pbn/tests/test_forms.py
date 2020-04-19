b757f37efe5d27e8b520e23c71a9ca5055b94204
Michał Pasternak <michal.dtz@gmail.com>
2020-04-19 23:52:30 +0200

    Fix tests

--- src/eksport_pbn/tests/test_forms.py
+++ src/eksport_pbn/tests/test_forms.py
@@ -29,7 +29,6 @@ def test_submit_report_form_validation_data_od_do(
     settings.NOTIFICATIONS_PORT = nginx_live_server.port
 
     page = admin_app.get(reverse("eksport_pbn:zamow"))
-    page.form["wydzial"] = wydzial.pk
     page.form["od_daty"] = "2010-01-01"
     page.form["do_daty"] = "2009-01-01"
     res = page.form.submit()
@@ -43,7 +42,6 @@ def test_submit_report_form_validation_rok_od_do(
     settings.NOTIFICATIONS_PORT = nginx_live_server.port
 
     page = admin_app.get(reverse("eksport_pbn:zamow"))
-    page.form["wydzial"] = wydzial.pk
     page.form["od_roku"] = "2015"
     page.form["do_roku"] = "2014"
     res = page.form.submit()
@@ -57,7 +55,6 @@ def test_submit_report_form_validation_artykuly_ksiazki(
     settings.NOTIFICATIONS_PORT = nginx_live_server.port
 
     page = admin_app.get(reverse("eksport_pbn:zamow"))
-    page.form["wydzial"] = wydzial.pk
     page.form["artykuly"] = False
     page.form["ksiazki"] = False
     page.form["rozdzialy"] = False
