5bfd3bb7dc2a46371e1bb5bdc330d10eebae9819
Michał Pasternak <michal.dtz@gmail.com>
2020-10-02 01:04:45 +0200

    Fix tests

--- src/bpp/tests/test_views/test_browse.py
+++ src/bpp/tests/test_views/test_browse.py
@@ -182,13 +182,13 @@ def test_artykuly(uczelnia, client):
 def test_artykul_ze_skrotem(uczelnia, client):
     a = Article.objects.create(
         title="123",
-        article_body="456\n<!-- tutaj -->\n789",
+        article_body="456\n<!-- tutaj -->\nTego ma nie byc",
         status=Article.STATUS.published,
         slug="1",
     )
 
     res = client.get(reverse("bpp:browse_uczelnia", args=(uczelnia.slug,)))
-    assert b"789" not in res.content
+    assert b"Tego ma nie byc" not in res.content
     assert "więcej" in res.rendered_content
 
     res = client.get(reverse("bpp:browse_artykul", args=(uczelnia.slug, a.slug)))
