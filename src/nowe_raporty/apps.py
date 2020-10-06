390e2ed7263a2e68beee59161e7fde8639ebb0b2
Michał Pasternak <michal.dtz@gmail.com>
2020-10-06 23:38:39 +0200

    Inicjuj formularze przy starcie aplikacji

--- src/nowe_raporty/apps.py
+++ src/nowe_raporty/apps.py
@@ -1,5 +1,24 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class NoweRaportyConfig(AppConfig):
-    name = 'nowe_raporty'
+    name = "nowe_raporty"
+
+    def ready(self):
+        if getattr(settings, "TESTING"):
+            return
+
+        from nowe_raporty.views import (
+            AutorRaportFormView,
+            JednostkaRaportFormView,
+            WydzialRaportFormView,
+        )
+
+        for elem in [
+            AutorRaportFormView,
+            JednostkaRaportFormView,
+            WydzialRaportFormView,
+        ]:
+            # Stwórz inicjalną wersję bazodanową formularza przy starcie aplikacji
+            elem().get_initial()
--- src/raport_slotow/apps.py
+++ src/raport_slotow/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class RaportSlotowConfig(AppConfig):
-    name = 'raport_slotow'
+    name = "raport_slotow"
+
+    def ready(self):
+        if getattr(settings, "TESTING"):
+            return
+
+        from .views.autor import WyborOsoby
+        from .views.uczelnia import ParametryRaportSlotowUczelnia
+        from .views.ewaluacja import ParametryRaportSlotowEwaluacja
+
+        for elem in (
+            WyborOsoby,
+            ParametryRaportSlotowEwaluacja,
+            ParametryRaportSlotowUczelnia,
+        ):
+            elem().get_initial()
