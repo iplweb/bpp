7e218472030be953f7d1282cf4a6726972533063
Michał Pasternak <michal.dtz@gmail.com>
2021-04-02 00:54:15 +0200

    PageableResource

--- src/pbn_api/core.py
+++ src/pbn_api/core.py
@@ -1,3 +1,4 @@
+import warnings
 from json import JSONDecodeError
 
 import requests
@@ -18,6 +19,46 @@ class PBNClientTransport:
             self.base_url = DEFAULT_BASE_URL
 
 
+class PageableResource:
+    def __init__(self, transport, res, url, headers):
+        self.url = url
+        self.headers = headers
+        self.transport = transport
+
+        self.current_content = res["content"]
+        self.current_page = res["number"]
+        self.total_elements = res["totalElements"]
+        self.total_pages = res["totalPages"]
+
+        self.done = False
+
+    def fetch_next_page(self):
+        self.current_page += 1
+        if self.current_page > self.total_pages:
+            return
+        res = self.transport.get(
+            self.url + f"?page={self.current_page}", headers=self.headers
+        )
+
+        if res is not None and "content" in res:
+            self.current_content = res["content"]
+            return True
+        else:
+            self.current_content = []
+
+    def __iter__(self):
+        if self.done:
+            return
+
+        while True:
+            try:
+                yield self.current_content.pop(0)
+            except IndexError:
+                if not self.fetch_next_page():
+                    self.done = True
+                    return
+
+
 class RequestsTransport(PBNClientTransport):
     def get(self, url, headers=None):
         sent_headers = {"X-App-Id": self.app_id, "X-App-Token": self.app_token}
@@ -32,8 +73,36 @@ class RequestsTransport(PBNClientTransport):
             raise e
 
     def get_pages(self, url, headers=None):
-        self.get(url, headers)
-        raise NotImplementedError
+        # Stronicowanie zwraca rezultaty w taki sposób:
+        # {'content': [{'mongoId': '5e709189878c28a04737dc6f',
+        #               'status': 'ACTIVE',
+        # ...
+        #              'versionHash': '---'}]}],
+        #  'first': True,
+        #  'last': False,
+        #  'number': 0,
+        #  'numberOfElements': 10,
+        #  'pageable': {'offset': 0,
+        #               'pageNumber': 0,
+        #               'pageSize': 10,
+        #               'paged': True,
+        #               'sort': {'sorted': False, 'unsorted': True},
+        #               'unpaged': False},
+        #  'size': 10,
+        #  'sort': {'sorted': False, 'unsorted': True},
+        #  'totalElements': 68577,
+        #  'totalPages': 6858}
+
+        res = self.get(url, headers)
+        if "pageable" not in res:
+            warnings.warn(
+                f"PBNClient.get_page request for {url} with headers {headers} did not return a paged resource, "
+                f"maybe use PBNClient.get instead",
+                RuntimeWarning,
+            )
+            return res
+
+        return PageableResource(self, res, url, headers)
 
 
 class PBNClient:
@@ -41,4 +110,6 @@ class PBNClient:
         self.transport = transport
 
     def get_conferences(self):
-        return self.transport.get_pages("/v1/conferences/page")
+        res = self.transport.get_pages("/v1/conferences/page")
+        for elem in res:
+            print(elem)
--- src/pbn_api/tests/test_core.py
+++ src/pbn_api/tests/test_core.py
@@ -3,7 +3,7 @@ import pytest
 from pbn_api.core import PBNClient, RequestsTransport
 
 
-class TestTransport:
+class TestTransport(RequestsTransport):
     def __init__(self, return_values=None):
         self.return_values = return_values
 
@@ -25,6 +25,33 @@ def test_RequestsTransport_get(mocker):
     rg.assert_called_once()
 
 
+def test_PBNClient_get_pages_warning():
+    t = TestTransport({"/v1/conferences/page": []})
+    c = PBNClient(t)
+    with pytest.warns(RuntimeWarning, match="did not return a paged"):
+        c.get_conferences()
+
+
 def test_PBNClient_get_conferences():
-    # t = TestTransport({"/v1/conferences/page": []})
-    raise NotImplementedError
+    t = TestTransport(
+        {
+            "/v1/conferences/page": {
+                "content": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                "number": 0,
+                "numberOfElements": 10,
+                "pageable": {
+                    "offset": 0,
+                    "pageNumber": 0,
+                    "pageSize": 10,
+                    "paged": True,
+                    "sort": {"sorted": False, "unsorted": True},
+                    "unpaged": False,
+                },
+                "size": 10,
+                "totalElements": 10,
+                "totalPages": 1,
+            }
+        }
+    )
+    c = PBNClient(t)
+    assert list(c.get_conferences()) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
