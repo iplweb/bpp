e579af0c809b742275268acd5a077d06f1f910a5
Michał Pasternak <michal.dtz@gmail.com>
2021-04-02 08:29:19 +0200

    Próba zapobieżenia problemowi z brakującą tabelą rozbieznosci_dyscyplin

--- src/rozbieznosci_dyscyplin/apps.py
+++ src/rozbieznosci_dyscyplin/apps.py
@@ -1,5 +1,28 @@
 from django.apps import AppConfig
+from django.db import DatabaseError, ProgrammingError
 
 
-class PrzypisywanieDyscyplinConfig(AppConfig):
-    name = 'przypisywanie_dyscyplin'
+class RozbieznosciDyscyplinConfig(AppConfig):
+    name = "rozbieznosci_dyscyplin"
+
+    def ready(self):
+        # Sprawdź, czy utworzone zostały widoki, z których korzysta ta aplikacja.
+        # W pewnych warunkach z powodu braku możliwości wpływu na kolejność migracji
+        # może dojść do sytuacji, gdzie zostaną usunięte (np przy ponownym tworzeniu
+        # tabeli bpp_rekord_mat) a nie bardzo mamy możliwość napisania testów automatycznych
+        # uwzględniających kolejność migracji (mpasternak, 2.04.2021)
+
+        from rozbieznosci_dyscyplin.models import (
+            BrakPrzypisaniaView,
+            RozbieznePrzypisaniaView,
+            RozbieznosciView,
+        )
+
+        for klass in BrakPrzypisaniaView, RozbieznePrzypisaniaView, RozbieznosciView:
+            try:
+                klass.objects.first()
+            except ProgrammingError:
+                raise DatabaseError(
+                    "Jeden lub wszystkie z widoków dla aplikacji rozbieznosc_dyscyplin nie istnieje. "
+                    "Prawdopodobnie moze miec to zwiazek z niedokonczona migracja. Prosze o weryfikacje. "
+                )
