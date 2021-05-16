e1e6128ad78165bbef3a5f17e09c771634df2f15
Michał Pasternak <michal.dtz@gmail.com>
2021-05-16 22:41:22 +0200

    Update MAX_UPLOAD for production

--- src/django_bpp/settings/production.py
+++ src/django_bpp/settings/production.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from .base import *
+from .base import *  # noqa
 
 DEBUG = False
 DEBUG_TOOLBAR = False
@@ -12,8 +12,11 @@ CSRF_COOKIE_SECURE = False
 COMPRESS_ENABLED = not DEBUG
 COMPRESS_OFFLINE = False
 
-TEMPLATES[0]["OPTIONS"]["loaders"] = [
-    ("django.template.loaders.cached.Loader", TEMPLATES[0]["OPTIONS"]["loaders"])
+TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa
+    (
+        "django.template.loaders.cached.Loader",
+        TEMPLATES[0]["OPTIONS"]["loaders"],  # noqa
+    )
 ]
 
 HTML_MINIFY = True
@@ -23,12 +26,17 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 CACHES = {
     "default": {
         "BACKEND": "redis_cache.RedisCache",
-        "LOCATION": [f"{REDIS_HOST}:{REDIS_PORT}",],
+        "LOCATION": [
+            f"{REDIS_HOST}:{REDIS_PORT}",  # noqa
+        ],
         "OPTIONS": {
-            "DB": REDIS_DB_CACHE,
+            "DB": REDIS_DB_CACHE,  # noqa
             "PARSER_CLASS": "redis.connection.HiredisParser",
             "CONNECTION_POOL_CLASS": "redis.BlockingConnectionPool",
-            "CONNECTION_POOL_CLASS_KWARGS": {"max_connections": 50, "timeout": 20,},
+            "CONNECTION_POOL_CLASS_KWARGS": {
+                "max_connections": 50,
+                "timeout": 20,
+            },
             "MAX_CONNECTIONS": 1000,
             "PICKLE_VERSION": -1,
         },
@@ -37,7 +45,7 @@ CACHES = {
 
 CACHE_MIDDLEWARE_SECONDS = 3600 * 24
 
-DATABASES["default"]["CONN_MAX_AGE"] = None
+DATABASES["default"]["CONN_MAX_AGE"] = None  # noqa
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 2621440 * 3  # 7.5 MB
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 4000
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 50000
