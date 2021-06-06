038cf7bc87ac953be08efa1bd481a0f7a2812c9d
Michał Pasternak <michal.dtz@gmail.com>
2021-06-06 21:45:03 +0200

    Fix tests, maybe

--- src/bpp/tests/test_models/test_uczelnia.py
+++ src/bpp/tests/test_models/test_uczelnia.py
@@ -14,11 +14,14 @@ def test_Uczelnia_clean_pbn_biezaco_tak_integracja_nie(uczelnia):
 
 
 @pytest.mark.django_db
-def test_Uczelnia_clean_integracja():
-    uczelnia = mommy.make(Uczelnia)
-
+def test_Uczelnia_clean_integracja(uczelnia, mocker):
     uczelnia.pbn_integracja = True
     uczelnia.pbn_app_token = uczelnia.pbn_app_name = "hej"
+
+    pbn_client = mocker.Mock()
+    pbn_client.return_value.get_languages.side_effect = KeyError("foo")
+
+    uczelnia.pbn_client = pbn_client
 
     with pytest.raises(ValidationError, match="Nie można pobrać"):
         uczelnia.clean()
@@ -29,6 +32,9 @@ def test_Uczelnia_pbn_client():
     uczelnia = mommy.make(Uczelnia)
 
     uczelnia.pbn_app_token = uczelnia.pbn_api_root = uczelnia.pbn_app_name = None
+
+    if uczelnia.pbn_app_name is not None:
+        raise Exception("x")
 
     with pytest.raises(AssertionError, match="nazwy aplikacji"):
         uczelnia.pbn_client()
