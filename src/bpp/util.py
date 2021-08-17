1a4f652e7d63bf17566e63c5860c6ba1e145d266
Michał Pasternak <michal.dtz@gmail.com>
2021-08-18 00:50:47 +0200

    Kosmetyka

--- src/bpp/util.py
+++ src/bpp/util.py
@@ -240,7 +240,10 @@ def pbar(query, count=None, label="Progres...", disable_progress_bar=False):
     if sys.stdout.isatty() and not disable_progress_bar:
         if count is None:
             if hasattr(query, "count"):
-                count = query.count()
+                try:
+                    count = query.count()
+                except TypeError:
+                    count = len(query)
             elif hasattr(query, "__len__"):
                 count = len(query)
 
