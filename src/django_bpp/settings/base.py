242e57dceda088ba7befb247e555c42d95e4ad21
Michał Pasternak <michal.dtz@gmail.com>
2021-08-18 11:17:37 +0200

    Nie używaj SSL dla połączeń lokalnych + usuń starą konfigurację

--- src/django_bpp/settings/base.py
+++ src/django_bpp/settings/base.py
@@ -364,15 +364,12 @@ DATABASES = {
         "HOST": django_getenv("DJANGO_BPP_DB_HOST", "localhost"),
         "PORT": int_or_None(django_getenv("DJANGO_BPP_DB_PORT", "5432")),
     },
-    # 'test': {
-    #     'ENGINE': django_getenv("DJANGO_BPP_DB_ENGINE", 'django.db.backends.postgresql_psycopg2'),
-    #     'NAME': django_getenv("DJANGO_BPP_DB_NAME", "test_django-bpp"),
-    #     'USER': django_getenv("DJANGO_BPP_DB_USER", "postgres"),
-    #     'PASSWORD': django_getenv("DJANGO_BPP_DB_PASSWORD", "password"),
-    #     'HOST': django_getenv("DJANGO_BPP_DB_HOST", "localhost"),
-    #     'PORT': int_or_None(django_getenv("DJANGO_BPP_DB_PORT", "5432")),
-    # },
 }
+
+if DATABASES["default"]["HOST"] in ["localhost", "127.0.0.1"]:
+    options = DATABASES["default"].get("OPTIONS", {})
+    options["sslmode"] = "disable"
+    DATABASES["default"]["OPTIONS"] = options
 
 SECRET_KEY = django_getenv("DJANGO_BPP_SECRET_KEY")
 
