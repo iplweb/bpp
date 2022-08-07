8be37bec6e4932f766c1d39a81f0b26af01300ea
Michał Pasternak <michal.dtz@gmail.com>
2022-08-08 01:39:40 +0200

    Fix tests

--- src/zglos_publikacje/tests.py
+++ src/zglos_publikacje/tests.py
@@ -12,25 +12,34 @@ def test_pierwsza_strona(webtest_app):
     url = reverse("zglos_publikacje:nowe_zgloszenie")
     page = webtest_app.get(url)
     page.forms[0]["0-tytul_oryginalny"] = "123"
+    page.forms[0]["0-rok"] = "2020"
     page.forms[0]["0-email"] = "123@123.pl"
 
     page2 = page.forms[0].submit()
     assert page2.status_code == 200
 
 
-def test_druga_strona(webtest_app, wprowadzanie_danych_user, transactional_db):
+def test_druga_strona(
+    webtest_app, wprowadzanie_danych_user, transactional_db, typy_odpowiedzialnosci
+):
     assert editors_emails()
     try:
         url = reverse("zglos_publikacje:nowe_zgloszenie")
         page = webtest_app.get(url)
         page.forms[0]["0-tytul_oryginalny"] = "123"
+        page.forms[0]["0-rok"] = "2020"
         page.forms[0]["0-email"] = "123@123.pl"
 
         page2 = page.forms[0].submit()
-        page2.forms[0]["1-opl_pub_cost_free"] = "true"
 
-        page3 = page2.forms[0].submit().maybe_follow()
-        assert b"powiadomiony" in page3.content
+        page3 = page2.forms[0].submit()
+
+        page3.forms[0]["2-opl_pub_cost_free"] = "true"
+
+        page4 = page3.forms[0].submit().maybe_follow()
+
+        page5 = page4.forms[0].submit().maybe_follow()
+        assert b"powiadomiony" in page5.content
         transaction.commit()
         from django.core import mail
 
