304d54c8f1b10cb44bfafb92e50177ac74e8de74
Michał Pasternak <michal.dtz@gmail.com>
2022-10-03 11:24:35 +0200

    Dynamiczne kolumny - import inicjalny

--- src/dynamic_columns/tests.py
+++ src/dynamic_columns/tests.py
@@ -1,1 +1,29 @@
-# Create your tests here.
+from django.urls import reverse
+
+from dynamic_columns.models import ModelAdmin
+
+from django.contrib.admin import site
+
+
+def test_autor_admin_hide_column(admin_app, autor_jan_kowalski):
+    POPRZEDNIE_NAZWISKA = "159ygquadfja0eth0qjaoidjgo"
+
+    autor_jan_kowalski.poprzednie_nazwiska = POPRZEDNIE_NAZWISKA
+    autor_jan_kowalski.save()
+
+    # Open the 'default' admin instance
+    res = admin_app.get(reverse("admin:bpp_autor_changelist"))
+    assert POPRZEDNIE_NAZWISKA in res
+    assert "Kowalski" in res
+
+    # Get the instance of AutorAdmin
+    autor_admin = site._registry.get(autor_jan_kowalski.__class__)
+
+    ma = ModelAdmin.objects.db_repr(autor_admin)
+    c = ma.modeladmincolumn_set.get(col_name="poprzednie_nazwiska")
+    c.enabled = False
+    c.save()
+
+    res = admin_app.get(reverse("admin:bpp_autor_changelist"))
+    assert POPRZEDNIE_NAZWISKA not in res
+    assert "Kowalski" in res
