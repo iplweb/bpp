30a342a819557516279ccb6f0d28a24b265bd85f
Michał Pasternak <michal.dtz@gmail.com>
2022-10-03 12:15:22 +0200

    Monkey patch export-import

--- src/bpp/admin/xlsx_export/mixins.py
+++ src/bpp/admin/xlsx_export/mixins.py
@@ -53,3 +53,44 @@ class EksportDanychMixin(ExportMixin):
 
         post_export.send(sender=None, model=self.model)
         return response
+
+    def get_export_queryset(self, request):
+        """
+        Jedyna roznica vs ExportMixin to ta linia:
+
+            "list_select_related": self.get_list_select_related(request),
+
+        Usunąć tą funkcję po poprawce:
+
+            https://github.com/django-import-export/django-import-export/issues/1493
+
+        """
+        list_display = self.get_list_display(request)
+        list_display_links = self.get_list_display_links(request, list_display)
+        list_filter = self.get_list_filter(request)
+        search_fields = self.get_search_fields(request)
+        if self.get_actions(request):
+            list_display = ["action_checkbox"] + list(list_display)
+
+        ChangeList = self.get_changelist(request)
+        changelist_kwargs = {
+            "request": request,
+            "model": self.model,
+            "list_display": list_display,
+            "list_display_links": list_display_links,
+            "list_filter": list_filter,
+            "date_hierarchy": self.date_hierarchy,
+            "search_fields": search_fields,
+            "list_select_related": self.get_list_select_related(request),
+            "list_per_page": self.list_per_page,
+            "list_max_show_all": self.list_max_show_all,
+            "list_editable": self.list_editable,
+            "model_admin": self,
+        }
+        import django
+
+        if django.VERSION >= (2, 1):
+            changelist_kwargs["sortable_by"] = self.sortable_by
+        cl = ChangeList(**changelist_kwargs)
+
+        return cl.get_queryset(request)
