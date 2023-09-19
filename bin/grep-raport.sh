c0d969c1121b7cb9cf6fe4ee5adc41f6ffc5d67b
Michał Pasternak <michal.dtz@gmail.com>
2023-09-19 12:16:44 +0200

    Wygrepuj dane z rezultatu importu listy ministerialnej

---
+++ bin/grep-raport.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Polecenie towarzyszące poleceniu z systemu `import_lista_ministerialna_2023`
+# dzieli plik wyjściowy na kilka strumieni, każdy wysyła do oddzielnego
+# pliku CSV.
+
+for a in PKT-0 PKT-1 PKT-2 PKT-3 PKT-4 PKT-5; do
+    grep $a $1 > $1-$a.csv;
+done
