39c0b8409640def87ed40e45dd389250c500f016
Michał Pasternak <michal.dtz@gmail.com>
2023-10-19 08:25:13 +0200

    Revert "Fix tests"
    This reverts commit 5750eb231d4e6145530beeae51822a4b94b378df.

--- src/bpp/tests/tests_legacy/test_views/test_autocomplete.py
+++ src/bpp/tests/tests_legacy/test_views/test_autocomplete.py
@@ -90,7 +90,7 @@ class TestAutocomplete(WebTestCase):
         baker.make(Jednostka, nazwa=S)
 
         x = autocomplete.GlobalNavigationAutocomplete()
-        x.q = "Foobar"
+        x.q = "Foo"
         res = x.get_results({"object_list": list(x.get_queryset())})
 
         cnt = 0
