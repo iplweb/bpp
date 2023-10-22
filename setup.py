88da5635465caffe1cb2d2b52d3f63a5364eec7b
Michał Pasternak <michal.dtz@gmail.com>
2023-10-22 22:36:51 +0200

    bump version v202310.1119 -> v202310.1120

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def requirements(fn="requirements.txt"):
 
 setup(
     name="bpp-iplweb",
-    version="202310.1119",
+    version="202310.1120",
     description="System informatyczny do zarządzania bibliografią publikacji pracowników naukowych",
     long_description=readme,
     long_description_content_type="text/x-rst",
