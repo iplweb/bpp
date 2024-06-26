67ff5ad6490162816f6f53e0f3ebbe4b006e9553
Michał Pasternak <michal.dtz@gmail.com>
2024-06-26 09:46:59 +0200

    Uwagi po kilku migracjach

--- bin/move-to-docker.sh
+++ bin/move-to-docker.sh
@@ -7,9 +7,23 @@
 # NIE uruchamiac, traktować jako luźną propozycję wyciągnięcia informacji z metalowego
 # serwera...
 
+
+
 # Skopiuj ustawienia z plików ~/.env, ~/.env.local, ~/env/bin/activate
 # do pliku .env obok docker-compose
 cat .env .env.local env/bin/activate > .env
+
+# uważaj na znak dolara w pliku .env (docker sprobuje substytuowac)
+# uważaj na "localhost" w pliku .env
+# ustaw DJANGO_BPP_REDIS_SERVER="redis"
+
+
+# Skonfiguruj wykonywanie kopii zapasowej
+cd /home/backup-ipl/...
+cp rclone-config ...
+
+# Zatrzymaj nginx
+service nginx stop
 
 # Nazwa bazy danych na "metalowym" serwerze
 DB_NAME=bpp
@@ -20,11 +34,11 @@ DB_CONTAINER=` docker ps | grep dbserver| cut -d\  -f1`
 # ID kontenera z webserverem
 WEB_CONTAINER=` docker ps | grep webserver| cut -d\  -f1`
 
+# ID kontenera z aplikacją
+APP_CONTAINER=` docker ps | grep appserver-1 | cut -d\  -f1`
+
 # Zatrzymaj usługi "metalowe"
 supervisorctl stop all
-
-# Zatrzymaj webserver dockera
-docker container pause $WEB_CONTAINER
 
 # Skasuj i utwórz pustą bazę BPP po stronie dockera
 docker exec -it $DB_CONTAINER dropdb --force -U postgres bpp
@@ -32,14 +46,14 @@ docker exec -it $DB_CONTAINER createdb -U postgres bpp
 docker exec -it $DB_CONTAINER createuser -U postgres -s bpp
 
 # Wczytaj dump bazy do PostgreSQLa
-pg_dump $DB_NAME | docker exec -t $DB_CONTAINER psql bpp
-
-# Odpauzuj web container
-docker container unpause $WEB_CONTAINER
+pg_dump $DB_NAME | docker exec -i $DB_CONTAINER psql -U postgres bpp
 
 # Zatrzymaj "metalowego" PostgreSQL
 service postgresql stop
-apt remove postgresql-14 redis-server redis supervisor nginx-*
+# apt remove postgresql-14
+systemctl disable postgresql
+
+apt remove -y redis-server redis supervisor nginx-*
 apt autoremove -y
 apt clean
 apt autoclean
@@ -47,6 +61,9 @@ apt autoclean
 # Usuń crontaby
 sudo crontab -e
 su - bpp -c "crontab -e"
+
+# Skopiuj mediaroot
+cd ~bpp/media/  && docker cp . $APP_CONTAINER:/mediaroot
 
 # Usuń użytkownika systemowego "bpp"
 userdel bpp
