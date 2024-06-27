4e1ec3eed68954cadef025cbe64ee8cb167c882b
Michał Pasternak <michal.dtz@gmail.com>
2024-06-27 22:18:14 +0200

    wg dokumentacji, ustawienia Sentry zaciągną się przez .config_from_object

--- src/django_bpp/celery_tasks.py
+++ src/django_bpp/celery_tasks.py
@@ -1,24 +1,5 @@
-# -*- encoding: utf-8 -*-
-
-from celery import Celery, signals
+from celery import Celery
 from django.conf import settings
-
-
-@signals.setup_logging.connect
-def setup_logging(**kwargs):
-    """Setup logging."""
-
-
-class Celery(Celery):
-    def on_configure(self):
-        if hasattr(settings, "RAVEN_CONFIG") and settings.RAVEN_CONFIG["dsn"]:
-            import raven
-            from raven.contrib.celery import register_logger_signal, register_signal
-
-            client = raven.Client(settings.RAVEN_CONFIG["dsn"])
-            register_logger_signal(client)
-            register_signal(client)
-
 
 app = Celery("django_bpp")
 app.config_from_object("django.conf:settings")
