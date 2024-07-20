95651f867c33b4fdb72fccc8e6320a211869d0a5
Michał Pasternak <michal.dtz@gmail.com>
2024-07-20 20:59:08 +0200

    Błąd z brakującym rokiem przy dodawaniu autora
    przez "zakładkę"

--- src/bpp/admin/wydawnictwo_zwarte_autor.py
+++ src/bpp/admin/wydawnictwo_zwarte_autor.py
@@ -12,3 +12,12 @@ class Wydawnictwo_Zwarte_Autor_Admin(Wydawnictwo_Autor_Base):
     import_export_change_list_template = (
         "admin/bpp/wydawnictwo_zwarte_autor/change_list.html"
     )
+
+    def _changeform_view(self, request, object_id, form_url, extra_context):
+        if extra_context is None:
+            extra_context = {}
+        rekord_pk = self.get_changeform_initial_data(request).get("rekord", None)
+        if rekord_pk is not None:
+            rok = Wydawnictwo_Zwarte.objects.get(pk=rekord_pk).rok
+            extra_context["rok"] = rok
+        return super()._changeform_view(request, object_id, form_url, extra_context)
