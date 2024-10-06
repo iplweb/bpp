da77fef7e63142c136b24faa02775fb1998cc7a8
Michał Pasternak <michal.dtz@gmail.com>
2024-10-06 20:30:46 +0200

    Szybsze generowanie XLS w raport slotów - ewaluacja

--- src/raport_slotow/tests/test_ewaluacja.py
+++ src/raport_slotow/tests/test_ewaluacja.py
@@ -1,21 +1,31 @@
 from django.urls import reverse
 
+from fixtures.conftest import _wydawnictwo_ciagle_maker
+from raport_slotow.tests.conftest import _rekord_slotu_maker
 from raport_slotow.views import ewaluacja
 
 from bpp.models import Autor_Dyscyplina
 
 
 def test_raport_ewaluacja_no_queries(
-    rekord_slotu,
     django_assert_max_num_queries,
     admin_client,
     rok,
     autor_jan_kowalski,
+    jednostka,
     dyscyplina1,
 ):
     Autor_Dyscyplina.objects.create(
         rok=rok, autor=autor_jan_kowalski, dyscyplina_naukowa=dyscyplina1
     )
+
+    for a in range(100):
+        wydawnictwo = _wydawnictwo_ciagle_maker()
+        wydawnictwo.dodaj_autora(autor_jan_kowalski, jednostka)
+
+        _rekord_slotu_maker(
+            autor_jan_kowalski, jednostka, dyscyplina1, wydawnictwo, rok
+        )
 
     url = reverse(
         "raport_slotow:raport-ewaluacja",
@@ -23,9 +33,12 @@ def test_raport_ewaluacja_no_queries(
 
     r = ewaluacja.RaportSlotowEwaluacja()
     r.data = dict(od_roku=rok, do_roku=rok, _export="html")
-    assert r.get_queryset().count() == 1
+    assert r.get_queryset().count() == 100
 
-    with django_assert_max_num_queries(13):
+    # UWAGA UWAGA UWAGA
+    # Jeżeli nagle z jakichś powodów ten raport zacznie generować więcej zapytań, to proszę
+    # się nad tym tematem POCHYLIC i nie zwiekszać tej wartosci max_num_queries...
+    with django_assert_max_num_queries(12):
         admin_client.get(
             url,
             data={
