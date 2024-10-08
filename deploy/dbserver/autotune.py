7934880ea6393f835b81f4d36c7076ffce2fabcf
Michał Pasternak <michal.dtz@gmail.com>
2024-10-08 18:28:00 +0200

    Better(?) autotune...

--- deploy/dbserver/autotune.py
+++ deploy/dbserver/autotune.py
@@ -11,11 +11,16 @@ ONE_GB_IN_KB = 1024 * ONE_MB_IN_KB
 
 how_much_ram_for_postgres = float(os.environ.get("POSTGRESQL_RAM_PERCENT", "0.5"))
 force_ram_for_postgres = os.environ.get("POSTGRESQL_RAM_THIS_MUCH_GB", None)
-default_ram_for_postgres = 4096
+default_ram_for_postgres = int(os.environ.get("POSTGRESQL_DEFAULT_RAM", 4096))
 
 
 def total_ram_size_mb():
     if force_ram_for_postgres:
+        print(
+            f"# autotune.py: RAM size for Postgres is {force_ram_for_postgres} kB, because of "
+            f"env var POSTGRESQL_RAM_THIS_MUCH_DB setting. Change the env var to tweak it or"
+            f"remove it to use automatic RAM size detection."
+        )
         return int(force_ram_for_postgres)
 
     import re
@@ -26,9 +31,22 @@ def total_ram_size_mb():
         matched = re.search(r"^MemTotal:\s+(\d+)", meminfo)
         if matched:
             mem_total_kB = int(matched.groups()[0])
+            print(
+                f"# autotune.py: detected {mem_total_kB} kB RAM; will use {int(how_much_ram_for_postgres*100)} % of it"
+            )
             return how_much_ram_for_postgres * mem_total_kB / 1024
 
+    print(
+        f"# autotune.py: unable to detect RAM size, returning default {default_ram_for_postgres} kB; "
+        f"change environment variable POSTGRESQL_DEFAULT_RAM if you need to change this"
+    )
     return default_ram_for_postgres
+
+
+def get_nproc():
+    import multiprocessing
+
+    return multiprocessing.cpu_count()
 
 
 def normalize_size(v):
@@ -65,10 +83,8 @@ def generate_config(ram_kb):
     config["effective_cache_size"] = ram_kb * 3 / 4
     config["maintenance_work_mem"] = min(ram_kb / 16, 2 * ONE_GB_IN_KB)
 
-    # We're tweaking this a little bit here. While we allow up to 250
-    # connections (because we've frequently seen customers with leaky or
-    # misconfigured connection pools), we assume 50 per GB.
-    conns = 75 * ram_kb / ONE_GB_IN_KB
+    # 100 połaczeń na 1 GB RAM
+    conns = 100 * ram_kb / ONE_GB_IN_KB
     config["max_connections"] = conns
     config["work_mem"] = (ram_kb * 3 / 4) / (conns * 3)
 
@@ -80,7 +96,23 @@ def generate_config(ram_kb):
     config["checkpoint_completion_target"] = "0.7"
     config["default_statistics_target"] = "100"
 
-    return {x: to_config_value(y, x) for x, y in config.items()}
+    nproc = get_nproc()
+    if nproc >= 4:
+
+        max_parallel = 2
+
+        if nproc in [5, 6]:
+            max_parallel = 3
+
+        if nproc >= 7:
+            max_parallel = 4
+
+    config["max_worker_processes"] = str(nproc)
+    config["max_parallel_workers_per_gather"] = str(max_parallel)
+    config["max_parallel_workers"] = str(nproc)
+    config["max_parallel_maintenance_workers"] = str(max_parallel)
+
+    return {x: to_config_value(y, x) for x, y in sorted(config.items())}
 
 
 def main():
