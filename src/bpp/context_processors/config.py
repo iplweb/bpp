81b64e53d2d9c4b92fb72a8b8e84246cb02a69f6
Michał Pasternak <michal.dtz@gmail.com>
2024-10-17 08:53:42 +0200

    Fix #1460
    Kopiuj kwartyl z/od źródła

--- src/bpp/context_processors/config.py
+++ src/bpp/context_processors/config.py
@@ -1,15 +1,12 @@
 from django.conf import settings
 
 
-def theme_name(request):
-    return {"THEME_NAME": "scss/" + settings.THEME_NAME + ".css"}
+def bpp_configuration(request):
+    from bpp.models.abstract import POLA_PUNKTACJI
 
-
-def enable_new_reports(request):
-    return {"ENABLE_NEW_REPORTS": settings.ENABLE_NEW_REPORTS}
-
-
-def max_no_authors_on_browse_jednostka_page(request):
     return {
-        "MAX_NO_AUTHORS_ON_BROWSE_JEDNOSTKA_PAGE": settings.MAX_NO_AUTHORS_ON_BROWSE_JEDNOSTKA_PAGE
+        "THEME_NAME": "scss/" + settings.THEME_NAME + ".css",
+        "ENABLE_NEW_REPORTS": settings.ENABLE_NEW_REPORTS,
+        "MAX_NO_AUTHORS_ON_BROWSE_JEDNOSTKA_PAGE": settings.MAX_NO_AUTHORS_ON_BROWSE_JEDNOSTKA_PAGE,
+        "BPP_POLA_PUNKTACJI": POLA_PUNKTACJI,
     }
