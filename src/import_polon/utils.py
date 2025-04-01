0e3525ae706c1987212353832ea6f2b16091191c
Michał Pasternak <michal.dtz@gmail.com>
2025-04-01 14:53:55 +0200

    Windows-1250 chyba, że cos. Chardet średnio działa

--- src/import_polon/utils.py
+++ src/import_polon/utils.py
@@ -9,12 +9,20 @@ def read_excel_or_csv_dataframe_guess_encoding(fn, header=0):
     if fnl.endswith(".xlsx") or fnl.endswith(".xls"):
         return pd.read_excel(fn, header=0).replace({np.nan: None})
     elif fnl.endswith(".csv"):
-        import chardet
 
-        encoding = chardet.detect(open(fn, "rb").read())
-        return pd.read_csv(
-            fn, header=0, sep=";", encoding=encoding["encoding"]
-        ).replace({np.nan: None})
+        try:
+            # Chardet średnio wykrywa właściwą stronę kodową dla eksportów z POLON w CSV...
+            return pd.read_csv(fn, header=0, sep=";", encoding="windows-1250").replace(
+                {np.nan: None}
+            )
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            # ale jezeli to nie windows-1250 to niech wykryje:
+            import chardet
+
+            encoding = chardet.detect(open(fn, "rb").read())
+            return pd.read_csv(
+                fn, header=0, sep=";", encoding=encoding["encoding"]
+            ).replace({np.nan: None})
 
     else:
         raise ValueError(f"Nieznany format pliku: {fn}")
