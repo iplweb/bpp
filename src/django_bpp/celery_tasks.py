2113bdc32b5f841ded217928d0fb125bf81ebce5
Michał Pasternak <michal.dtz@gmail.com>
2025-08-20 22:39:57 +0200

    Programming is hard.

--- src/django_bpp/celery_tasks.py
+++ src/django_bpp/celery_tasks.py
@@ -1,6 +1,8 @@
 from celery import Celery
+from django.apps import apps
 from django.conf import settings
 
 app = Celery("django_bpp")
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
