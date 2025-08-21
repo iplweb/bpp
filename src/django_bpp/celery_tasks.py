60b8f7dfdefc98bb9aa13ed53497adf0c6bd1266
Michał Pasternak <michal.dtz@gmail.com>
2025-08-21 19:34:16 +0200

    Celery-singleton for task queue

--- src/django_bpp/celery_tasks.py
+++ src/django_bpp/celery_tasks.py
@@ -1,4 +1,6 @@
 from celery import Celery
+from celery.signals import worker_ready
+from celery_singleton import clear_locks
 from django.apps import apps
 from django.conf import settings
 
@@ -6,3 +8,8 @@ app = Celery("django_bpp")
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+
+
+@worker_ready.connect
+def unlock_all(**kwargs):
+    clear_locks(app)
