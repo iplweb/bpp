ca16385d9be8873d31371275a43da1ebf82b4830
Michał Pasternak <michal.dtz@gmail.com>
2025-08-25 00:08:32 +0200

    możliwość ponownej wysyłki elementu z kolejki eksportu PBN

--- src/pbn_api/admin/pbn_export_queue.py
+++ src/pbn_api/admin/pbn_export_queue.py
@@ -1,9 +1,11 @@
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from pbn_api.admin.mixins import ReadOnlyListChangeFormAdminMixin
 from pbn_api.models.queue import PBN_Export_Queue
+from pbn_api.tasks import task_sprobuj_wyslac_do_pbn
 
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from django.utils.safestring import mark_safe
 
@@ -14,10 +16,11 @@ class RenderHTMLWidget(forms.Textarea):
 
 
 @admin.register(PBN_Export_Queue)
-class PBN_Export_QueueAdmin(ReadOnlyListChangeFormAdminMixin, admin.ModelAdmin):
+class PBN_Export_QueueAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_display = [
         "rekord_do_wysylki",
+        "object_id",
         "zamowil",
         "wysylke_podjeto",
         "wysylke_zakonczono",
@@ -56,5 +59,54 @@ class PBN_Export_QueueAdmin(ReadOnlyListChangeFormAdminMixin, admin.ModelAdmin):
 
     formfield_overrides = {models.TextField: {"widget": RenderHTMLWidget}}
 
+    def _resend_single_item(self, obj, user, message_suffix=""):
+        """Common logic for resending a single PBN export queue item"""
+        obj.refresh_from_db()
+        obj.wysylke_zakonczono = None
+        obj.zakonczono_pomyslnie = None
+        obj.retry_after_user_authorised = None
+        obj.dopisz_komunikat(
+            f"Ponownie wysłano przez użytkownika: {user}{message_suffix}"
+        )
+        obj.save()
+        task_sprobuj_wyslac_do_pbn.delay(obj.pk)
+
+    def resend_to_pbn_action(self, request, queryset):
+        count = 0
+        for obj in queryset:
+            self._resend_single_item(obj, request.user, " (akcja masowa)")
+            count += 1
+
+        self.message_user(request, f"Ponowiono wysyłkę do PBN dla {count} elementów")
+
+    resend_to_pbn_action.short_description = "Wyślij ponownie"
+
+    actions = [resend_to_pbn_action]
+
     def save_form(self, request, form, change):
+        return form.save(commit=False)
+
+    def response_change(self, request, obj):
+        if "_resend_to_pbn" in request.POST:
+            self._resend_single_item(obj, request.user)
+            self.message_user(request, f"Ponowiono wysyłkę do PBN: {obj}")
+            return HttpResponseRedirect(
+                reverse(
+                    "admin:{}_{}_change".format(
+                        obj._meta.app_label, obj._meta.model_name
+                    ),
+                    args=[obj.pk],
+                )
+            )
+        return super().response_change(request, obj)
+
+    def has_add_permission(self, request):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        # Uczyń FAKTYCZNIE readonly :-)
+        messages.error(
+            request,
+            "Obiekt NIE został zapisany -- nie można edytować tej części serwisu.",
+        )
         return
