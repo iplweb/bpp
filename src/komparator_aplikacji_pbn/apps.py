b9ef7c2a52035ba6f57be22611197511a2f1fbcb
Michał Pasternak <michal.dtz@gmail.com>
2025-09-01 01:38:30 +0200

    fix build

---
+++ src/komparator_aplikacji_pbn/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class KomparatorAplikacjiPbnConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "komparator_aplikacji_pbn"
+    verbose_name = "Komparator aplikacji PBN"
