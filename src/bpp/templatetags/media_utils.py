84e6dcd08b343d27c4ba697350788a38152c24cc
Michał Pasternak <michal.dtz@gmail.com>
2025-10-05 22:24:47 +0200

    errata (logo na pierwszej)

--- src/bpp/templatetags/media_utils.py
+++ src/bpp/templatetags/media_utils.py
@@ -9,4 +9,4 @@ def media_exists(path):
     """Check if a media file exists in the storage."""
     if not path:
         return False
-    return default_storage.exists(path)
+    return default_storage.exists(path.path)
