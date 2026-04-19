064da89eae34124a336e75645a140c50fdc0cd58
Michał Pasternak <michal.dtz@gmail.com>
2026-04-19 18:02:33 +0200

    test(long_running): move TestReport to test_bpp app
    Definiowanie `class TestReport(Report)` w ciele fixture'a powodowało
    ponowną rejestrację modelu `long_running.testreport` przy każdym
    uruchomieniu testu korzystającego z fixture'a `report`, co Django
    sygnalizowało `RuntimeWarning: Model 'long_running.testreport' was
    already registered. Reloading models is not advised...`.
    
    TestReport ma teraz stałą definicję w `test_bpp.models` wraz z
    migracją `0003_testreport`, a fixture sprowadza się do prostego
    `baker.make(TestReport)`. Nieużywany nigdzie override
    `send_notification` (dziedziczony NullNotificationMixin i tak jest
    no-opem) został usunięty.

--- src/long_running/tests/conftest.py
+++ src/long_running/tests/conftest.py
@@ -1,9 +1,7 @@
 import pytest
-from django.db import connection
 from model_bakery import baker
 
-from long_running.models import Report
-from test_bpp.models import TestOperation
+from test_bpp.models import TestOperation, TestReport
 
 
 @pytest.fixture
@@ -13,17 +11,4 @@ def operation(admin_user):
 
 @pytest.fixture
 def report(db):
-    class TestReport(Report):
-        sent_notifications = []
-
-        def send_notification(self, *args, **kw):
-            self.sent_notifications.append((args, kw))
-
-    # Create the schema for our test model
-    with connection.schema_editor() as schema_editor:
-        schema_editor.create_model(TestReport)
-
-    yield baker.make(TestReport)
-
-    # with connection.schema_editor() as schema_editor:
-    #     schema_editor.delete_model(TestReport)
+    return baker.make(TestReport)
