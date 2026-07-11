b94743253024cc62030b76df7230d07f7ac23fb3
Michał Pasternak <michal.dtz@gmail.com>
2026-07-11 13:20:06 +0200

    feat(oauth_mcp): RFC 8414 metadata /.well-known/oauth-authorization-server

--- src/oauth_mcp/urls.py
+++ src/oauth_mcp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from oauth2_provider import urls as oauth2_urls
 
+from oauth_mcp.views_metadata import oauth_authorization_server_metadata
+
 # BEZ `app_name` na tym module! Deklaracja `app_name="oauth_mcp"` zagnieżdżałaby
 # namespace DOT (`oauth_mcp:oauth2_provider:authorize`) i psuła
 # `{% url 'oauth2_provider:authorize' %}` w szablonie zgody → NoReverseMatch.
@@ -10,4 +12,9 @@ from oauth2_provider import urls as oauth2_urls
 # NIE management-views (/o/applications/ CRUD dostępne każdemu zalogowanemu).
 urlpatterns = [
     path("o/", include((oauth2_urls.base_urlpatterns, "oauth2_provider"))),
+    path(
+        ".well-known/oauth-authorization-server",
+        oauth_authorization_server_metadata,
+        name="oauth-as-metadata",
+    ),
 ]
